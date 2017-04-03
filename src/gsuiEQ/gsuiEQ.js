3f8e9d5295d053b749404c922914cadc3c33ea58
Thomas Tortorini <thomastortorini@gmail.com>
2017-04-03 09:42:29 +0200

    gsuiEQ: now use of <template>, cleanup CSS

--- src/gsuiEQ/gsuiEQ.js
+++ src/gsuiEQ/gsuiEQ.js
@@ -1,7 +1,29 @@
 "use strict";
 
-function gsuiEQ( elRoot ) {
+function gsuiEQ() {
+	var root = this._clone(),
+		canvas = root.querySelector( "canvas" );
+
+	this.rootElement = root;
+	this.uiSpectrum = new gsuiSpectrum( canvas );
+	this.uiSpectrum.setResolution( 256 );
 };
 
 gsuiEQ.prototype = {
+	drawSpectrum: function( data ) {
+		this.uiSpectrum.draw( data );
+	},
+
+	// private:
+	_clone: function() {
+		var div = document.createElement( "div" );
+
+		gsuiEQ.template = gsuiEQ.template || this._init();
+		div.appendChild( document.importNode( gsuiEQ.template.content, true ) );
+		return div.removeChild( div.querySelector( "*" ) );
+	},
+	_init: function() {
+		// ...
+		return document.getElementById( "gsuiEQ" );
+	}
 };
