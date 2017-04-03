684b1383fce3d217b45bafd08638d62070b44c25
Thomas Tortorini <thomastortorini@gmail.com>
2017-04-03 08:04:52 +0200

    gsuiSlider: now use of <template>

--- src/gsuiSlider/gsuiSlider.js
+++ src/gsuiSlider/gsuiSlider.js
@@ -1,43 +1,31 @@
 "use strict";
 
-function gsuiSlider( elRoot ) {
-	var inp = elRoot.querySelector( "input" );
+function gsuiSlider() {
+	var root = this._clone(),
+		inp = root.querySelector( "input" );
 
-	this.elRoot = elRoot;
-	this.elThumb = elRoot.querySelector( ".gsui-thumb" );
-	this.elLine = elRoot.querySelector( ".gsui-line" );
-	this.elLineColor = elRoot.querySelector( ".gsui-lineColor" );
-	this.input = inp;
-	this.elRoot.onwheel = this._wheel.bind( this );
-	this.elRoot.onmousedown = this._mousedown.bind( this );
-	this.options( {
-		min: inp.min || 0,
-		max: inp.max || 100,
-		step: inp.step || 1,
-		value: inp.value || 0,
-		startFrom: 0
-	} );
+	this.rootElement = root;
+	this._elInput = inp;
+	this._elThumb = root.querySelector( ".gsui-thumb" );
+	this._elLine = root.querySelector( ".gsui-line" );
+	this._elLineColor = root.querySelector( ".gsui-lineColor" );
+	root.onwheel = this._wheel.bind( this );
+	root.onmousedown = this._mousedown.bind( this );
+	this.options( { min: 0, max: 100,
+		step: 1, value: 50, startFrom: 0 } );
 }
-
-document.body.addEventListener( "mousemove", function( e ) {
-	gsuiSlider._sliderClicked && gsuiSlider._sliderClicked._mousemove( e );
-} );
-
-document.body.addEventListener( "mouseup", function( e ) {
-	gsuiSlider._sliderClicked && gsuiSlider._sliderClicked._mouseup( e );
-} );
 
 gsuiSlider.prototype = {
 	setValue: function( val ) {
-		var prval = this.input.value;
+		var prval = this._elInput.value;
 
-		this.input.value = val;
-		if ( this.input.value !== prval ) {
+		this._elInput.value = val;
+		if ( this._elInput.value !== prval ) {
 			this._updateVal();
 		}
 	},
 	options: function( obj ) {
-		var k, v, inp = this.input;
+		var k, v, inp = this._elInput;
 
 		for( k in obj ) {
 			v = this[ k ] = obj[ k ];
@@ -49,28 +37,44 @@ gsuiSlider.prototype = {
 		this._updateVal();
 	},
 	resize: function( w, h ) {
-		this.elRoot.style.width = w + "px";
-		this.elRoot.style.height = h + "px";
+		this.rootElement.style.width = w + "px";
+		this.rootElement.style.height = h + "px";
 		this._updateSize( w, h );
 	},
 	resized: function() {
-		var rc = this.elRoot.getBoundingClientRect();
+		var rc = this.rootElement.getBoundingClientRect();
 
 		this._updateSize( rc.width, rc.height );
 	},
 
 	// private:
+	_clone: function() {
+		var div = document.createElement( "div" );
+
+		gsuiSlider.template = gsuiSlider.template || this._init();
+		div.appendChild( document.importNode( gsuiSlider.template.content, true ) );
+		return div.removeChild( div.querySelector( "*" ) );
+	},
+	_init: function() {
+		document.body.addEventListener( "mousemove", function( e ) {
+			gsuiSlider._sliderClicked && gsuiSlider._sliderClicked._mousemove( e );
+		} );
+		document.body.addEventListener( "mouseup", function( e ) {
+			gsuiSlider._sliderClicked && gsuiSlider._sliderClicked._mouseup( e );
+		} );
+		return document.getElementById( "gsuiSlider" );
+	},
 	_updateVal: function() {
-		var inp = this.input,
+		var inp = this._elInput,
 			val = inp.value,
 			start = this.startFrom,
 			prc = ( val - inp.min ) / ( inp.max - inp.min ) * 100,
 			prcStart = ( start - inp.min ) / ( inp.max - inp.min ) * 100;
 
 		this._value = val;
-		this.elThumb.style.top = 100 - prc + "%";
-		this.elLineColor.style.top = ( start < val ? 100 - prc : 100 - prcStart ) + "%";
-		this.elLineColor.style.bottom = ( start < val ? prcStart : prc ) + "%";
+		this._elThumb.style.top = 100 - prc + "%";
+		this._elLineColor.style.top = ( start < val ? 100 - prc : 100 - prcStart ) + "%";
+		this._elLineColor.style.bottom = ( start < val ? prcStart : prc ) + "%";
 	},
 	_updateSize: function( w, h ) {
 		if ( w !== this.width || h !== this.height ) {
@@ -81,18 +85,18 @@ gsuiSlider.prototype = {
 			thumbMrgs % 2 && ++thumbMrgs;
 			this.width = w;
 			this.height = h;
-			this.elThumb.style.width =
-			this.elThumb.style.height = w - thumbMrgs + "px";
-			this.elThumb.style.marginLeft =
-			this.elThumb.style.marginTop = ( w - thumbMrgs ) / -2 + "px";
-			this.elLine.style.width = lineW + "px";
-			this.elLine.style.marginLeft = lineW / -2 + "px";
-			this.elLine.style.top =
-			this.elLine.style.bottom = w / 2 + "px";
+			this._elThumb.style.width =
+			this._elThumb.style.height = w - thumbMrgs + "px";
+			this._elThumb.style.marginLeft =
+			this._elThumb.style.marginTop = ( w - thumbMrgs ) / -2 + "px";
+			this._elLine.style.width = lineW + "px";
+			this._elLine.style.marginLeft = lineW / -2 + "px";
+			this._elLine.style.top =
+			this._elLine.style.bottom = w / 2 + "px";
 		}
 	},
 	_wheel: function( e ) {
-		var inp = this.input,
+		var inp = this._elInput,
 			dy = e.deltaY > 0 ? -1 : 1;
 
 		inp.value = +inp.value + inp.step * dy;
@@ -100,18 +104,18 @@ gsuiSlider.prototype = {
 	},
 	_mousedown: function( e ) {
 		gsuiSlider._sliderClicked = this;
-		this._rcLine = this.elLine.getBoundingClientRect();
-		this.elThumb.classList.add( "gsui-big" );
+		this._rcLine = this._elLine.getBoundingClientRect();
+		this._elThumb.classList.add( "gsui-big" );
 		this._mousemove( e );
 	},
 	_mouseup: function( e ) {
 		delete gsuiSlider._sliderClicked;
-		this.elThumb.classList.remove( "gsui-big" );
+		this._elThumb.classList.remove( "gsui-big" );
 	},
 	_mousemove: function( e ) {
 		var y = 1 - ( e.pageY - this._rcLine.top ) / ( this._rcLine.height - 1 ),
-			min = +this.input.min,
-			max = +this.input.max;
+			min = +this._elInput.min,
+			max = +this._elInput.max;
 
 		this.setValue( min + y * ( max - min ) );
 	}
