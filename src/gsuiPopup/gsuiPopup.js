8c2f77d49d3f6230049ce5aa3b3661b34656001c
Thomas Tortorini <thomastortorini@gmail.com>
2017-04-27 03:58:48 +0200

    gsuiPopup: no more need to `new` the unique instance

--- src/gsuiPopup/gsuiPopup.js
+++ src/gsuiPopup/gsuiPopup.js
@@ -1,54 +1,57 @@
 "use strict";
 
 function gsuiPopup( el ) {
-	this.elRoot = el;
-	this.elWindow = el.querySelector( ".gsuiPopup-window" );
-	this.elHeader = el.querySelector( ".gsuiPopup-window header" );
-	this.elMsg = el.querySelector( ".gsui-msg" );
-	this.elText = el.querySelector( ".gsui-text" );
-	this.elCancel = el.querySelector( ".gsui-cancel" );
-	this.elOk = el.querySelector( ".gsui-ok" );
-	this.elForm = el.querySelector( "form" );
+	var that = gsuiPopup;
+	
+	that.elRoot = el;
+	that.elWindow = el.querySelector( ".gsuiPopup-window" );
+	that.elHeader = el.querySelector( ".gsuiPopup-window header" );
+	that.elMsg = el.querySelector( ".gsui-msg" );
+	that.elText = el.querySelector( ".gsui-text" );
+	that.elCancel = el.querySelector( ".gsui-cancel" );
+	that.elOk = el.querySelector( ".gsui-ok" );
+	that.elForm = el.querySelector( "form" );
 
 	el.onclick =
-	this.elCancel.onclick = this._cancelClick.bind( this );
-	this.elForm.onsubmit = this._submit.bind( this );
-	this.elText.onkeypress =
-	this.elText.onkeydown =
-	this.elText.onkeyup =
-	this.elWindow.onclick = function( e ) {
+	that.elCancel.onclick = that._cancelClick;
+	that.elForm.onsubmit = that._submit;
+	that.elText.onkeypress =
+	that.elText.onkeydown =
+	that.elText.onkeyup =
+	that.elWindow.onclick = function( e ) {
 		e.stopPropagation();
 	};
 };
 
-gsuiPopup.prototype = {
+Object.assign( gsuiPopup, {
 	alert: function( title, msg ) {
-		return this._open( "alert", title, msg );
+		return gsuiPopup._open( "alert", title, msg );
 	},
 	confirm: function( title, msg ) {
-		return this._open( "confirm", title, msg );
+		return gsuiPopup._open( "confirm", title, msg );
 	},
 	prompt: function( title, msg, val ) {
-		return this._open( "prompt", title, msg, val );
+		return gsuiPopup._open( "prompt", title, msg, val );
 	},
 	close: function() {
-		if ( this.isOpen ) {
-			this.elCancel.click();
+		if ( gsuiPopup.isOpen ) {
+			gsuiPopup.elCancel.click();
 		}
 	},
 
 	// private:
 	_open: function( type, title, msg, value ) {
-		var that = this;
+		var that = gsuiPopup;
 
-		this.isOpen = true;
-		this.elHeader.textContent = title;
-		this.elMsg.innerHTML = msg;
-		this.elText.value = arguments.length > 3 ? value : "";
-		this.elWindow.dataset.gsuiType = this.type = type;
-		this.elWindow.classList.toggle( "gsui-nocancel", type !== "prompt" && type !== "confirm" );
-		this.elWindow.classList.toggle( "gsui-notext", type !== "prompt" );
-		this.elRoot.classList.add( "gsui-show" );
+		that.isOpen = true;
+		that.elHeader.textContent = title;
+		that.elMsg.innerHTML = msg;
+		that.elText.value = arguments.length > 3 ? value : "";
+		that.elWindow.dataset.gsuiType =
+		that.type = type;
+		that.elWindow.classList.toggle( "gsui-nocancel", type !== "prompt" && type !== "confirm" );
+		that.elWindow.classList.toggle( "gsui-notext", type !== "prompt" );
+		that.elRoot.classList.add( "gsui-show" );
 		setTimeout( function() {
 			if ( type === "prompt" ) {
 				that.elText.select();
@@ -65,14 +68,14 @@ gsuiPopup.prototype = {
 		} );
 	},
 	_cancelClick: function() {
-		this.resolve(
-			this.type === "confirm" ? false :
-			this.type === "prompt" ? null : undefined );
+		gsuiPopup.resolve(
+			gsuiPopup.type === "confirm" ? false :
+			gsuiPopup.type === "prompt" ? null : undefined );
 	},
 	_submit: function() {
-		this.resolve(
-			this.type === "confirm" ? true :
-			this.type === "prompt" ? this.elText.value : undefined );
+		gsuiPopup.resolve(
+			gsuiPopup.type === "confirm" ? true :
+			gsuiPopup.type === "prompt" ? gsuiPopup.elText.value : undefined );
 		return false;
 	}
-};
+} );
