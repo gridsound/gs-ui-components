e5f796e8f8a47746d324f2fa19917d0ad295dd1f
Thomas Tortorini <thomastortorini@gmail.com>
2017-05-01 02:50:40 +0200

    gsuiToggle: drop the group thing on the right click

--- src/gsuiToggle/gsuiToggle.js
+++ src/gsuiToggle/gsuiToggle.js
@@ -1,51 +1,49 @@
 "use strict";
 
-function gsuiToggle( element, data ) {
-	this.element = element;
-	this.data = data;
-	this.isOn = false;
-	this._circleClass = element.querySelector( ".gsui-circle" ).classList;
+function gsuiToggle() {
+	var root = this._clone(),
+		inp = root.querySelector( "input" );
 
-	element.oncontextmenu = function() { return false; };
-	element.onmousedown = this.__elementMousedown.bind( this );
+	this.rootElement = root;
+	this._circCL = root.querySelector( ".gsui-circle" ).classList;
+	this.checked = false;
+	root.oncontextmenu = function() { return false; };
+	root.onmousedown = this._mousedown.bind( this );
 }
 
 gsuiToggle.prototype = {
-	toggle: function( b ) {
-		if ( b !== this.isOn ) {
-			this.isOn = b;
-			this._circleClass.toggle( "gsui-on", b );
-			this.data.onchange && this.data.onchange( b );
-		}
+	check: function() {
+		this._set( true );
 	},
-	groupWith: function( tgg ) {
-		var group = this.group;
-
-		tgg.group = tgg.group || [ tgg ];
-		if ( tgg !== this && group !== tgg.group ) {
-			group && group.splice( group.indexOf( this ), 1 );
-			this.group = tgg.group;
-			tgg.group.push( this );
-		}
+	uncheck: function() {
+		this._set( false );
+	},
+	toggle: function( b ) {
+		this._set( argument.length ? !!b : !this.checked );
 	},
 
 	// private:
-	__elementMousedown: function( e ) {
-		if ( e.button === 0 ) {
-			this.toggle( !this.isOn );
-		} else if ( e.button === 2 ) {
-			if ( this.group ) {
-				var b = this.group.every( function( tgg ) {
-						return tgg === this || !tgg.isOn;
-					}, this );
+	_clone: function() {
+		var div = document.createElement( "div" );
 
-				this.group.forEach( function( tgg ) {
-					if ( tgg !== this ) {
-						tgg.toggle( b );
-					}
-				}, this );
+		gsuiToggle.template = gsuiToggle.template || document.getElementById( "gsuiToggle" );
+		div.appendChild( document.importNode( gsuiToggle.template.content, true ) );
+		return div.removeChild( div.querySelector( "*" ) );
+	},
+	_set: function( b, change ) {
+		if ( b !== this.checked ) {
+			this.checked = b;
+			this._circCL.toggle( "gsui-on", b );
+			if ( change && this.onchange ) {
+				this.onchange( b );
 			}
-			this.toggle( true );
+		}
+	},
+	_mousedown: function( e ) {
+		if ( e.button === 0 ) {
+			this._set( !this.checked, true );
+		} else if ( e.button === 2 && this.onmousedownright ) {
+			this.onmousedownright();
 		}
 	}
 };
