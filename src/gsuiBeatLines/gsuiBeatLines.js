8cf7ef951d858af82c0da80e26f254cd45f6b8b9
Thomas Tortorini <thomastortorini@gmail.com>
2017-05-05 08:08:58 +0200

    gsuiBeatLines: handle the time cursor

--- src/gsuiBeatLines/gsuiBeatLines.js
+++ src/gsuiBeatLines/gsuiBeatLines.js
@@ -5,17 +5,24 @@ function gsuiBeatLines( el ) {
 	el = el || document.createElementNS( "http://www.w3.org/2000/svg", "svg" );
 	el.setAttribute( "preserveAspectRatio", "none" );
 	el.classList.add( "gsuiBeatLines" );
-	this.setResolution( 256 );
+	this.elTime = this._newRect();
+	this.elTime.setAttribute( "class", "gsui-currentTime" );
 	this.offset = 0;
 	this.beatsPerMeasure =
 	this.stepsPerBeat = 4;
 	this.steps = [];
+	this.setCurrentTime( 0 );
+	this.setResolution( 256 );
 }
 
 gsuiBeatLines.prototype = {
-	setResolution( w ) {
+	setResolution: function( w ) {
 		this.width = w;
 		this.rootElement.setAttribute( "viewBox", "0 0 " + w + " 1" );
+	},
+	setCurrentTime: function( beat ) {
+		this.currentTime = beat;
+		this._timeUpdate();
 	},
 	draw: function() {
 		var rectClass,
@@ -35,11 +42,7 @@ gsuiBeatLines.prototype = {
 		++step;
 		em += stepEm;
 		while ( elSteps.length < stepsDuration ) {
-			elStep = document.createElementNS( "http://www.w3.org/2000/svg", "rect" );
-			elStep.setAttribute( "y", 0 );
-			elStep.setAttribute( "height", 1 );
-			this.rootElement.appendChild( elStep );
-			elSteps.push( elStep );
+			elSteps.push( this._newRect() );
 		}
 		for ( ; stepId < stepsDuration; ++stepId ) {
 			rectClass = "gsui-" + ( step % stepsMeasure ? step % stepsBeat ?
@@ -55,5 +58,21 @@ gsuiBeatLines.prototype = {
 		for ( ; stepId < elSteps.length; ++stepId ) {
 			elSteps[ stepId ].style.display = "none";
 		}
+		this._timeUpdate();
+	},
+
+	// private:
+	_newRect: function() {
+		var rc = document.createElementNS( "http://www.w3.org/2000/svg", "rect" );
+
+		rc.setAttribute( "y", 0 );
+		rc.setAttribute( "height", "1px" );
+		rc.setAttribute( "width", "1px" );
+		this.rootElement.prepend( rc );
+		return rc;
+	},
+	_timeUpdate: function() {
+		this.elTime.style.display = this.currentTime > this.offset ? "block" : "none";
+		this.elTime.setAttribute( "x", this.currentTime - this.offset + "em" );
 	}
 };
