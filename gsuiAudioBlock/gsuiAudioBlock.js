a699a8bf49df4a5263f3a8fda4ff1b610a4c29f1
Thomas Tortorini <thomastortorini@gmail.com>
2017-09-11 21:36:50 +0200

    gsuiAudioBlock: call onmousedown regardless of e.button

--- gsuiAudioBlock/gsuiAudioBlock.js
+++ gsuiAudioBlock/gsuiAudioBlock.js
@@ -121,9 +121,9 @@ gsuiAudioBlock.prototype = {
 	_evmdRoot( e ) {
 		if ( e.button === 0 ) {
 			this._isDragging = true;
-			if ( this.onmousedown && this.onmousedown( this, e ) !== false ) {
-				gsuiAudioBlock._focused = this;
-			}
+		}
+		if ( this.onmousedown && this.onmousedown( this, e ) !== false ) {
+			gsuiAudioBlock._focused = this;
 		}
 	},
 	_evmmBody( e ) {
