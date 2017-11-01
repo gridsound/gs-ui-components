45f20ee239f0f50d9cfd29e9f4eb71c64860a41d
Thomas Tortorini <thomastortorini@gmail.com>
2017-11-01 05:20:17 +0100

    gsuiPopup: .custom() + new design

--- gsuiPopup/gsuiPopup.js
+++ gsuiPopup/gsuiPopup.js
@@ -2,13 +2,31 @@
 
 window.gsuiPopup = {
 	alert( title, msg ) {
+		gsuiPopup._init();
+		gsuiPopup._emptyCnt();
 		return gsuiPopup._open( "alert", title, msg );
 	},
 	confirm( title, msg ) {
+		gsuiPopup._init();
+		gsuiPopup._emptyCnt();
 		return gsuiPopup._open( "confirm", title, msg );
 	},
 	prompt( title, msg, val ) {
+		gsuiPopup._init();
+		gsuiPopup._emptyCnt();
 		return gsuiPopup._open( "prompt", title, msg, val );
+	},
+	custom( title, elements, fnSubmit ) {
+		var elCnt;
+
+		gsuiPopup._init();
+		gsuiPopup._emptyCnt();
+		gsuiPopup._fnSubmit = fnSubmit;
+		elCnt = gsuiPopup.elCnt;
+		elements.length
+			? elCnt.append.apply( elCnt, elements )
+			: elCnt.append( elements );
+		return gsuiPopup._open( "custom", title );
 	},
 	close() {
 		if ( gsuiPopup.isOpen ) {
@@ -17,39 +35,52 @@ window.gsuiPopup = {
 	},
 
 	// private:
+	_emptyCnt() {
+		var elCnt = gsuiPopup.elCnt;
+
+		while ( elCnt.firstChild ) {
+			elCnt.firstChild.remove();
+		}
+	},
 	_init() {
-		var that = gsuiPopup,
-			el = document.getElementById( "gsuiPopup" );
+		var el, that = gsuiPopup;
 
-		that.elRoot = el;
-		that.elWindow = el.querySelector( "#gsuiPopup-window" );
-		that.elHeader = el.querySelector( "#gsuiPopup-window header" );
-		that.elMsg = el.querySelector( ".gsui-msg" );
-		that.elText = el.querySelector( ".gsui-text" );
-		that.elCancel = el.querySelector( ".gsui-cancel" );
-		that.elOk = el.querySelector( ".gsui-ok" );
-		that.elForm = el.querySelector( "form" );
+		if ( !that._ready ) {
+			el =
+			that.elRoot = document.getElementById( "gsuiPopup" );
+			that.elWindow = el.querySelector( "#gsuipp-window" );
+			that.elHeader = el.querySelector( "#gsuipp-head" );
+			that.elCnt = el.querySelector( "#gsuipp-cnt" );
+			that.elMsg = el.querySelector( "#gsuipp-msg" );
+			that.elText = el.querySelector( "#gsuipp-inpText" );
+			that.elCancel = el.querySelector( "#gsuipp-inpCancel" );
+			that.elOk = el.querySelector( "#gsuipp-inpOk" );
+			that.elForm = el.querySelector( "form" );
 
-		el.onclick =
-		that.elCancel.onclick = that._cancelClick;
-		that.elForm.onsubmit = that._submit;
-		that.elText.onkeypress =
-		that.elText.onkeydown =
-		that.elText.onkeyup =
-		that.elWindow.onclick = function( e ) {
-			e.stopPropagation();
-		};
-		that._ready = true;
+			el.onclick =
+			that.elCancel.onclick = that._cancelClick;
+			that.elForm.onsubmit = that._submit;
+			that.elText.onkeypress =
+			that.elText.onkeydown =
+			that.elText.onkeyup =
+			that.elWindow.onclick = function( e ) {
+				e.stopPropagation();
+			};
+			that.elWindow.onkeydown = function( e ) {
+				if ( e.keyCode === 27 ) {
+					that._cancelClick();
+				}
+				e.stopPropagation();
+			};
+			that._ready = true;
+		}
 	},
 	_open( type, title, msg, value ) {
 		var that = gsuiPopup;
 
-		if ( !that._ready ) {
-			that._init();
-		}
 		that.isOpen = true;
 		that.elHeader.textContent = title;
-		that.elMsg.innerHTML = msg;
+		that.elMsg.innerHTML = msg || "";
 		that.elText.value = arguments.length > 3 ? value : "";
 		that.elWindow.dataset.gsuiType =
 		that.type = type;
@@ -76,7 +107,10 @@ window.gsuiPopup = {
 			gsuiPopup.type === "confirm" ? false :
 			gsuiPopup.type === "prompt" ? null : undefined );
 	},
-	_submit() {
+	_submit( e ) {
+		if ( gsuiPopup.type === "custom" && gsuiPopup._fnSubmit ) {
+			gsuiPopup._fnSubmit( e );
+		}
 		gsuiPopup.resolve(
 			gsuiPopup.type === "confirm" ? true :
 			gsuiPopup.type === "prompt" ? gsuiPopup.elText.value : undefined );
