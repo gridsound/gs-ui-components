ec8d1a09b356974fef9c388b18a6912d92bdc2b7
Thomas Tortorini <thomastortorini@gmail.com>
2017-12-19 06:21:04 +0100

    gsuiDotline: root.mousedown to create a new dot

--- gsuiDotline/gsuiDotline.js
+++ gsuiDotline/gsuiDotline.js
@@ -4,6 +4,7 @@ function gsuiDotline() {
 	this.rootElement = document.createElement( "div" );
 	this.rootElement.className = "gsuiDotline";
 	this.rootElement.oncontextmenu = _ => false;
+	this.rootElement.onmousedown = this._mousedown.bind( this );
 	this._dots = {};
 	this._dotsId = 0;
 	this._nlDots = this.rootElement.getElementsByClassName( "gsuiDotline-dot" );
@@ -33,7 +34,7 @@ gsuiDotline.prototype = {
 		return this._dots;
 	},
 	resize() {
-		this._rootBCR = this.rootElement.getBoundingClientRect();
+		return this._rootBCR = this.rootElement.getBoundingClientRect();
 	},
 
 	// private:
@@ -49,6 +50,7 @@ gsuiDotline.prototype = {
 		element.onmouseup = this._mouseupDot.bind( this, dotId );
 		this._updateDot( dotId, x, y );
 		this.rootElement.append( element );
+		return dotId;
 	},
 	_updateDot( dotId, x, y ) {
 		var bcr = this._rootBCR,
@@ -65,18 +67,33 @@ gsuiDotline.prototype = {
 		delete this._dots[ dotId ];
 	},
 	_selectDot( dotId, b ) {
+		this._locked = b;
 		this._dots[ dotId ].element.classList.toggle( "gsuiDotline-dotSelected", b );
+		if ( b ) {
+			this._dots[ dotId ].element.setCapture( true );
+		}
 	},
 
 	// events:
+	_mousedown( e ) {
+		if ( e.button === 0 ) {
+			var h = this._height,
+				bcr = this.resize();
+
+			this._selectDot(
+				this._createDot(
+					( e.pageX - bcr.left ) / bcr.width * this._width,
+					h - ( e.pageY - bcr.top ) / bcr.height * h
+				), true );
+		}
+	},
 	_mousedownDot( dotId, e ) {
+		e.stopPropagation();
 		if ( e.button === 2 ) {
 			this._deleteDot( dotId );
 		} else if ( e.button === 0 ) {
 			this.resize();
 			this._selectDot( dotId, true );
-			this._locked = true;
-			this._dots[ dotId ].element.setCapture( true );
 		}
 	},
 	_mousemoveDot( dotId, e ) {
@@ -90,7 +107,6 @@ gsuiDotline.prototype = {
 		}
 	},
 	_mouseupDot( dotId ) {
-		delete this._locked;
 		this._selectDot( dotId, false );
 	}
 };
