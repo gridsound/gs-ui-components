680a5e9073440b58a22122daa27c9e06a2c2eb95
Thomas Tortorini <thomastortorini@gmail.com>
2018-03-26 15:25:26 +0200

    gsuiTrack: use Proxy

--- gsuiTrack/gsuiTrack.js
+++ gsuiTrack/gsuiTrack.js
@@ -1,67 +1,52 @@
 "use strict";
 
-function gsuiTrack() {
-	var root = this._clone();
+class gsuiTrack {
+	constructor() {
+		const root = gsuiTrack.template.cloneNode( true );
 
-	this.rootElement = root;
-	this.rowElement = root.querySelector( ".gsui-row" );
-	this.rowElement.remove();
-	this.uiToggle = new gsuiToggle();
-	this.uiSpan = new gsuiSpanEditable();
-	root.append( this.uiToggle.rootElement );
-	root.append( this.uiSpan.rootElement );
+		this.rootElement = root;
+		this.rowElement = root.querySelector( ".gsui-row" );
+		this.rowElement.remove();
+		this.uiToggle = new gsuiToggle();
+		this.uiSpan = new gsuiSpanEditable();
+		root.append( this.uiToggle.rootElement, this.uiSpan.rootElement );
 
-	this.data = {};
-	this.uiToggle.onchange = this._evocToggle.bind( this );
-	this.uiSpan.onchange = this._evocSpan.bind( this );
-	this.toggle( true );
-}
+		this.onchange = () => {};
+		this.uiSpan.onchange = name => this.onchange( { name } );
+		this.uiToggle.onchange = toggle => this.onchange( { toggle } );
+		this.data = new Proxy( Object.seal( {
+			name: "",
+			toggle: true
+		} ), { set: this._setProp.bind( this ) } );
+		this.data.toggle = true;
+	}
 
-gsuiTrack.prototype = {
 	remove() {
 		this.rootElement.remove();
 		this.rowElement.remove();
-	},
-	change( obj ) {
-		"toggle" in obj && this.toggle( obj.toggle );
-		"name" in obj && this.name( obj.name );
-	},
-	toggle( b ) {
-		this._evocToggle( b, false );
-	},
-	name( n ) {
-		this._evocSpan( n, false );
-	},
+	}
 	setPlaceholder( p ) {
 		this.uiSpan.setPlaceholder( p );
-	},
+	}
 
 	// private:
-	_clone() {
-		var div = document.createElement( "div" );
-
-		gsuiTrack.template = gsuiTrack.template || document.getElementById( "gsuiTrack" );
-		div.appendChild( document.importNode( gsuiTrack.template.content, true ) );
-		return div.removeChild( div.querySelector( "*" ) );
-	},
-
-	// events:
-	_evocToggle( b, isUserAction ) {
+	_setProp( tar, prop, val ) {
+		if ( prop in tar ) {
+			tar[ prop ] = val;
+			this[ prop === "name" ? "_setName" : "_setToggle" ]( val );
+		}
+		return true;
+	}
+	_setName( name ) {
+		this.uiSpan.setValue( name );
+	}
+	_setToggle( b ) {
 		this.rootElement.classList.toggle( "gsui-mute", !b );
 		this.rowElement.classList.toggle( "gsui-mute", !b );
-		if ( isUserAction === false ) {
-			this.uiToggle.toggle( b );
-		} else {
-			this.onchange( { toggle: b } );
-		}
-		this.data.toggle = b;
-	},
-	_evocSpan( name, isUserAction ) {
-		if ( isUserAction === false ) {
-			this.uiSpan.setValue( name );
-		} else {
-			this.onchange( { name: name } );
-		}
-		this.data.name = name;
+		this.uiToggle.toggle( b );
 	}
-};
+}
+
+gsuiTrack.template = document.querySelector( "#gsuiTrack-template" );
+gsuiTrack.template.remove();
+gsuiTrack.template.removeAttribute( "id" );
