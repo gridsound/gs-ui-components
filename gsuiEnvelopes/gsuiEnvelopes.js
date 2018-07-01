ceeb1fec65ce28fc9fe6bdf233b72d51c883e686
Thomas Tortorini <thomastortorini@gmail.com>
2018-07-01 23:08:52 +0200

    gsuiEnvelopes: CSS/HTML + choose env

--- gsuiEnvelopes/gsuiEnvelopes.js
+++ gsuiEnvelopes/gsuiEnvelopes.js
@@ -3,6 +3,8 @@
 class gsuiEnvelopes {
 	constructor() {
 		const root = gsuiEnvelopes.template.cloneNode( true ),
+			btnEnvs = root.querySelectorAll( ".gsuiEnvelopes-envBtn" ),
+			btnDotmode = root.querySelector( ".gsuiEnvelopes-dotLinkBtn" ),
 			dotlineWraps = root.querySelectorAll( ".gsuiEnvelopes-line" ),
 			dotline0 = new gsuiDotline(),
 			dotline1 = new gsuiDotline(),
@@ -16,14 +18,17 @@ class gsuiEnvelopes {
 
 		this.rootElement = root;
 		this.store = {};
-		this._dotlineAttack = dotline0;
-		this._dotlineRelease = dotline1;
+		this._dotlineAtt = dotline0;
+		this._dotlineRel = dotline1;
 		dotline0.options( dotlineOpts );
 		dotline1.options( dotlineOpts );
 		dotline0.lastDotLinkedTo( 1 );
 		dotline1.firstDotLinkedTo( 1 );
 		dotlineWraps[ 0 ].append( dotline0.rootElement );
 		dotlineWraps[ 1 ].append( dotline1.rootElement );
+		btnEnvs.forEach( btn => btn.onclick = this.switchEnv.bind( this, btn.dataset.env ) );
+		btnDotmode.onclick = this._onclickDotmode.bind( this );
+		this.switchEnv( "gain" );
 	}
 
 	remove() {
@@ -32,11 +37,23 @@ class gsuiEnvelopes {
 	}
 	attached() {
 		this._attached = true;
-		this._dotlineAttack.attached();
-		this._dotlineRelease.attached();
+		this._dotlineAtt.attached();
+		this._dotlineRel.attached();
 	}
 	empty() {
+	}
+	switchEnv( envName ) {
+		this.rootElement.dataset.env = envName;
+		return false;
+	}
 
+	// private:
+	_onclickDotmode() {
+		const b = this.rootElement.classList.toggle( "gsuiEnvelopes-dotLinked" ) ? "linked" : "free";
+
+		this._dotlineAtt.dotsMoveMode( b );
+		this._dotlineRel.dotsMoveMode( b );
+		return false;
 	}
 }
 
