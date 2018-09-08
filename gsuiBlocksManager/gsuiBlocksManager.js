d26f63e490aa798eed6dd2be765f37ed18371660
Thomas Tortorini <thomastortorini@gmail.com>
2018-09-09 00:49:57 +0200

    gsuiBlocksManager: fix, add a check before __onmousedown

--- gsuiBlocksManager/gsuiBlocksManager.js
+++ gsuiBlocksManager/gsuiBlocksManager.js
@@ -366,80 +366,81 @@ class gsuiBlocksManager {
 		}
 	}
 	__mousedown( e ) {
-		const blc = this.__getBlc( e.currentTarget );
+		if ( !gsuiBlocksManager._focused ) {
+			const blc = this.__getBlc( e.currentTarget );
 
-		window.getSelection().removeAllRanges();
-		this.__mdBlc = blc;
-		if ( e.button === 2 ) {
-			this.__mmFn = this.__mousemove_deletion;
-			this.__status = "deleting";
-			if ( blc ) {
-				this.block_deleted( blc, true );
-				this.__blcsEditing.set( +blc.dataset.id, blc );
-			}
-		} else if ( e.button === 0 ) {
-			this.__mdPageX = e.pageX;
-			this.__mdPageY = e.pageY;
-			this.__mdWhen = this.__getWhenByPageX( e.pageX );
-			this.__beatSnap = this.__getBeatSnap();
-			if ( e.shiftKey ) {
-				this.__mmFn = this.__mousemove_selection1;
-				this.__status = "selecting-1";
-				this.__mdRowInd = this.__getRowIndexByPageY( e.pageY );
-			} else if ( blc ) {
-				const data = this._getData(),
-					blcsEditing = this.__fillBlcsMap( blc );
+			gsuiBlocksManager._focused = this;
+			window.getSelection().removeAllRanges();
+			this.__mdBlc = blc;
+			if ( e.button === 2 ) {
+				this.__mmFn = this.__mousemove_deletion;
+				this.__status = "deleting";
+				if ( blc ) {
+					this.block_deleted( blc, true );
+					this.__blcsEditing.set( +blc.dataset.id, blc );
+				}
+			} else if ( e.button === 0 ) {
+				this.__mdPageX = e.pageX;
+				this.__mdPageY = e.pageY;
+				this.__mdWhen = this.__getWhenByPageX( e.pageX );
+				this.__beatSnap = this.__getBeatSnap();
+				if ( e.shiftKey ) {
+					this.__mmFn = this.__mousemove_selection1;
+					this.__status = "selecting-1";
+					this.__mdRowInd = this.__getRowIndexByPageY( e.pageY );
+				} else if ( blc ) {
+					const data = this._getData(),
+						blcsEditing = this.__fillBlcsMap( blc );
 
-				blc.classList.add( "gsui-hover" );
-				if ( e.target.classList.contains( "gsuiBlocksManager-block-crop" ) ) {
-					this.__mmFn = this.__mousemove_crop;
-					e.target.classList.add( "gsui-hover" );
-					if ( e.target.classList.contains( "gsuiBlocksManager-block-cropA" ) ) {
-						this.__status = "cropping-a";
-						this.__valueAMin =
-						this.__valueAMax = Infinity;
+					blc.classList.add( "gsui-hover" );
+					if ( e.target.classList.contains( "gsuiBlocksManager-block-crop" ) ) {
+						this.__mmFn = this.__mousemove_crop;
+						e.target.classList.add( "gsui-hover" );
+						if ( e.target.classList.contains( "gsuiBlocksManager-block-cropA" ) ) {
+							this.__status = "cropping-a";
+							this.__valueAMin =
+							this.__valueAMax = Infinity;
+							blcsEditing.forEach( ( blc, id ) => {
+								const dat = data[ id ];
+
+								this.__valueAMin = Math.min( this.__valueAMin, dat.offset );
+								this.__valueAMax = Math.min( this.__valueAMax, dat.duration );
+							} );
+							this.__valueAMin *= -1;
+							this.__valueAMax = Math.max( 0, this.__valueAMax - this.__beatSnap );
+						} else {
+							this.__status = "cropping-b";
+							this.__valueAMin =
+							this.__valueAMax = Infinity;
+							blcsEditing.forEach( ( blc, id ) => (
+								this.__valueAMin = Math.min( this.__valueAMin, data[ id ].duration )
+							) );
+							this.__valueAMin = -Math.max( 0, this.__valueAMin - this.__beatSnap );
+						}
+					} else {
+						this.__mmFn = this.__mousemove_move;
+						this.__status = "moving";
+						this.__mdRowInd = this.__getRowIndexByPageY( e.pageY );
 						blcsEditing.forEach( ( blc, id ) => {
-							const dat = data[ id ];
+							const valB = this.__getRowIndexByRow( blc.parentNode.parentNode );
 
-							this.__valueAMin = Math.min( this.__valueAMin, dat.offset );
-							this.__valueAMax = Math.min( this.__valueAMax, dat.duration );
+							this.__valueAMin = Math.min( this.__valueAMin, data[ id ].when );
+							this.__valueBMin = Math.min( this.__valueBMin, valB );
+							this.__valueBMax = Math.max( this.__valueBMax, valB );
 						} );
 						this.__valueAMin *= -1;
-						this.__valueAMax = Math.max( 0, this.__valueAMax - this.__beatSnap );
-					} else {
-						this.__status = "cropping-b";
-						this.__valueAMin =
-						this.__valueAMax = Infinity;
-						blcsEditing.forEach( ( blc, id ) => (
-							this.__valueAMin = Math.min( this.__valueAMin, data[ id ].duration )
-						) );
-						this.__valueAMin = -Math.max( 0, this.__valueAMin - this.__beatSnap );
+						this.__valueBMin *= -1;
+						this.__valueBMax = this.__rows.length - 1 - this.__valueBMax;
 					}
-				} else {
-					this.__mmFn = this.__mousemove_move;
-					this.__status = "moving";
-					this.__mdRowInd = this.__getRowIndexByPageY( e.pageY );
-					blcsEditing.forEach( ( blc, id ) => {
-						const valB = this.__getRowIndexByRow( blc.parentNode.parentNode );
-
-						this.__valueAMin = Math.min( this.__valueAMin, data[ id ].when );
-						this.__valueBMin = Math.min( this.__valueBMin, valB );
-						this.__valueBMax = Math.max( this.__valueBMax, valB );
-					} );
-					this.__valueAMin *= -1;
-					this.__valueBMin *= -1;
-					this.__valueBMax = this.__rows.length - 1 - this.__valueBMax;
 				}
 			}
 		}
-		gsuiBlocksManager._focused = this;
 	}
 	__mouseup() {
 		const blcsEditing = this.__blcsEditing,
 			mdBlc = this.__mdBlc;
 
 		switch ( this.__status ) {
-			default: return;
 			case "deleting":
 				if ( blcsEditing.size || this.__blcsSelected.size ) {
 					this.blcsManagerCallback( "deleting", blcsEditing );
