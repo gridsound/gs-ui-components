851424ebe61635ab2ff73daa3adc505fb0dbdb63
Thomas Tortorini <thomastortorini@gmail.com>
2019-03-29 08:31:52 +0100

    gsuiAnalyser: draw pixels directly

--- gsuiAnalyser/gsuiAnalyser.js
+++ gsuiAnalyser/gsuiAnalyser.js
@@ -4,6 +4,7 @@ class gsuiAnalyser {
 	setCanvas( canvas ) {
 		this.rootElement = canvas;
 		this._ctx = canvas.getContext( "2d" );
+		this._scaleToData = true;
 	}
 	clear() {
 		this._ctx.clearRect( 0, 0, this.rootElement.width, this.rootElement.height );
@@ -16,34 +17,41 @@ class gsuiAnalyser {
 		cnv.height = Math.max( h, 2 );
 		this._ctx.putImageData( img, 0, 0 );
 	}
+	scaleToData( b ) {
+		this._scaleToData = b;
+	}
 	draw( ldata, rdata ) {
-		const datalen = ldata.length,
-			w = this.rootElement.width / 2;
+		const w = ldata.length * 2;
 
-		if ( this._datalen !== datalen || this._width !== w ) {
-			this._width = w;
-			this._datalen = datalen;
-			this._widths = gsuiSpectrum.calcFreqSizes( datalen, w );
-		}
 		this._moveImage();
+		if ( this._scaleToData && w !== this.rootElement.width ) {
+			this.rootElement.width = w;
+		}
 		this._draw( ldata, rdata );
 	}
 
 	// private:
-	_draw( ldata, rdata ) {
-		const ctx = this._ctx;
-
-		ctx.save();
-			ctx.translate( this._width, 0 );
-			gsuiSpectrum.draw( ctx, rdata, this._widths );
-			ctx.scale( -1, 1 );
-			gsuiSpectrum.draw( ctx, ldata, this._widths );
-		ctx.restore();
-	}
 	_moveImage() {
 		const cnv = this.rootElement,
 			img = this._ctx.getImageData( 0, 0, cnv.width, cnv.height - 1 );
 
 		this._ctx.putImageData( img, 0, 1 );
+	}
+	_draw( ldata, rdata ) {
+		const ctx = this._ctx,
+			w2 = ctx.canvas.width / 2,
+			len = Math.min( w2, ldata.length ),
+			imgL = gsuiSpectrum.draw( ctx, ldata ),
+			imgR = gsuiSpectrum.draw( ctx, rdata ),
+			imgLflip = ctx.createImageData( len, 1 );
+
+		for ( let x = 0, x2 = len - 1; x < len; ++x, --x2 ) {
+			imgLflip.data[ x * 4     ] = imgL.data[ x2 * 4     ];
+			imgLflip.data[ x * 4 + 1 ] = imgL.data[ x2 * 4 + 1 ];
+			imgLflip.data[ x * 4 + 2 ] = imgL.data[ x2 * 4 + 2 ];
+			imgLflip.data[ x * 4 + 3 ] = imgL.data[ x2 * 4 + 3 ];
+		}
+		ctx.putImageData( imgLflip, 0, 0 );
+		ctx.putImageData( imgR, w2, 0 );
 	}
 }
