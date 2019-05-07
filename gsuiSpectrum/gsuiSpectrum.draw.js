972a379330daaee3ab8ae75625cb9cb0b23054bc
Thomas Tortorini <thomastortorini@gmail.com>
2019-05-07 06:20:23 +0200

    gsuiSpectrum: simplify the code and coding style

--- gsuiSpectrum/gsuiSpectrum.draw.js
+++ gsuiSpectrum/gsuiSpectrum.draw.js
@@ -2,49 +2,50 @@
 
 gsuiSpectrum.draw = function( ctx, data ) {
 	const datalen = data.length,
-		img = ctx.createImageData( datalen, 1 );
+		img = ctx.createImageData( datalen, 1 ),
+		imgData = img.data;
 
-	for ( let i = 0, x = 0; i < datalen; ++i, x += 4 ) {
-		let r, g, b,
+	for ( let i = 0; i < datalen; ++i ) {
+		const x = i * 4,
 			datum = 1 - Math.cos( data[ i ] / 255 * Math.PI / 2 );
 
 		if ( datum < .05 ) {
-			r = 4 + 10 * datum;
-			g = 4 + 10 * datum;
-			b = 5 + 20 * datum;
+			imgData[ x     ] = 4 + 10 * datum | 0;
+			imgData[ x + 1 ] = 4 + 10 * datum | 0;
+			imgData[ x + 2 ] = 5 + 20 * datum | 0;
 		} else {
-			let col, colId;
+			const col = gsuiSpectrum.colors[ gsuiSpectrum._getColorId( datum ) ],
+				datumCut = datum / col[ 3 ];
 
-			     if ( datum < .08 ) { colId = 0; datum /= .08; }
-			else if ( datum < .15 ) { colId = 1; datum /= .15; }
-			else if ( datum < .17 ) { colId = 2; datum /= .17; }
-			else if ( datum < .25 ) { colId = 3; datum /= .25; }
-			else if ( datum < .3  ) { colId = 4; datum /= .3; }
-			else if ( datum < .4  ) { colId = 5; datum /= .4; }
-			else if ( datum < .6  ) { colId = 6; datum /= .6; }
-			else if ( datum < .8  ) { colId = 7; datum /= .8; }
-			else                    { colId = 8; }
-			col = gsuiSpectrum.colors[ colId ];
-			r = col[ 0 ] * datum;
-			g = col[ 1 ] * datum;
-			b = col[ 2 ] * datum;
+			imgData[ x     ] = col[ 0 ] * datumCut | 0;
+			imgData[ x + 1 ] = col[ 1 ] * datumCut | 0;
+			imgData[ x + 2 ] = col[ 2 ] * datumCut | 0;
 		}
-		img.data[ x     ] = ~~r;
-		img.data[ x + 1 ] = ~~g;
-		img.data[ x + 2 ] = ~~b;
-		img.data[ x + 3 ] = 255;
+		imgData[ x + 3 ] = 255;
 	}
 	return img;
 };
 
+gsuiSpectrum._getColorId = function( datum ) {
+	     if ( datum < .08 ) { return 0; }
+	else if ( datum < .15 ) { return 1; }
+	else if ( datum < .17 ) { return 2; }
+	else if ( datum < .25 ) { return 3; }
+	else if ( datum < .3  ) { return 4; }
+	else if ( datum < .4  ) { return 5; }
+	else if ( datum < .6  ) { return 6; }
+	else if ( datum < .8  ) { return 7; }
+	return 8;
+};
+
 gsuiSpectrum.colors = [
-	[   5,   2,  20 ], // 0
-	[   8,   5,  30 ], // 1
-	[  15,   7,  50 ], // 2
-	[  75,   7,  35 ],   // 3
-	[  80,   0,   0 ],   // 4
-	[ 180,   0,   0 ],   // 5
-	[ 200,  25,  10 ], // 6
-	[ 200, 128,  10 ], // 7
-	[ 200, 200,  20 ], // 8
+	[   5,   2,  20, .08 ], // 0
+	[   8,   5,  30, .15 ], // 1
+	[  15,   7,  50, .17 ], // 2
+	[  75,   7,  35, .25 ],   // 3
+	[  80,   0,   0, .3  ],   // 4
+	[ 180,   0,   0, .4  ],   // 5
+	[ 200,  25,  10, .6  ], // 6
+	[ 200, 128,  10, .8  ], // 7
+	[ 200, 200,  20, 1   ], // 8
 ];
