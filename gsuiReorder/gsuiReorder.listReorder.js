95f68c312db3227d5207115992e5e4cf09f61bf9
Thomas Tortorini <thomastortorini@gmail.com>
2019-08-10 23:54:41 +0200

    gsuiReorder.listReorder: fix

--- gsuiReorder/gsuiReorder.listReorder.js
+++ gsuiReorder/gsuiReorder.listReorder.js
@@ -1,7 +1,9 @@
 "use strict";
 
 gsuiReorder.listReorder = list => {
-	const arr = Array.from( list.children ).sort( ( a, b ) => +a.dataset.order - +b.dataset.order );
+	const arr = Array.prototype
+			.filter.call( list.children, el => "order" in el.dataset )
+			.sort( ( a, b ) => +a.dataset.order - +b.dataset.order );
 
 	list.append.apply( list, arr );
 };
