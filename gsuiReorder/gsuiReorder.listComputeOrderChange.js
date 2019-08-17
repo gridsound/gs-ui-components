c0191d17cff9a5653d546c7fb00131e452c02b23
Thomas Tortorini <thomastortorini@gmail.com>
2019-08-17 04:11:24 +0200

    gsuiReorder.listComputeOrderChange: fix

--- gsuiReorder/gsuiReorder.listComputeOrderChange.js
+++ gsuiReorder/gsuiReorder.listComputeOrderChange.js
@@ -4,9 +4,17 @@ gsuiReorder.listComputeOrderChange = ( list, obj ) => {
 	let i = 0;
 
 	Array.prototype.forEach.call( list.children, el => {
-		if ( "id" in el.dataset && "order" in el.dataset ) {
-			if ( +el.dataset.order !== i ) {
-				obj[ el.dataset.id ] = { order: i };
+		const dt = el.dataset;
+
+		if ( "id" in dt && "order" in dt ) {
+			if ( +dt.order !== i ) {
+				const objId = obj[ dt.id ];
+
+				if ( objId ) {
+					objId.order = i;
+				} else if ( !( dt.id in obj ) ) {
+					obj[ dt.id ] = { order: i };
+				}
 			}
 			++i;
 		}
