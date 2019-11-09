a6aad34a3d8ad505268070866b94387e890af9e6
Thomas Tortorini <thomastortorini@gmail.com>
2019-11-09 04:25:58 +0100

    gsuiBeatlines: dynamic contrast with zoom

--- gsuiBeatlines/gsuiBeatlines.js
+++ gsuiBeatlines/gsuiBeatlines.js
@@ -26,26 +26,29 @@ class gsuiBeatlines {
 	}
 	render() {
 		const el = this.rootElement,
+			alpha = Math.min( this._pxPerBeat / 32, 1 ),
 			stepPx = this._viewBox / this._beatsPerMeasure / this._stepsPerBeat,
-			measureSteps = this._stepsPerBeat * this._beatsPerMeasure,
-			measureColor = "rgb(0,0,0)",
-			beatColor = "rgba(0,0,0,.4)",
-			stepColor = "rgba(0,0,0,.1)",
-			steps = [ `<rect x='0' y='0' height='1px' width='1px' fill='${ measureColor }'/>` ];
+			mesrSteps = this._stepsPerBeat * this._beatsPerMeasure,
+			mesrColor = `rgba(0,0,0,${ 1 * alpha })`,
+			beatColor = `rgba(0,0,0,${ .5 * alpha })`,
+			stepColor = `rgba(0,0,0,${ .2 * alpha })`,
+			steps = [ `<rect x='0' y='0' height='1px' width='1px' fill='${ mesrColor }'/>` ];
 
-		for ( let step = 1; step < measureSteps; ++step ) {
+		for ( let step = 1; step < mesrSteps; ++step ) {
 			steps.push( `<rect height='1px' width='1px' y='0' x='${
 				stepPx + stepPx * ( step - 1 ) - .5
 			}' fill='${
 				step % this._stepsPerBeat ? stepColor : beatColor
 			}'/>` );
 		}
-		steps.push( `<rect y='0' height='1px' width='1px' fill='${ measureColor }' x='${
-			stepPx + stepPx * ( measureSteps - 1 ) - .5 }'/>` );
+		steps.push( `<rect y='0' height='1px' width='1px' fill='${ mesrColor }' x='${
+			stepPx + stepPx * ( mesrSteps - 1 ) - .5 }'/>` );
 		el.style.backgroundImage = `url("${ encodeURI(
 			"data:image/svg+xml,<svg preserveAspectRatio='none' xmlns='http://www.w3.org/2000/svg' " +
 			`viewBox='0 0 ${ this._viewBox } 1'>${ steps.join( " " ) }</svg>`
-			) }")`;
+		) }")`;
 		el.style.backgroundSize = `${ this._viewBox }px 1px`;
 	}
 }
+
+Object.freeze( gsuiBeatlines );
