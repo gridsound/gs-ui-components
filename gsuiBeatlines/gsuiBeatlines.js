2ac1d85cfff7b14c6f5fd7e1f0d17206e7fe3403
Thomas Tortorini <thomastortorini@gmail.com>
2020-01-16 17:12:37 +0100

    gsuiBeatlines: beautify the code

--- gsuiBeatlines/gsuiBeatlines.js
+++ gsuiBeatlines/gsuiBeatlines.js
@@ -5,18 +5,19 @@ class gsuiBeatlines {
 		this.rootElement = el;
 		this._beatsPerMeasure =
 		this._stepsPerBeat = 4;
-		this._pxPerBeat = 32;
-		this._width = this._pxPerBeat * this._beatsPerMeasure;
+		this._width =
+		this._pxPerBeat = 0;
 		Object.seal( this );
 
 		el.classList.add( "gsuiBeatlines" );
 		el.style.backgroundAttachment = "local";
+		this.pxPerBeat( 32 );
 	}
 
 	pxPerBeat( pxBeat ) {
 		this._pxPerBeat = pxBeat;
-		this._width = pxBeat * this._beatsPerMeasure;
-		this.rootElement.style.backgroundSize = `${ this._width }px 1px`;
+		this._updateWidth();
+		this._updateBGSize();
 	}
 	getBeatsPerMeasure() {
 		return this._beatsPerMeasure;
@@ -27,32 +28,45 @@ class gsuiBeatlines {
 	timeSignature( a, b ) {
 		this._beatsPerMeasure = Math.max( 1, ~~a );
 		this._stepsPerBeat = Math.max( 1, ~~b );
-		this._width = this._pxPerBeat * this._beatsPerMeasure;
+		this._updateWidth();
 		this.render();
 	}
 	render() {
-		const el = this.rootElement,
-			alpha = Math.min( this._pxPerBeat / 32, 1 ),
+		const alpha = Math.min( this._pxPerBeat / 32, 1 ),
 			stepPx = this._width / this._beatsPerMeasure / this._stepsPerBeat,
-			mesrSteps = this._stepsPerBeat * this._beatsPerMeasure,
+			stepsPerMeasure = this._stepsPerBeat * this._beatsPerMeasure,
 			mesrColor = `rgba(0,0,0,${ 1 * alpha })`,
 			beatColor = `rgba(0,0,0,${ .5 * alpha })`,
 			stepColor = `rgba(0,0,0,${ .2 * alpha })`,
-			steps = [ `<rect x='0' y='0' height='1px' width='1.25px' fill='${ mesrColor }'/>` ];
+			steps = [ this._createRect( 0, 1.25, mesrColor ) ];
 
-		for ( let step = 1; step < mesrSteps; ++step ) {
+		for ( let step = 1; step < stepsPerMeasure; ++step ) {
 			const x = stepPx + stepPx * ( step - 1 ) - .5,
 				col = step % this._stepsPerBeat ? stepColor : beatColor;
 
-			steps.push( `<rect height='1px' width='1px' y='0' x='${ x }' fill='${ col }'/>` );
+			steps.push( this._createRect( x, 1, col ) );
 		}
-		steps.push( `<rect y='0' height='1px' width='1.25px' fill='${ mesrColor }' x='${
-			stepPx + stepPx * ( mesrSteps - 1 ) - .5 }'/>` );
-		el.style.backgroundImage = `url("${ encodeURI(
+		steps.push( this._createRect( stepPx + stepPx * ( stepsPerMeasure - 1 ) - .5, 1.25, mesrColor ) );
+		this._updateBGImage( steps );
+		this._updateBGSize();
+	}
+
+	// private:
+	// .........................................................................
+	_createRect( x, w, col ) {
+		return `<rect x='${ x }' y='0' height='1' width='${ w }' fill='${ col }'/>`;
+	}
+	_updateWidth() {
+		this._width = this._pxPerBeat * this._beatsPerMeasure;
+	}
+	_updateBGImage( steps ) {
+		this.rootElement.style.backgroundImage = `url("${ encodeURI(
 			"data:image/svg+xml,<svg preserveAspectRatio='none' xmlns='http://www.w3.org/2000/svg' " +
 			`viewBox='0 0 ${ this._width } 1'>${ steps.join( " " ) }</svg>`
 		) }")`;
-		el.style.backgroundSize = `${ this._width }px 1px`;
+	}
+	_updateBGSize() {
+		this.rootElement.style.backgroundSize = `${ this._width }px 1px`;
 	}
 }
 
