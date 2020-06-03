0019aa36a46886641e4a914c714516f10a63b05e
Thomas Tortorini <thomastortorini@gmail.com>
2020-06-03 09:15:34 +0200

    gsuiMixer: stateless

--- gsuiMixer/gsuiMixer.js
+++ gsuiMixer/gsuiMixer.js
@@ -1,44 +1,24 @@
 "use strict";
 
 class gsuiMixer {
-	constructor() {
+	constructor( opt ) {
 		const root = gsuiMixer.template.cloneNode( true ),
-			addBtn = root.querySelector( ".gsuiMixer-addChan" ),
-			gsdata = new GSDataMixer( {
-				actionCallback: ( obj, msg ) => this.onchange( obj, msg ),
-				dataCallbacks: {
-					addChan: this._addChan.bind( this ),
-					removeChan: this._removeChan.bind( this ),
-					redirectChan: this._updateChanConnections.bind( this ),
-					toggleChan: ( id, b ) => this._chans[ id ].root.classList.toggle( "gsuiMixerChannel-muted", !b ),
-					changePanChan: ( id, val ) => this._chans[ id ].pan.setValue( val ),
-					changeGainChan: ( id, val ) => this._chans[ id ].gain.setValue( val ),
-					changeOrderChan: ( id, val ) => this._chans[ id ].root.dataset.order = val,
-					renameChan: ( id, val ) => {
-						this._chans[ id ].name.textContent = val;
-						this.onupdateChan( id, "name", val );
-					},
-				},
-			} );
+			addBtn = root.querySelector( ".gsuiMixer-addChan" );
 
 		this.rootElement = root;
-		this.gsdata = gsdata;
 		this._pmain = root.querySelector( ".gsuiMixer-panMain" );
 		this._pchans = root.querySelector( ".gsuiMixer-panChannels" );
 		this._chans = {};
 		this._chanSelected = null;
-		this.oninput =
-		this.onchange =
-		this.onaddChan =
-		this.ondeleteChan =
-		this.onupdateChan =
-		this.onselectChan = GSUtils.noop;
-		this._analyserW = 32;
-		this._analyserH = 10;
+		this.oninput = opt.oninput;
+		this.onchange = opt.onchange;
+		this.onselectChan = opt.onselectChan;
+		this._analyserW =
+		this._analyserH = 0;
 		this._attached = false;
 		Object.seal( this );
 
-		addBtn.onclick = gsdata.callAction.bind( gsdata, "addChan" );
+		addBtn.onclick = () => this.onchange( "addChannel" );
 		new gsuiReorder( {
 			rootElement: root,
 			direction: "row",
@@ -47,110 +27,59 @@ class gsuiMixer {
 			handleSelector: ".gsuiMixerChannel-grip",
 			parentSelector: ".gsuiMixer-panChannels",
 			onchange: elChan => {
-				const obj = gsuiReorder.listComputeOrderChange( this._pchans, {} ),
-					chanName = this.gsdata.data[ elChan.dataset.id ].name;
-
-				this.onchange( obj, [ "mixer", "reorderChan", chanName ] );
+				this.onchange( "reorderChannel", elChan.dataset.id,
+					gsuiReorder.listComputeOrderChange( this._pchans, {} ) );
 			},
 		} );
-		this.empty();
-		this.selectChan( "main" );
 	}
 
+	// .........................................................................
 	attached() {
 		const pan = this._pchans;
 
 		this._attached = true;
 		pan.style.bottom = `${ pan.clientHeight - pan.offsetHeight }px`;
-		Object.entries( this.gsdata.data ).forEach( ( [ id, obj ] ) => {
-			const html = this._chans[ id ];
-
+		Object.entries( this._chans ).forEach( ( [ id, html ] ) => {
 			html.pan.attached();
 			html.gain.attached();
-			this.onaddChan( id, obj );
 		} );
 		this.resized();
 	}
 	resized() {
-		const chans = Object.values( this._chans ),
-			{ width, height } = chans[ 0 ].analyser.rootElement.getBoundingClientRect();
+		const chans = Object.values( this._chans );
 
-		this._analyserW = width;
-		this._analyserH = height;
-		chans.forEach( html => html.analyser.setResolution( width, height ) );
+		if ( chans.length ) {
+			const { width, height } = chans[ 0 ].analyser.rootElement.getBoundingClientRect();
+
+			this._analyserW = width;
+			this._analyserH = height;
+			chans.forEach( html => html.analyser.setResolution( width, height ) );
+		}
 	}
 	updateAudioData( id, ldata, rdata ) {
 		this._chans[ id ].analyser.draw( ldata, rdata );
 	}
-	empty() {
-		this.gsdata.clear();
-		this.gsdata.change( {
-			channels: {
-				main: {
-					toggle: true,
-					name: "main",
-					gain: 1,
-					pan: 0,
-				},
-			},
-		} );
-		this.selectChan( "main" );
-	}
-	change( obj ) {
-		this.gsdata.change( obj );
-		gsuiReorder.listReorder( this._pchans, obj.channels );
-	}
-	getCurrentChannelId() {
+	getSelectedChannelId() {
 		return this._chanSelected;
 	}
-	selectChan( id ) {
-		if ( id in this.gsdata.data ) {
-			const chan = this._chans[ id ].root,
-				pchan = this._chans[ this._chanSelected ];
+	selectChannel( id ) {
+		const chan = this._chans[ id ].root,
+			pchan = this._chans[ this._chanSelected ];
 
-			pchan && pchan.root.classList.remove( "gsuiMixer-selected" );
-			chan.classList.add( "gsuiMixer-selected" );
-			this._chanSelected = id;
-			this._updateChanConnections();
-			this.onselectChan( id );
-		}
+		pchan && pchan.root.classList.remove( "gsuiMixer-selected" );
+		chan.classList.add( "gsuiMixer-selected" );
+		this._chanSelected = id;
+		this._updateChanConnections();
+		this.onselectChan( id );
 	}
 
-	// private:
 	// .........................................................................
-	_getNextChan( el, dir ) {
-		const sibling = el[ dir ];
-
-		return sibling && "id" in sibling.dataset ? sibling : null;
-	}
-	_updateChanConnections() {
-		const selId = this._chanSelected;
-
-		if ( selId ) {
-			const chanDest = this.gsdata.data[ selId ].dest,
-				html = this._chans[ selId ];
-			let bOnce = false;
-
-			Object.entries( this.gsdata.data ).forEach( ( [ id, chan ] ) => {
-				const html = this._chans[ id ],
-					a = id === chanDest,
-					b = chan.dest === selId;
-
-				html.connectA.dataset.icon = a ? "caret-up" : "";
-				html.connectB.dataset.icon = b ? "caret-down" : "";
-				bOnce = bOnce || b;
-			} );
-			html.connectA.dataset.icon = selId !== "main" ? "caret-down" : "";
-			html.connectB.dataset.icon = bOnce ? "caret-up" : "";
-		}
-	}
-	_addChan( id, chan ) {
+	addChannel( id, chan ) {
 		const root = gsuiMixer.channelTemplate.cloneNode( true ),
 			qs = n => root.querySelector( `.gsuiMixerChannel-${ n }` ),
 			pan = new gsuiSlider(),
 			gain = new gsuiSlider(),
 			canvas = qs( "analyser" ),
-			call = this.gsdata.callAction,
 			html = { root, pan, gain,
 				name: qs( "name" ),
 				connectA: qs( "connectA" ),
@@ -167,39 +96,94 @@ class gsuiMixer {
 		gain.options( { min: 0, max: 1, step: .01, type: "linear-y", mousemoveSize: 400 } );
 		pan.oninput = val => this.oninput( id, "pan", val );
 		gain.oninput = val => this.oninput( id, "gain", val );
-		pan.onchange = call.bind( this.gsdata, "updateChanProp", id, "pan" );
-		gain.onchange = call.bind( this.gsdata, "updateChanProp", id, "gain" );
+		pan.onchange = val => this.onchange( "changeChannel", id, "pan", val );
+		gain.onchange = val => this.onchange( "changeChannel", id, "gain", val );
 		canvas.onclick =
-		qs( "nameWrap" ).onclick = this.selectChan.bind( this, id );
-		qs( "toggle" ).onclick = call.bind( this.gsdata, "toggleChan", id );
-		qs( "delete" ).onclick = call.bind( this.gsdata, "removeChan", id );
-		qs( "connect" ).onclick = () => this.gsdata.callAction( "redirectChan", this._chanSelected, id );
+		qs( "nameWrap" ).onclick = this.selectChannel.bind( this, id );
+		qs( "toggle" ).onclick = () => this.onchange( "toggleChannel", id );
+		qs( "delete" ).onclick = () => this.onchange( "removeChannel", id );
+		qs( "connect" ).onclick = () => this.onchange( "redirectChannel", this._chanSelected, id );
 		( id === "main" ? this._pmain : this._pchans ).append( root );
 		if ( this._attached ) {
 			pan.attached();
 			gain.attached();
+			if ( !this._analyserW ) {
+				this.resized();
+			}
 			html.analyser.setResolution( this._analyserW, this._analyserH );
 		}
-		this.onaddChan( id, chan );
-		this._updateChanConnections();
+		if ( this._chanSelected ) {
+			this._updateChanConnections();
+		} else if ( id === "main" ) {
+			this.selectChannel( id );
+		}
 	}
-	_removeChan( id ) {
+	removeChannel( id ) {
 		const el = this._chans[ id ].root;
 
 		if ( id === this._chanSelected ) {
 			const next = this._getNextChan( el, "nextElementSibling" );
 
 			if ( next ) {
-				this.selectChan( next.dataset.id );
+				this.selectChannel( next.dataset.id );
 			} else {
 				const prev = this._getNextChan( el, "previousElementSibling" );
 
-				this.selectChan( prev ? prev.dataset.id : "main" );
+				this.selectChannel( prev ? prev.dataset.id : "main" );
 			}
 		}
-		this.ondeleteChan( id );
 		delete this._chans[ id ];
 		el.remove();
+	}
+	toggleChannel( id, b ) {
+		this._chans[ id ].root.classList.toggle( "gsuiMixerChannel-muted", !b );
+	}
+	changePanChannel( id, val ) {
+		this._chans[ id ].pan.setValue( val );
+	}
+	changeGainChannel( id, val ) {
+		this._chans[ id ].gain.setValue( val );
+	}
+	renameChannel( id, name ) {
+		this._chans[ id ].name.textContent = name;
+	}
+	reorderChannel( id, n ) {
+		this._chans[ id ].root.dataset.order = n;
+	}
+	reorderChannels( channels ) {
+		gsuiReorder.listReorder( this._pchans, channels );
+	}
+	redirectChannel( id, dest ) {
+		this._chans[ id ].root.dataset.dest = dest;
+		this._updateChanConnections();
+	}
+
+	// private:
+	// .........................................................................
+	_getNextChan( el, dir ) {
+		const sibling = el[ dir ];
+
+		return sibling && "id" in sibling.dataset ? sibling : null;
+	}
+	_updateChanConnections() {
+		const selId = this._chanSelected;
+
+		if ( selId ) {
+			const html = this._chans[ selId ],
+				chanDest = html.root.dataset.dest;
+			let bOnce = false;
+
+			Object.entries( this._chans ).forEach( ( [ id, html ] ) => {
+				const a = id === chanDest,
+					b = html.root.dataset.dest === selId;
+
+				html.connectA.dataset.icon = a ? "caret-up" : "";
+				html.connectB.dataset.icon = b ? "caret-down" : "";
+				bOnce = bOnce || b;
+			} );
+			html.connectA.dataset.icon = selId !== "main" ? "caret-down" : "";
+			html.connectB.dataset.icon = bOnce ? "caret-up" : "";
+		}
 	}
 }
 
