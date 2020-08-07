066a955d34134379b27879734767d19928b7c608
Thomas Tortorini <thomastortorini@gmail.com>
2020-08-07 17:30:40 +0200

    gsui.js: GSUtils.dispatchEvent -> GSUI.dispatchEvent

--- gsui.js
+++ gsui.js
@@ -1,7 +1,15 @@
 "use strict";
 
 const GSUI = {
+
 	dragshield: document.createElement( "gsui-dragshield" ),
+
+	dispatchEvent( el, component, eventName, ...args ) {
+		el.dispatchEvent( new CustomEvent( "gsuiEvents", {
+			bubbles: true,
+			detail: { component, eventName, args },
+		} ) );
+	},
 };
 
 document.body.prepend( GSUI.dragshield );
