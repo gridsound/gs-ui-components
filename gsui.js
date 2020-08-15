3dc8470c7a2da149db273d3e45b37d2eb32194d9
Thomas Tortorini <thomastortorini@gmail.com>
2020-08-15 07:14:43 +0200

    gsui.js: createElement

--- gsui.js
+++ gsui.js
@@ -10,6 +10,22 @@ const GSUI = {
 			detail: { component, eventName, args },
 		} ) );
 	},
+
+	createElement( tag, props, children ) {
+		const el = document.createElement( tag );
+
+		Object.entries( props ).forEach( ( [ p, val ] ) => {
+			if ( val || val === "" ) {
+				el.setAttribute( p, val );
+			}
+		} );
+		if ( Array.isArray( children ) ) {
+			el.append( ...children.filter( Boolean ) );
+		} else if ( children ) {
+			el.append( children );
+		}
+		return el;
+	},
 };
 
 document.body.prepend( GSUI.dragshield );
