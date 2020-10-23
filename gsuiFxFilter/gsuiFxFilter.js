edf153175040d9f17aaae47fe73dd26a0810279a
Thomas Tortorini <thomastortorini@gmail.com>
2020-10-24 00:32:28 +0200

    gsuiFxFilter: custom element

--- gsuiFxFilter/gsuiFxFilter.js
+++ gsuiFxFilter/gsuiFxFilter.js
@@ -1,28 +1,30 @@
 "use strict";
 
-class gsuiFxFilter {
+class gsuiFxFilter extends HTMLElement {
 	constructor() {
-		const root = gsuiFxFilter.template.cloneNode( true ),
-			elType = root.querySelector( ".gsuiFxFilter-areaType .gsuiFxFilter-area-content" ),
-			elGraph = root.querySelector( ".gsuiFxFilter-areaGraph .gsuiFxFilter-area-content" ),
+		const children = GSUI.getTemplate( "gsui-fx-filter" ),
+			elType = children[ 0 ].querySelector( ".gsuiFxFilter-area-content" ),
+			elGraph = children[ 1 ].querySelector( ".gsuiFxFilter-area-content" ),
 			uiCurves = GSUI.createElement( "gsui-curves" ),
 			uiSliders = new Map( [
-				[ "Q", root.querySelector( ".gsuiFxFilter-areaQ gsui-slider" ) ],
-				[ "gain", root.querySelector( ".gsuiFxFilter-areaGain gsui-slider" ) ],
-				[ "detune", root.querySelector( ".gsuiFxFilter-areaDetune gsui-slider" ) ],
-				[ "frequency", root.querySelector( ".gsuiFxFilter-areaFrequency gsui-slider" ) ],
+				[ "Q", children[ 4 ].querySelector( "gsui-slider" ) ],
+				[ "gain", children[ 3 ].querySelector( "gsui-slider" ) ],
+				[ "detune", children[ 5 ].querySelector( "gsui-slider" ) ],
+				[ "frequency", children[ 2 ].querySelector( "gsui-slider" ) ],
 			] );
 
-		this.rootElement = root;
+		super();
 		this.askData =
 		this.oninput =
 		this.onchange = () => {};
+		this._children = children;
 		this._nyquist = 24000;
 		this._uiCurves = uiCurves;
 		this._uiSliders = uiSliders;
 		this._elType = elType;
 		this._attached = false;
 		this._currType = "lowpass";
+		this._onresize = this._onresize.bind( this );
 		Object.seal( this );
 
 		elType.onclick = this._onclickType.bind( this );
@@ -34,22 +36,56 @@ class gsuiFxFilter {
 	}
 
 	// .........................................................................
+	connectedCallback() {
+		this._attached = true;
+		if ( this._children ) {
+			this.classList.add( "gsuiFxFilter" );
+			this.append( ...this._children );
+			this._children = null;
+			this._onresize();
+			this.updateWave();
+		}
+		GSUI.observeSizeOf( this, this._onresize );
+	}
+	disconnectedCallback() {
+		this._attached = false;
+		GSUI.unobserveSizeOf( this, this._onresize );
+	}
+	static get observedAttributes() {
+		return [ "type", "frequency", "q", "gain", "detune" ];
+	}
+	attributeChangedCallback( prop, prev, val ) {
+		if ( prev !== val ) {
+			switch ( prop ) {
+				case "type": {
+					const gainQ = gsuiFxFilter.typeGainQ[ val ];
+
+					this._toggleTypeBtn( this._currType, false );
+					this._toggleTypeBtn( val, true );
+					this._currType = val;
+					this._uiSliders.get( "Q" ).enable( gainQ.q );
+					this._uiSliders.get( "gain" ).enable( gainQ.gain );
+				} break;
+				case "q":
+					this._uiSliders.get( "Q" ).setValue( +val );
+					break;
+				case "gain":
+				case "detune":
+					this._uiSliders.get( prop ).setValue( +val );
+					break;
+				case "frequency":
+					this._uiSliders.get( "frequency" ).setValue( ( Math.log2( val / this._nyquist ) + 11 ) / 11 );
+					break;
+			}
+		}
+	}
+
+	// .........................................................................
 	setNyquist( n ) {
 		this._nyquist = n;
 	}
-	isAttached() {
-		return this._attached;
-	}
-	attached() {
-		this._attached = true;
-		this._uiCurves.resized();
-		this.updateWave();
-	}
-	resized() {
-		this._uiCurves.resized();
-	}
 	toggle( b ) {
-		this.rootElement.classList.toggle( "gsuiFxFilter-enable", b );
+		this.classList.toggle( "gsuiFxFilter-enable", b );
 		setTimeout( () => this.updateWave(), 150 );
 	}
 	updateWave() {
@@ -60,29 +96,6 @@ class gsuiFxFilter {
 				this._uiCurves.setCurve( "0", curve );
 			}
 		}
-	}
-
-	// .........................................................................
-	change( prop, val ) {
-		switch ( prop ) {
-			case "type": this._changeType( val ); break;
-			case "frequency": this._changeFrequency( val ); break;
-			case "Q":
-			case "gain":
-			case "detune": this._uiSliders.get( prop ).setValue( val ); break;
-		}
-	}
-	_changeFrequency( hz ) {
-		this._uiSliders.get( "frequency" ).setValue( ( Math.log2( hz / this._nyquist ) + 11 ) / 11 );
-	}
-	_changeType( type ) {
-		const gainQ = gsuiFxFilter.typeGainQ[ type ];
-
-		this._toggleTypeBtn( this._currType, false );
-		this._toggleTypeBtn( type, true );
-		this._currType = type;
-		this._uiSliders.get( "Q" ).enable( gainQ.Q );
-		this._uiSliders.get( "gain" ).enable( gainQ.gain );
 	}
 
 	// .........................................................................
@@ -102,6 +115,9 @@ class gsuiFxFilter {
 
 	// events
 	// .........................................................................
+	_onresize() {
+		this._uiCurves.resized();
+	}
 	_oninputProp( prop, val ) {
 		this.oninput( prop, val );
 		this.updateWave();
@@ -115,22 +131,18 @@ class gsuiFxFilter {
 	}
 }
 
-gsuiFxFilter.template = document.querySelector( "#gsuiFxFilter" );
-gsuiFxFilter.template.remove();
-gsuiFxFilter.template.removeAttribute( "id" );
-
 gsuiFxFilter.typeGainQ = GSUtils.deepFreeze( {
-	lowpass:   { gain: false, Q: true },
-	highpass:  { gain: false, Q: true },
-	bandpass:  { gain: false, Q: true },
-	lowshelf:  { gain: true,  Q: false },
-	highshelf: { gain: true,  Q: false },
-	peaking:   { gain: true,  Q: true },
-	notch:     { gain: false, Q: true },
-	allpass:   { gain: false, Q: true },
+	lowpass:   { gain: false, q: true },
+	highpass:  { gain: false, q: true },
+	bandpass:  { gain: false, q: true },
+	lowshelf:  { gain: true,  q: false },
+	highshelf: { gain: true,  q: false },
+	peaking:   { gain: true,  q: true },
+	notch:     { gain: false, q: true },
+	allpass:   { gain: false, q: true },
 } );
 
-Object.freeze( gsuiFxFilter );
+customElements.define( "gsui-fx-filter", gsuiFxFilter );
 
 if ( typeof gsuiEffects !== "undefined" ) {
 	gsuiEffects.fxsMap.set( "filter", { cmp: gsuiFxFilter, name: "Filter", height: 160 } );
