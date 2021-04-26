474d6b9275cd4d8c7a264fcb69a1f510c0261a1f
mr21 <thomastortorini@gmail.com>
2021-04-26 05:38:34 +0200

    gsuiEnvelopeGraph: private fields

--- gsuiEnvelopeGraph/gsuiEnvelopeGraph.js
+++ gsuiEnvelopeGraph/gsuiEnvelopeGraph.js
@@ -1,22 +1,22 @@
 "use strict";
 
 class gsuiEnvelopeGraph extends HTMLElement {
+	#mainLine = GSUI.createElementNS( "polyline", { class: "gsuiEnvelopeGraph-mainLine" } )
+	#attLine = GSUI.createElementNS( "polyline", { class: "gsuiEnvelopeGraph-line" } )
+	#relLine = GSUI.createElementNS( "polyline", { class: "gsuiEnvelopeGraph-line" } )
+	#svg = GSUI.createElementNS( "svg", { preserveAspectRatio: "none" },
+		this.#mainLine, this.#attLine, this.#relLine )
+	#width = 0
+	#height = 0
+
 	constructor() {
 		super();
-		this._svg = GSUI.createElementNS( "svg", { preserveAspectRatio: "none" },
-			this._mainLine = GSUI.createElementNS( "polyline", { class: "gsuiEnvelopeGraph-mainLine" } ),
-			this._attLine = GSUI.createElementNS( "polyline", { class: "gsuiEnvelopeGraph-line" } ),
-			this._relLine = GSUI.createElementNS( "polyline", { class: "gsuiEnvelopeGraph-line" } ),
-		);
 		this.attack = .25;
 		this.hold = .25;
 		this.decay = .25;
 		this.substain = .8;
 		this.release = 1;
-		this.duration = "auto";
 		this.duration = 4;
-		this.width =
-		this.height = 0;
 		Object.seal( this );
 	}
 
@@ -24,7 +24,7 @@ class gsuiEnvelopeGraph extends HTMLElement {
 	connectedCallback() {
 		if ( !this.firstChild ) {
 			this.classList.add( "gsuiEnvelopeGraph" );
-			this.append( this._svg );
+			this.append( this.#svg );
 			this.resized();
 		}
 	}
@@ -35,23 +35,23 @@ class gsuiEnvelopeGraph extends HTMLElement {
 			w = ~~bcr.width,
 			h = ~~bcr.height;
 
-		this.width = w;
-		this.height = h;
-		this._svg.setAttribute( "viewBox", `0 0 ${ w } ${ h }` );
+		this.#width = w;
+		this.#height = h;
+		this.#svg.setAttribute( "viewBox", `0 0 ${ w } ${ h }` );
 		this.draw();
 	}
 	draw() {
 		if ( this.firstChild ) {
-			const pts = gsuiEnvelopeGraph._getPoints(
-					this.width, this.height, this.duration,
+			const pts = gsuiEnvelopeGraph.#getPoints(
+					this.#width, this.#height, this.duration,
 					this.attack, this.hold, this.decay, this.substain, this.release );
 
-			this._attLine.setAttribute( "points", pts.slice( 0, 8 ).join( " " ) );
-			this._relLine.setAttribute( "points", pts.slice( -4 ).join( " " ) );
-			this._mainLine.setAttribute( "points", pts.join( " " ) );
+			this.#attLine.setAttribute( "points", pts.slice( 0, 8 ).join( " " ) );
+			this.#relLine.setAttribute( "points", pts.slice( -4 ).join( " " ) );
+			this.#mainLine.setAttribute( "points", pts.join( " " ) );
 		}
 	}
-	static _getPoints( w, h, dur, att, hold, dec, sub, rel ) {
+	static #getPoints( w, h, dur, att, hold, dec, sub, rel ) {
 		const dur2 = dur !== "auto" ? dur : att + hold + dec + 1 + rel,
 			bpp = w / dur2,
 			attX = bpp * att,
