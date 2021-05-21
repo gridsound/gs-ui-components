e381a61844ae39710f512eb800d7e3aac1eb4ab8
mr21 <thomastortorini@gmail.com>
2021-05-22 00:59:30 +0200

    gsuiSynthesizer: private fields

--- gsuiSynthesizer/gsuiSynthesizer.js
+++ gsuiSynthesizer/gsuiSynthesizer.js
@@ -1,29 +1,26 @@
 "use strict";
 
 class gsuiSynthesizer extends HTMLElement {
-	constructor() {
-		const children = GSUI.getTemplate( "gsui-synthesizer" ),
-			elOscList = children[ 3 ],
-			elNewOsc = elOscList.firstChild;
+	#env = null
+	#lfo = null
+	#waveList = []
+	#uiOscs = new Map()
+	#children = GSUI.getTemplate( "gsui-synthesizer" )
+	#elOscList = this.#children[ 3 ]
 
+	constructor() {
 		super();
-		this._children = children;
-		this._env =
-		this._lfo = null;
-		this._waveList = [];
-		this._elOscList = elOscList;
-		this._uiOscs = new Map();
 		Object.seal( this );
 
-		elNewOsc.onclick = this._onclickNewOsc.bind( this );
+		this.#children[ 3 ].firstChild.onclick = this.#onclickNewOsc.bind( this );
 		new gsuiReorder( {
-			rootElement: elOscList,
+			rootElement: this.#elOscList,
 			direction: "column",
 			dataTransferType: "oscillator",
 			itemSelector: ".gsuiOscillator",
 			handleSelector: ".gsuiOscillator-grip",
 			parentSelector: ".gsuiSynthesizer-oscList",
-			onchange: this._onchangeReorder.bind( this ),
+			onchange: this.#onchangeReorder.bind( this ),
 		} );
 	}
 
@@ -31,56 +28,56 @@ class gsuiSynthesizer extends HTMLElement {
 	connectedCallback() {
 		if ( !this.firstChild ) {
 			this.classList.add( "gsuiSynthesizer" );
-			this.append( ...this._children );
-			this.querySelector( ".gsuiSynthesizer-env" ).append( this._env );
-			this.querySelector( ".gsuiSynthesizer-lfo" ).append( this._lfo );
-			this._children = null;
+			this.append( ...this.#children );
+			this.querySelector( ".gsuiSynthesizer-env" ).append( this.#env );
+			this.querySelector( ".gsuiSynthesizer-lfo" ).append( this.#lfo );
+			this.#children = null;
 		}
 	}
 
 	// .........................................................................
 	setEnvelope( env ) {
-		this._env = env;
+		this.#env = env;
 	}
 	setLFO( lfo ) {
-		this._lfo = lfo;
+		this.#lfo = lfo;
 	}
 	setWaveList( arr ) {
-		this._waveList = arr;
-		this._uiOscs.forEach( o => o.addWaves( arr ) );
+		this.#waveList = arr;
+		this.#uiOscs.forEach( o => o.addWaves( arr ) );
 	}
 	getOscillator( id ) {
-		return this._uiOscs.get( id );
+		return this.#uiOscs.get( id );
 	}
 
 	// .........................................................................
 	addOscillator( id, osc ) {
 		const uiOsc = document.createElement( "gsui-oscillator" );
 
-		this._uiOscs.set( id, uiOsc );
-		uiOsc.addWaves( this._waveList );
+		this.#uiOscs.set( id, uiOsc );
+		uiOsc.addWaves( this.#waveList );
 		uiOsc.dataset.id = id;
-		this._elOscList.append( uiOsc );
+		this.#elOscList.append( uiOsc );
 	}
 	removeOscillator( id ) {
-		const osc = this._uiOscs.get( id );
+		const osc = this.#uiOscs.get( id );
 
 		if ( osc ) {
 			osc.remove();
-			this._uiOscs.delete( id );
+			this.#uiOscs.delete( id );
 		}
 	}
 	reorderOscillators( obj ) {
-		gsuiReorder.listReorder( this._elOscList, obj );
+		gsuiReorder.listReorder( this.#elOscList, obj );
 	}
 
 	// events:
 	// .........................................................................
-	_onclickNewOsc() {
+	#onclickNewOsc() {
 		GSUI.dispatchEvent( this, "gsuiSynthesizer", "addOscillator" );
 	}
-	_onchangeReorder() {
-		const oscs = gsuiReorder.listComputeOrderChange( this._elOscList, {} );
+	#onchangeReorder() {
+		const oscs = gsuiReorder.listComputeOrderChange( this.#elOscList, {} );
 
 		GSUI.dispatchEvent( this, "gsuiSynthesizer", "reorderOscillator", oscs );
 	}
