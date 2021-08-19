bd9641d8c3c7c9272dbbd1f204b91915fb1ce276
mr21 <thomastortorini@gmail.com>
2021-08-20 01:44:30 +0200

    gsuiSlider: private fields

--- gsuiSlider/gsuiSlider.html.js
+++ gsuiSlider/gsuiSlider.html.js
@@ -1,15 +1,13 @@
 "use strict";
 
-GSUI.setTemplate( "gsui-slider", () => {
-	return [
-		GSUI.createElement( "input", { type: "range", class: "gsuiSlider-input" } ),
-		GSUI.createElement( "div", { class: "gsuiSlider-line" },
-			GSUI.createElement( "div", { class: "gsuiSlider-lineColor" } ),
-		),
-		GSUI.createElementNS( "svg", { class: "gsuiSlider-svg" },
-			GSUI.createElementNS( "circle", { class: "gsuiSlider-svgLine" } ),
-			GSUI.createElementNS( "circle", { class: "gsuiSlider-svgLineColor" } ),
-		),
-		GSUI.createElement( "div", { class: "gsuiSlider-eventCatcher" } ),
-	];
-} );
+GSUI.setTemplate( "gsui-slider", () => [
+	GSUI.createElement( "input", { type: "range", class: "gsuiSlider-input" } ),
+	GSUI.createElement( "div", { class: "gsuiSlider-line" },
+		GSUI.createElement( "div", { class: "gsuiSlider-lineColor" } ),
+	),
+	GSUI.createElementNS( "svg", { class: "gsuiSlider-svg" },
+		GSUI.createElementNS( "circle", { class: "gsuiSlider-svgLine" } ),
+		GSUI.createElementNS( "circle", { class: "gsuiSlider-svgLineColor" } ),
+	),
+	GSUI.createElement( "div", { class: "gsuiSlider-eventCatcher" } ),
+] );
--- gsuiSlider/gsuiSlider.js
+++ gsuiSlider/gsuiSlider.js
@@ -1,64 +1,67 @@
 "use strict";
 
 class gsuiSlider extends HTMLElement {
-	constructor() {
-		const [ input, ...html ] = GSUI.getTemplate( "gsui-slider" );
+	#min = 0
+	#max = 100
+	#step = 1
+	#scrollStep = 1
+	#wheelChange = false
+	#mousemoveSize = 0
+	#strokeWidth = 4
+	#previousval = ""
+	#enable = true
+	#circ = false
+	#axeX = false
+	#connected = false
+	#pxval = 0
+	#pxmoved = 0
+	#svgLineLen = 0
+	#children = GSUI.getTemplate( "gsui-slider" )
+	#elInput = this.#children.shift()
+	#elLine = null
+	#elLineColor = null
+	#elSvg = null
+	#elSvgLine = null
+	#elSvgLineColor = null
 
+	constructor() {
 		super();
-		this._min = 0;
-		this._max = 100;
-		this._step = 1;
-		this._scrollStep = 1;
-		this._wheelChange = false;
-		this._mousemoveSize = 0;
-		this._strokeWidth = 4;
 
 		this.oninput =
 		this.onchange =
 		this.oninputend =
 		this.oninputstart = null;
-
-		this.value =
-		this._previousval = "";
-		this._enable = true;
-		this._circ =
-		this._axeX =
-		this._locked =
-		this._connected = false;
+		this.value = "";
+		this._locked = false;
 		this.width =
-		this.height =
-		this._pxval =
-		this._pxmoved =
-		this._svgLineLen = 0;
-		this._children = html;
-		this._elInput = input;
-		this._elLine = html[ 0 ];
-		this._elLineColor = this._elLine.firstElementChild;
-		this._elSvg = html[ 1 ];
-		this._elSvgLine = this._elSvg.firstElementChild;
-		this._elSvgLineColor = this._elSvg.lastElementChild;
+		this.height = 0;
+		this.#elLine = this.#children[ 0 ];
+		this.#elLineColor = this.#elLine.firstElementChild;
+		this.#elSvg = this.#children[ 1 ];
+		this.#elSvgLine = this.#elSvg.firstElementChild;
+		this.#elSvgLineColor = this.#elSvg.lastElementChild;
 		Object.seal( this );
 
-		this.onwheel = this._wheel.bind( this );
+		this.onwheel = this.#onwheel.bind( this );
 		this.onmouseup = this._mouseup.bind( this );
-		this.onmousedown = this._mousedown.bind( this );
-		this.onmousemove = this._mousemove.bind( this );
-		this.onmouseleave = this._mouseleave.bind( this );
+		this.onmousedown = this.#onmousedown.bind( this );
+		this.onmousemove = this.#onmousemove.bind( this );
+		this.onmouseleave = this.#onmouseleave.bind( this );
 	}
 
 	// .........................................................................
 	connectedCallback() {
 		this.classList.add( "gsuiSlider" );
-		if ( this._children ) {
+		if ( this.#children ) {
 			const brc = this.getBoundingClientRect();
 
-			this.append( ...this._children );
+			this.append( ...this.#children );
 			this.width = brc.width;
 			this.height = brc.height;
-			this._children = null;
-			this._connected = true;
-			this._setSVGcirc();
-			this._updateVal();
+			this.#children = null;
+			this.#connected = true;
+			this.#setSVGcirc();
+			this.#updateVal();
 		}
 	}
 	static get observedAttributes() {
@@ -70,39 +73,39 @@ class gsuiSlider extends HTMLElement {
 
 			switch ( prop ) {
 				case "type":
-					this._setType( val );
-					this._setSVGcirc();
+					this.#setType( val );
+					this.#setSVGcirc();
 					updateVal = true;
 					break;
 				case "min":
-					this._elInput.min = this._min = +val;
+					this.#elInput.min = this.#min = +val;
 					updateVal = true;
 					break;
 				case "max":
-					this._elInput.max = this._max = +val;
+					this.#elInput.max = this.#max = +val;
 					updateVal = true;
 					break;
 				case "step":
-					this._elInput.step = this._step = +val;
+					this.#elInput.step = this.#step = +val;
 					updateVal = true;
 					break;
 				case "scroll-step":
-					this._scrollStep = +val;
+					this.#scrollStep = +val;
 					break;
 				case "wheel-change":
-					this._wheelChange = typeof val === "string";
+					this.#wheelChange = typeof val === "string";
 					break;
 				case "mousemove-size":
-					this._mousemoveSize = +val;
+					this.#mousemoveSize = +val;
 					break;
 				case "stroke-width":
-					this._strokeWidth = +val;
-					this._setSVGcirc();
+					this.#strokeWidth = +val;
+					this.#setSVGcirc();
 					break;
 			}
 			if ( updateVal ) {
-				this._previousval = this._getInputVal();
-				this._updateVal();
+				this.#previousval = this.#getInputVal();
+				this.#updateVal();
 			}
 		}
 	}
@@ -110,34 +113,33 @@ class gsuiSlider extends HTMLElement {
 	// .........................................................................
 	setValue( val, bymouse ) {
 		if ( !this._locked || bymouse ) {
-			const prevVal = this._getInputVal(),
-				newVal = ( this._elInput.value = val, this._getInputVal() );
+			const prevVal = this.#getInputVal(),
+				newVal = ( this.#elInput.value = val, this.#getInputVal() );
 
 			if ( newVal !== prevVal ) {
-				this._updateVal();
+				this.#updateVal();
 				if ( bymouse && this.oninput ) {
 					this.oninput( +newVal );
 				}
 			}
 			if ( !bymouse ) {
-				this._previousval = newVal;
+				this.#previousval = newVal;
 			}
 		}
 	}
 	enable( b ) {
-		this._enable = b;
+		this.#enable = b;
 		this.classList.toggle( "gsuiSlider-disable", !b );
 	}
 
-	// private:
 	// .........................................................................
-	_setType( type ) {
-		const st = this._elLineColor.style,
+	#setType( type ) {
+		const st = this.#elLineColor.style,
 			circ = type === "circular",
 			axeX = type === "linear-x";
 
-		this._circ = circ;
-		this._axeX = axeX;
+		this.#circ = circ;
+		this.#axeX = axeX;
 		this.classList.toggle( "gsuiSlider-circular", circ );
 		this.classList.toggle( "gsuiSlider-linear", !circ );
 		if ( !circ ) {
@@ -154,59 +156,59 @@ class gsuiSlider extends HTMLElement {
 			}
 		}
 	}
-	_setSVGcirc() {
-		if ( this._circ && this.width && this.height ) {
+	#setSVGcirc() {
+		if ( this.#circ && this.width && this.height ) {
 			const size = Math.min( this.width, this.height ),
 				size2 = size / 2,
-				circR = ~~( ( size - this._strokeWidth ) / 2 );
+				circR = ~~( ( size - this.#strokeWidth ) / 2 );
 
-			this._elSvg.setAttribute( "viewBox", `0 0 ${ size } ${ size }` );
-			this._elSvgLine.setAttribute( "cx", size2 );
-			this._elSvgLine.setAttribute( "cy", size2 );
-			this._elSvgLine.setAttribute( "r", circR );
-			this._elSvgLineColor.setAttribute( "cx", size2 );
-			this._elSvgLineColor.setAttribute( "cy", size2 );
-			this._elSvgLineColor.setAttribute( "r", circR );
-			this._elSvgLine.style.strokeWidth =
-			this._elSvgLineColor.style.strokeWidth = this._strokeWidth;
-			this._svgLineLen = circR * 2 * Math.PI;
+			this.#elSvg.setAttribute( "viewBox", `0 0 ${ size } ${ size }` );
+			this.#elSvgLine.setAttribute( "cx", size2 );
+			this.#elSvgLine.setAttribute( "cy", size2 );
+			this.#elSvgLine.setAttribute( "r", circR );
+			this.#elSvgLineColor.setAttribute( "cx", size2 );
+			this.#elSvgLineColor.setAttribute( "cy", size2 );
+			this.#elSvgLineColor.setAttribute( "r", circR );
+			this.#elSvgLine.style.strokeWidth =
+			this.#elSvgLineColor.style.strokeWidth = this.#strokeWidth;
+			this.#svgLineLen = circR * 2 * Math.PI;
 		}
 	}
-	_getInputVal() {
-		const val = this._elInput.value;
+	#getInputVal() {
+		const val = this.#elInput.value;
 
 		return Math.abs( +val ) < .000001 ? "0" : val;
 	}
-	_getRange() {
-		return this._max - this._min;
+	#getRange() {
+		return this.#max - this.#min;
 	}
-	_getMousemoveSize() {
-		return this._mousemoveSize || (
-			this._circ
-				? this._svgLineLen
-				: this._axeX
-					? this._elLine.getBoundingClientRect().width
-					: this._elLine.getBoundingClientRect().height
+	#getMousemoveSize() {
+		return this.#mousemoveSize || (
+			this.#circ
+				? this.#svgLineLen
+				: this.#axeX
+					? this.#elLine.getBoundingClientRect().width
+					: this.#elLine.getBoundingClientRect().height
 		);
 	}
-	_updateVal() {
-		this.value = +this._getInputVal();
-		if ( this._connected ) {
-			const len = this._getRange(),
-				prcval = ( this.value - this._min ) / len,
-				prcstart = -this._min / len,
+	#updateVal() {
+		this.value = +this.#getInputVal();
+		if ( this.#connected ) {
+			const len = this.#getRange(),
+				prcval = ( this.value - this.#min ) / len,
+				prcstart = -this.#min / len,
 				prclen = Math.abs( prcval - prcstart ),
 				prcmin = Math.min( prcval, prcstart );
 
-			if ( this._circ ) {
-				const line = this._elSvgLineColor.style;
+			if ( this.#circ ) {
+				const line = this.#elSvgLineColor.style;
 
 				line.transform = `rotate(${ 90 + prcmin * 360 }deg)`;
-				line.strokeDasharray = `${ prclen * this._svgLineLen }, 999999`;
+				line.strokeDasharray = `${ prclen * this.#svgLineLen }, 999999`;
 			} else {
-				const line = this._elLineColor.style;
+				const line = this.#elLineColor.style;
 
-				if ( this._axeX ) {
+				if ( this.#axeX ) {
 					line.left = `${ prcmin * 100 }%`;
 					line.width = `${ prclen * 100 }%`;
 				} else {
@@ -216,44 +218,43 @@ class gsuiSlider extends HTMLElement {
 			}
 		}
 	}
-	_onchange() {
-		const val = this._getInputVal();
+	#onchange() {
+		const val = this.#getInputVal();
 
-		if ( this._previousval !== val ) {
+		if ( this.#previousval !== val ) {
 			this.onchange && this.onchange( +val );
-			this._previousval = val;
+			this.#previousval = val;
 		}
 	}
 
-	// events:
 	// .........................................................................
-	_wheel( e ) {
-		if ( this._enable && this._wheelChange ) {
+	#onwheel( e ) {
+		if ( this.#enable && this.#wheelChange ) {
 			const d = e.deltaY > 0 ? -1 : 1;
 
-			this.setValue( +this._getInputVal() + this._scrollStep * d, true );
+			this.setValue( +this.#getInputVal() + this.#scrollStep * d, true );
 			return false;
 		}
 	}
-	_mousedown() {
-		if ( this._enable ) {
-			this._onchange();
+	#onmousedown() {
+		if ( this.#enable ) {
+			this.#onchange();
 			if ( this.oninputstart ) {
 				this.oninputstart( this.value );
 			}
-			this._pxval = this._getRange() / this._getMousemoveSize();
-			this._pxmoved = 0;
+			this.#pxval = this.#getRange() / this.#getMousemoveSize();
+			this.#pxmoved = 0;
 			this.requestPointerLock();
 		}
 	}
-	_mousemove( e ) {
+	#onmousemove( e ) {
 		if ( this._locked ) {
-			const bound = this._getRange() / 5,
-				mov = this._circ || !this._axeX ? -e.movementY : e.movementX,
-				val = +this._previousval + ( this._pxmoved + mov ) * this._pxval;
+			const bound = this.#getRange() / 5,
+				mov = this.#circ || !this.#axeX ? -e.movementY : e.movementX,
+				val = +this.#previousval + ( this.#pxmoved + mov ) * this.#pxval;
 
-			if ( this._min - bound < val && val < this._max + bound ) {
-				this._pxmoved += mov;
+			if ( this.#min - bound < val && val < this.#max + bound ) {
+				this.#pxmoved += mov;
 			}
 			this.setValue( val, true );
 		}
@@ -262,14 +263,14 @@ class gsuiSlider extends HTMLElement {
 		if ( this._locked ) {
 			document.exitPointerLock();
 			this._locked = false;
-			this._onchange();
+			this.#onchange();
 			if ( this.oninputend ) {
 				this.oninputend( this.value );
 			}
 		}
 	}
-	_mouseleave() {
-		this._onchange();
+	#onmouseleave() {
+		this.#onchange();
 	}
 }
 
