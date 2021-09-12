ede80448872e4cc1a664259e45b5317a0dded2d6
mr21 <thomastortorini@gmail.com>
2021-09-12 09:54:37 +0200

    gsuiKeys: put the external data in static + Object.freeze

--- gsuiKeys/gsuiKeys.js
+++ gsuiKeys/gsuiKeys.js
@@ -1,6 +1,35 @@
 "use strict";
 
 class gsuiKeys extends HTMLElement {
+	static keyNames = Object.freeze( {
+		en: Object.freeze( [ "c",  "c#",  "d",  "d#",  "e",  "f",  "f#",  "g",   "g#",   "a",  "a#",  "b" ] ),
+		fr: Object.freeze( [ "do", "do#", "ré", "ré#", "mi", "fa", "fa#", "sol", "sol#", "la", "la#", "si" ] ),
+	} )
+	static keyboardToKey = { // 1.
+		KeyZ:   [ 0,  0 ], KeyS:      [ 0,  1 ],
+		KeyX:   [ 0,  2 ], KeyD:      [ 0,  3 ],
+		KeyC:   [ 0,  4 ],
+		KeyV:   [ 0,  5 ], KeyG:      [ 0,  6 ],
+		KeyB:   [ 0,  7 ], KeyH:      [ 0,  8 ],
+		KeyN:   [ 0,  9 ], KeyJ:      [ 0, 10 ],
+		KeyM:   [ 0, 11 ],
+		Comma:  [ 1,  0 ], KeyL:      [ 1,  1 ],
+		Period: [ 1,  2 ], Semicolon: [ 1,  3 ],
+		Slash:  [ 1,  4 ],
+		//
+		KeyQ:         [ 1,  0 ], Digit2: [ 1,  1 ],
+		KeyW:         [ 1,  2 ], Digit3: [ 1,  3 ],
+		KeyE:         [ 1,  4 ],
+		KeyR:         [ 1,  5 ], Digit5: [ 1,  6 ],
+		KeyT:         [ 1,  7 ], Digit6: [ 1,  8 ],
+		KeyY:         [ 1,  9 ], Digit7: [ 1, 10 ],
+		KeyU:         [ 1, 11 ],
+		KeyI:         [ 2,  0 ], Digit9: [ 2,  1 ],
+		KeyO:         [ 2,  2 ], Digit0: [ 2,  3 ],
+		KeyP:         [ 2,  4 ],
+		BracketLeft:  [ 2,  5 ], Equal:  [ 2,  6 ],
+		BracketRight: [ 2,  7 ],
+	}
 	#keysDown = new Map()
 	#gain = 1
 	#nbOct = 0
@@ -132,11 +161,10 @@ class gsuiKeys extends HTMLElement {
 	}
 }
 
-gsuiKeys.midiToKeyStr = m => gsuiKeys.keyIds[ m % 12 ] + ~~( m / 12 );
-gsuiKeys.keyStrToMidi = k => {
-	const key = k.substr( 0, k[ 1 ] !== "#" ? 1 : 2 );
-
-	return k.substr( key.length ) * 12 + gsuiKeys.keyIds[ key ];
-};
-
+Object.freeze( gsuiKeys );
 customElements.define( "gsui-keys", gsuiKeys );
+
+/*
+1. The arrays inside keyboardToKey have this format: [ relative octave, key index (C = 0, C# = 1) ].
+The arrangement of the code represent the white and black keys.
+*/
