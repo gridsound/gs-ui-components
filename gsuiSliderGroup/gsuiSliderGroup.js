e4b0a50ad0de77a0b90c5cc25cc70f00c50a45f1
mr21 <thomastortorini@gmail.com>
2021-09-12 06:56:19 +0200

    gsuiSliderGroup: private fields + findElements

--- gsuiSliderGroup/gsuiSliderGroup.js
+++ gsuiSliderGroup/gsuiSliderGroup.js
@@ -1,38 +1,40 @@
 "use strict";
 
 class gsuiSliderGroup extends HTMLElement {
-	constructor() {
-		const root = GSUI.getTemplate( "gsui-slidergroup" );
+	#min = 0
+	#max = 0
+	#def = 0
+	#exp = 0
+	#step = 0
+	#button = 0
+	#sliders = new Map()
+	#selected = new Map()
+	#valueSaved = new Map()
+	#bcr = null
+	#evMouseup = null
+	#evMousemove = null
+	#uiFn = Object.freeze( {
+		when: this.#sliderWhen.bind( this ),
+		value: this.#sliderValue.bind( this ),
+		duration: this.#sliderDuration.bind( this ),
+		selected: this.#sliderSelected.bind( this ),
+	} )
+	#children = GSUI.getTemplate( "gsui-slidergroup" )
+	#elements = GSUI.findElements( this.#children, {
+		slidersParent: ".gsuiSliderGroup-sliders",
+		defValue: ".gsuiSliderGroup-defaultValue",
+		beatlines: "gsui-beatlines",
+		currentTime: ".gsuiSliderGroup-currentTime",
+		loopA: ".gsuiSliderGroup-loopA",
+		loopB: ".gsuiSliderGroup-loopB",
+	} )
+	scrollElement = this.#children
 
+	constructor() {
 		super();
-		this.scrollElement = root;
-		this._slidersParent = root.querySelector( ".gsuiSliderGroup-sliders" );
-		this._uiBeatlines = root.querySelector( "gsui-beatlines" );
-		this._currentTime = root.querySelector( ".gsuiSliderGroup-currentTime" );
-		this._defValue = root.querySelector( ".gsuiSliderGroup-defaultValue" );
-		this._loopA = root.querySelector( ".gsuiSliderGroup-loopA" );
-		this._loopB = root.querySelector( ".gsuiSliderGroup-loopB" );
-		this._min =
-		this._max =
-		this._def =
-		this._exp =
-		this._step =
-		this._button = 0;
-		this._sliders = new Map();
-		this._selected = new Map();
-		this._valueSaved = new Map();
-		this._bcr =
-		this._evMouseup =
-		this._evMousemove = null;
-		this._uiFn = Object.freeze( {
-			when: this._sliderWhen.bind( this ),
-			value: this._sliderValue.bind( this ),
-			duration: this._sliderDuration.bind( this ),
-			selected: this._sliderSelected.bind( this ),
-		} );
 		Object.seal( this );
 
-		root.oncontextmenu = () => false;
+		this.#children.oncontextmenu = () => false;
 	}
 
 	// .........................................................................
@@ -41,18 +43,18 @@ class gsuiSliderGroup extends HTMLElement {
 			const beatlines = this.hasAttribute( "beatlines" );
 
 			if ( !beatlines ) {
-				this._uiBeatlines.remove();
-				this._currentTime.remove();
-				this._loopA.remove();
-				this._loopB.remove();
-				this._uiBeatlines =
-				this._currentTime =
-				this._loopA =
-				this._loopB = null;
+				this.#elements.beatlines.remove();
+				this.#elements.currentTime.remove();
+				this.#elements.loopA.remove();
+				this.#elements.loopB.remove();
+				this.#elements.beatlines =
+				this.#elements.currentTime =
+				this.#elements.loopA =
+				this.#elements.loopB = null;
 			}
 			this.append( this.scrollElement );
-			this._updatePxPerBeat();
-			this._slidersParent.onmousedown = this._mousedown.bind( this );
+			this.#updatePxPerBeat();
+			this.#elements.slidersParent.onmousedown = this.#mousedown.bind( this );
 			GSUI.recallAttributes( this, { pxperbeat: 64 } );
 			if ( beatlines ) {
 				GSUI.recallAttributes( this, { currenttime: 0, timedivision: "4/4" } );
@@ -66,24 +68,24 @@ class gsuiSliderGroup extends HTMLElement {
 		if ( prev !== val ) {
 			switch ( prop ) {
 				case "timedivision":
-					GSUI.setAttribute( this._uiBeatlines, "timedivision", val );
+					GSUI.setAttribute( this.#elements.beatlines, "timedivision", val );
 					break;
 				case "pxperbeat":
-					this._updatePxPerBeat();
+					this.#updatePxPerBeat();
 					break;
 				case "currenttime":
-					this._currentTime.style.left = `${ val }em`;
+					this.#elements.currentTime.style.left = `${ val }em`;
 					break;
 				case "loopa":
-					this._loopA.classList.toggle( "gsuiSliderGroup-loopOn", val );
+					this.#elements.loopA.classList.toggle( "gsuiSliderGroup-loopOn", val );
 					if ( val ) {
-						this._loopA.style.width = `${ val }em`;
+						this.#elements.loopA.style.width = `${ val }em`;
 					}
 					break;
 				case "loopb":
-					this._loopB.classList.toggle( "gsuiSliderGroup-loopOn", val );
+					this.#elements.loopB.classList.toggle( "gsuiSliderGroup-loopOn", val );
 					if ( val ) {
-						this._loopB.style.left = `${ val }em`;
+						this.#elements.loopB.style.left = `${ val }em`;
 					}
 					break;
 			}
@@ -92,86 +94,83 @@ class gsuiSliderGroup extends HTMLElement {
 
 	// .........................................................................
 	empty() {
-		this._sliders.forEach( s => s.element.remove() );
-		this._sliders.clear();
-		this._selected.clear();
-		this._valueSaved.clear();
+		this.#sliders.forEach( s => s.element.remove() );
+		this.#sliders.clear();
+		this.#selected.clear();
+		this.#valueSaved.clear();
 	}
 	options( { min, max, step, exp, def } ) {
-		this._min = min;
-		this._max = max;
-		this._step = step;
-		this._exp = exp ?? 0;
-		this._def = def ?? max;
-		this._defValue.style.top = `${ 100 - ( this._def - min ) / ( max - min ) * 100 }%`;
+		this.#min = min;
+		this.#max = max;
+		this.#step = step;
+		this.#exp = exp ?? 0;
+		this.#def = def ?? max;
+		this.#elements.defValue.style.top = `${ 100 - ( this.#def - min ) / ( max - min ) * 100 }%`;
 	}
 
 	// .........................................................................
 	delete( id ) {
-		this._sliders.get( id ).element.remove();
-		this._sliders.delete( id );
-		this._selected.delete( id );
-		delete this._slidersObj;
-		this._sliderSelectedClass();
+		this.#sliders.get( id ).element.remove();
+		this.#sliders.delete( id );
+		this.#selected.delete( id );
+		this.#sliderSelectedClass();
 	}
 	set( id, when, duration, value ) {
 		const element = GSUI.getTemplate( "gsui-slidergroup-slider" ),
 			sli = Object.seal( { element, when, duration, value, selected: false } );
 
-		element._slider =
-		element.firstElementChild._slider = sli;
 		element.dataset.id = id;
-		this._sliders.set( id, sli );
-		this._sliderWhen( sli, when );
-		this._sliderValue( sli, value );
-		this._sliderDuration( sli, duration );
-		this._slidersParent.append( element );
+		this.#sliders.set( id, sli );
+		this.#sliderWhen( sli, when );
+		this.#sliderValue( sli, value );
+		this.#sliderDuration( sli, duration );
+		this.#elements.slidersParent.append( element );
 	}
 	setProp( id, prop, value ) {
-		const sli = this._sliders.get( id );
+		const sli = this.#sliders.get( id );
 
 		if ( sli ) {
 			sli[ prop ] = value;
-			this._uiFn[ prop ]( sli, value );
+			this.#uiFn[ prop ]( sli, value );
 		}
 	}
 
 	// .........................................................................
-	_roundVal( val ) {
-		return +( Math.round( val / this._step ) * this._step ).toFixed( 8 );
+	#roundVal( val ) {
+		return +( Math.round( val / this.#step ) * this.#step ).toFixed( 8 );
 	}
-	_updatePxPerBeat() {
+	#updatePxPerBeat() {
 		const ppb = this.getAttribute( "pxperbeat" );
 
-		this._slidersParent.style.fontSize = `${ ppb }px`;
-		if ( this._uiBeatlines ) {
-			GSUI.setAttribute( this._uiBeatlines, "pxperbeat", ppb );
+		this.#elements.slidersParent.style.fontSize = `${ ppb }px`;
+		if ( this.#elements.beatlines ) {
+			GSUI.setAttribute( this.#elements.beatlines, "pxperbeat", ppb );
 		}
 	}
-	_sliderWhen( sli, when ) {
+	#sliderWhen( sli, when ) {
 		sli.element.style.left = `${ when }em`;
 		sli.element.style.zIndex = Math.floor( when * 100 );
 	}
-	_sliderDuration( sli, dur ) {
+	#sliderDuration( sli, dur ) {
 		sli.element.style.width = `${ dur }em`;
 	}
-	_sliderSelected( sli, b ) {
+	#sliderSelected( sli, b ) {
 		b
-			? this._selected.set( sli.element.dataset.id, sli )
-			: this._selected.delete( sli.element.dataset.id );
+			? this.#selected.set( sli.element.dataset.id, sli )
+			: this.#selected.delete( sli.element.dataset.id );
 		sli.element.classList.toggle( "gsuiSliderGroup-sliderSelected", !!b );
-		this._sliderSelectedClass();
+		this.#sliderSelectedClass();
 	}
-	_sliderSelectedClass() {
-		this._slidersParent.classList.toggle(
-			"gsuiSliderGroup-slidersSelected", this._selected.size > 0 );
+	#sliderSelectedClass() {
+		this.#elements.slidersParent.classList.toggle(
+			"gsuiSliderGroup-slidersSelected", this.#selected.size > 0 );
 	}
-	_sliderValue( sli, val ) {
+	#sliderValue( sli, val ) {
 		const el = sli.element.firstElementChild,
 			st = el.style,
-			max = this._max,
-			min = this._min,
-			valNum = Number.isFinite( val ) ? val : this._def,
+			max = this.#max,
+			min = this.#min,
+			valNum = Number.isFinite( val ) ? val : this.#def,
 			sameDir = min >= 0 === max >= 0,
 			percX = Math.abs( ( valNum - ( sameDir ? min : 0 ) ) ) / ( max - min ) * 100,
 			perc0 = sameDir ? 0 : Math.abs( min ) / ( max - min ) * 100;
@@ -187,34 +186,34 @@ class gsuiSliderGroup extends HTMLElement {
 	}
 
 	// .........................................................................
-	_mousedown( e ) {
-		if ( !this._evMouseup && ( e.button === 0 || e.button === 2 ) ) {
-			this._bcr = this._slidersParent.getBoundingClientRect();
-			this._button = e.button;
-			this._valueSaved.clear();
-			this._sliders.forEach( ( sli, id ) => this._valueSaved.set( id, sli.value ) );
-			this._evMouseup = this._mouseup.bind( this );
-			this._evMousemove = this._mousemove.bind( this );
-			document.addEventListener( "mouseup", this._evMouseup );
-			document.addEventListener( "mousemove", this._evMousemove );
+	#mousedown( e ) {
+		if ( !this.#evMouseup && ( e.button === 0 || e.button === 2 ) ) {
+			this.#bcr = this.#elements.slidersParent.getBoundingClientRect();
+			this.#button = e.button;
+			this.#valueSaved.clear();
+			this.#sliders.forEach( ( sli, id ) => this.#valueSaved.set( id, sli.value ) );
+			this.#evMouseup = this.#mouseup.bind( this );
+			this.#evMousemove = this.#mousemove.bind( this );
+			document.addEventListener( "mouseup", this.#evMouseup );
+			document.addEventListener( "mousemove", this.#evMousemove );
 			GSUI.unselectText();
 			GSUI.dragshield.show( "pointer" );
-			this._mousemove( e );
+			this.#mousemove( e );
 		}
 	}
-	_mousemove( e ) {
-		const sliders = this._selected.size > 0
-				? this._selected
-				: this._sliders,
-			x = e.pageX - this._bcr.left,
-			y = e.pageY - this._bcr.top,
-			min = this._min,
-			max = this._max,
+	#mousemove( e ) {
+		const sliders = this.#selected.size > 0
+				? this.#selected
+				: this.#sliders,
+			x = e.pageX - this.#bcr.left,
+			y = e.pageY - this.#bcr.top,
+			min = this.#min,
+			max = this.#max,
 			xval = x / this.getAttribute( "pxperbeat" ),
-			rval = this._button === 2
-				? this._def
-				: this._roundVal( min + ( max - min ) *
-					( 1 - Math.min( Math.max( 0, y / this._bcr.height ), 1 ) ) );
+			rval = this.#button === 2
+				? this.#def
+				: this.#roundVal( min + ( max - min ) *
+					( 1 - Math.min( Math.max( 0, y / this.#bcr.height ), 1 ) ) );
 		let firstWhen = 0;
 
 		sliders.forEach( sli => {
@@ -225,21 +224,21 @@ class gsuiSliderGroup extends HTMLElement {
 		sliders.forEach( sli => {
 			if ( firstWhen <= sli.when && sli.when <= xval && xval <= sli.when + sli.duration ) {
 				sli.value = rval;
-				this._sliderValue( sli, rval );
+				this.#sliderValue( sli, rval );
 				GSUI.dispatchEvent( this, "gsuiSliderGroup", "input", sli.element.dataset.id, rval );
 			}
 		} );
 	}
-	_mouseup() {
+	#mouseup() {
 		const arr = [];
 
 		GSUI.dragshield.hide();
-		document.removeEventListener( "mouseup", this._evMouseup );
-		document.removeEventListener( "mousemove", this._evMousemove );
-		this._evMouseup =
-		this._evMousemove = null;
-		this._sliders.forEach( ( sli, id ) => {
-			if ( sli.value !== this._valueSaved.get( id ) ) {
+		document.removeEventListener( "mouseup", this.#evMouseup );
+		document.removeEventListener( "mousemove", this.#evMousemove );
+		this.#evMouseup =
+		this.#evMousemove = null;
+		this.#sliders.forEach( ( sli, id ) => {
+			if ( sli.value !== this.#valueSaved.get( id ) ) {
 				arr.push( [ id, sli.value ] );
 			}
 		} );
