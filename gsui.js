fb0c8d8b43dc164918ba7ee0deb750400556efcf
mr21 <thomastortorini@gmail.com>
2021-10-24 20:29:33 +0200

    gsui.js: use a class + private fields

--- gsui.js
+++ gsui.js
@@ -1,22 +1,22 @@
 "use strict";
 
-const GSUI = Object.freeze( {
+class GSUI {
 
-	noop() {},
-	popup: document.createElement( "gsui-popup" ),
-	dragshield: document.createElement( "gsui-dragshield" ),
+	static noop() {}
+	static popup = document.createElement( "gsui-popup" )
+	static dragshield = document.createElement( "gsui-dragshield" )
 
 	// .........................................................................
-	clamp( n, min, max ) {
+	static clamp( n, min, max ) {
 		return (
 			min < max
 				? Math.max( min, Math.min( n || 0, max ) )
 				: Math.max( max, Math.min( n || 0, min ) )
 		);
-	},
+	}
 
 	// .........................................................................
-	diffObjects( a, b ) {
+	static diffObjects( a, b ) {
 		let empty = true;
 		const diff = Object.entries( b ).reduce( ( diff, [ bk, bv ] ) => {
 				const av = a[ bk ],
@@ -40,48 +40,48 @@ const GSUI = Object.freeze( {
 			}
 		} );
 		return empty ? undefined : Object.freeze( diff );
-	},
+	}
 
 	// .........................................................................
-	findElements( root, graph ) {
+	static findElements( root, graph ) {
 		return typeof graph === "string"
-			? GSUI._findElemStr( root, graph )
+			? GSUI.#findElemStr( root, graph )
 			: Object.seal( Array.isArray( graph )
-				? GSUI._findElemArr( root, graph )
-				: GSUI._findElemObj( root, graph ) );
-	},
-	_findElemArr( root, arr ) {
+				? GSUI.#findElemArr( root, graph )
+				: GSUI.#findElemObj( root, graph ) );
+	}
+	static #findElemArr( root, arr ) {
 		return arr.map( sel => GSUI.findElements( root, sel ) );
-	},
-	_findElemObj( root, obj ) {
+	}
+	static #findElemObj( root, obj ) {
 		const ent = Object.entries( obj );
 
 		ent.forEach( kv => kv[ 1 ] = GSUI.findElements( root, kv[ 1 ] ) );
 		return Object.fromEntries( ent );
-	},
-	_findElemStr( root, sel ) {
+	}
+	static #findElemStr( root, sel ) {
 		if ( Array.isArray( root ) ) {
 			let el;
 
-			Array.prototype.find.call( root, r => el = GSUI._findElemQuery( r, sel ) );
+			Array.prototype.find.call( root, r => el = GSUI.#findElemQuery( r, sel ) );
 			return el || null;
 		}
-		return GSUI._findElemQuery( root, sel );
-	},
-	_findElemQuery( root, sel ) {
+		return GSUI.#findElemQuery( root, sel );
+	}
+	static #findElemQuery( root, sel ) {
 		return root.matches( sel )
 			? root
 			: root.querySelector( sel );
-	},
+	}
 
 	// .........................................................................
-	dispatchEvent( el, component, eventName, ...args ) {
+	static dispatchEvent( el, component, eventName, ...args ) {
 		el.dispatchEvent( new CustomEvent( "gsuiEvents", {
 			bubbles: true,
 			detail: { component, eventName, args },
 		} ) );
-	},
-	listenEvents( el, cbs ) {
+	}
+	static listenEvents( el, cbs ) {
 		el.addEventListener( "gsuiEvents", e => {
 			const d = e.detail,
 				cbs2 = cbs[ d.component ] || cbs.default,
@@ -92,28 +92,28 @@ const GSUI = Object.freeze( {
 				e.stopImmediatePropagation();
 			}
 		} );
-	},
+	}
 
 	// .........................................................................
-	_templates: new Map(),
-	setTemplate( tmpId, fn ) {
-		GSUI._templates.set( tmpId, fn );
-	},
-	hasTemplate( tmpId ) {
-		return GSUI._templates.has( tmpId );
-	},
-	getTemplate( tmpId, ...args ) {
-		return GSUI._templates.get( tmpId )( ...args );
-	},
+	static #templates = new Map()
+	static setTemplate( tmpId, fn ) {
+		GSUI.#templates.set( tmpId, fn );
+	}
+	static hasTemplate( tmpId ) {
+		return GSUI.#templates.has( tmpId );
+	}
+	static getTemplate( tmpId, ...args ) {
+		return GSUI.#templates.get( tmpId )( ...args );
+	}
 
 	// .........................................................................
-	createElement( tag, attr, ...children ) {
-		return GSUI._createElement( "http://www.w3.org/1999/xhtml", tag, attr, children );
-	},
-	createElementSVG( tag, attr, ...children ) {
-		return GSUI._createElement( "http://www.w3.org/2000/svg", tag, attr, children );
-	},
-	_createElement( ns, tag, attr, children ) {
+	static createElement( tag, attr, ...children ) {
+		return GSUI.#createElement( "http://www.w3.org/1999/xhtml", tag, attr, children );
+	}
+	static createElementSVG( tag, attr, ...children ) {
+		return GSUI.#createElement( "http://www.w3.org/2000/svg", tag, attr, children );
+	}
+	static #createElement( ns, tag, attr, children ) {
 		const el = document.createElementNS( ns, tag );
 
 		if ( attr ) {
@@ -121,65 +121,65 @@ const GSUI = Object.freeze( {
 		}
 		el.append( ...children.flat( 1 ).filter( Boolean ) );
 		return el;
-	},
-	setAttributes( el, obj ) {
+	}
+	static setAttributes( el, obj ) {
 		Object.entries( obj ).forEach( kv => GSUI.setAttribute( el, ...kv ) );
-	},
-	setAttribute( el, attr, val ) {
+	}
+	static setAttribute( el, attr, val ) {
 		val !== false && val !== null && val !== undefined
 			? el.setAttribute( attr, val === true ? "" : val )
 			: el.removeAttribute( attr );
-	},
+	}
 
 	// .........................................................................
-	observeSizeOf( el, fn ) {
-		if ( GSUI._resizeMap.has( el ) ) {
-			GSUI._resizeMap.get( el ).push( fn );
+	static observeSizeOf( el, fn ) {
+		if ( GSUI.#resizeMap.has( el ) ) {
+			GSUI.#resizeMap.get( el ).push( fn );
 		} else {
-			GSUI._resizeMap.set( el, [ fn ] );
+			GSUI.#resizeMap.set( el, [ fn ] );
 		}
-		GSUI._resizeObs.observe( el );
-	},
-	unobserveSizeOf( el, fn ) {
-		const fns = GSUI._resizeMap.get( el ),
+		GSUI.#resizeObs.observe( el );
+	}
+	static unobserveSizeOf( el, fn ) {
+		const fns = GSUI.#resizeMap.get( el ),
 			fnInd = fns.indexOf( fn );
 
-		GSUI._resizeObs.unobserve( el );
+		GSUI.#resizeObs.unobserve( el );
 		if ( fnInd > -1 ) {
 			fns.splice( fnInd, 1 );
 			if ( fns.length === 0 ) {
-				GSUI._resizeMap.delete( el );
+				GSUI.#resizeMap.delete( el );
 			}
 		}
-	},
-	_resizeMap: new Map(),
-	_resizeObs: new ResizeObserver( entries => {
+	}
+	static #resizeMap = new Map()
+	static #resizeObs = new ResizeObserver( entries => {
 		entries.forEach( e => {
-			GSUI._resizeMap.get( e.target )
+			GSUI.#resizeMap.get( e.target )
 				.forEach( fn => fn( e.contentRect.width, e.contentRect.height ) );
 		} );
-	} ),
+	} )
 
 	// .........................................................................
-	empty( el ) {
+	static empty( el ) {
 		while ( el.lastChild ) {
 			el.lastChild.remove();
 		}
-	},
+	}
 
 	// .........................................................................
-	unselectText() {
+	static unselectText() {
 		window.getSelection().removeAllRanges();
-	},
+	}
 
 	// .........................................................................
-	recallAttributes( el, props ) {
+	static recallAttributes( el, props ) {
 		Object.entries( props ).forEach( ( [ p, val ] ) => {
 			el.hasAttribute( p )
 				? el.attributeChangedCallback( p, null, el.getAttribute( p ) )
 				: GSUI.setAttribute( el, p, val );
 		} );
-	},
-} );
+	}
+}
 
 document.body.prepend( GSUI.dragshield, GSUI.popup );
