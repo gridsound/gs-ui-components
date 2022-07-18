bf3ed52e5f4804528699e7a5167bb26af8e039d0
mr21 <thomastortorini@gmail.com>
2022-07-18 20:56:24 +0200

    gsuiWaveform: merge the two files

--- gsuiWaveform/gsuiWaveform.js
+++ gsuiWaveform/gsuiWaveform.js
@@ -19,6 +19,7 @@ class gsuiWaveform {
 		}
 	}
 
+	// .........................................................................
 	remove() {
 		this.empty();
 		this.rootElement.remove();
@@ -33,5 +34,45 @@ class gsuiWaveform {
 	}
 	drawBuffer( buf, offset, duration ) {
 		gsuiWaveform.drawBuffer( this.polygon, this.width, this.height, buf, offset, duration );
+	}
+
+	// .........................................................................
+	static drawBuffer( polygon, w, h, buf, offset, duration ) {
+		const d0 = buf.getChannelData( 0 );
+		const d1 = buf.numberOfChannels > 1 ? buf.getChannelData( 1 ) : d0;
+		const off = offset || 0;
+		const dur = duration || buf.duration - off;
+
+		gsuiWaveform.draw( polygon, w, h, d0, d1, buf.duration, off, dur );
+	}
+	static draw( polygon, w, h, data0, data1, bufferDuration, offset, duration ) {
+		const h2 = h / 2;
+		const step = duration / bufferDuration * data0.length / w;
+		const ind = ~~( offset / bufferDuration * data0.length );
+		const iinc = ~~Math.max( 1, step / 100 );
+		let dots0 = `0,${ gsuiWaveform.formatNb( h2 + data0[ ind ] * h2 ) }`;
+		let dots1 = `0,${ gsuiWaveform.formatNb( h2 + data1[ ind ] * h2 ) }`;
+
+		for ( let p = 1; p < w; ++p ) {
+			let lmin = Infinity;
+			let rmax = -Infinity;
+			let i = ~~( ind + ( p - 1 ) * step );
+			const iend = i + step;
+
+			for ( ; i < iend; i += iinc ) {
+				lmin = Math.min( lmin, data0[ i ], data1[ i ] );
+				rmax = Math.max( rmax, data0[ i ], data1[ i ] );
+			}
+			if ( Math.abs( rmax - lmin ) * h2 < 1 ) {
+				rmax += 1 / h;
+				lmin -= 1 / h;
+			}
+			dots0 += ` ${ p },${ gsuiWaveform.formatNb( h2 + lmin * h2 ) }`;
+			dots1  =  `${ p },${ gsuiWaveform.formatNb( h2 + rmax * h2 ) } ${ dots1 }`;
+		}
+		GSUI.$setAttribute( polygon, "points", `${ dots0 } ${ dots1 }` );
+	}
+	static formatNb( n ) {
+		return n.toFixed( 2 );
 	}
 }
