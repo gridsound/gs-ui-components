9665222c6ca6df779c91a9a91014b364ea6dd1d3
mr21 <thomastortorini@gmail.com>
2022-11-22 02:32:52 +0100

    gsuiFxDelay: max time -> .95

--- gsuiFxDelay/gsuiFxDelay.html.js
+++ gsuiFxDelay/gsuiFxDelay.html.js
@@ -6,7 +6,7 @@ GSUI.$setTemplate( "gsui-fx-delay", () => [
 		GSUI.$createElement( "span", { class: "gsuiFxDelay-param-value" } ),
 	),
 	GSUI.$createElement( "div", { class: "gsuiFxDelay-param", "data-prop": "gain" },
-		GSUI.$createElement( "gsui-slider", { type: "linear-x", step: .01, min: 0, max: 1 } ),
+		GSUI.$createElement( "gsui-slider", { type: "linear-x", step: .01, min: 0, max: .95 } ),
 		GSUI.$createElement( "span", { class: "gsuiFxDelay-param-value" } ),
 	),
 	GSUI.$createElement( "div", { class: "gsuiFxDelay-param", "data-prop": "pan" },
