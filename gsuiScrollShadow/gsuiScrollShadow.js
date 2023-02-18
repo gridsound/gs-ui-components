690c15e4e1c7cbbc365a277fd6e16b80274ab622
mr21 <thomastortorini@gmail.com>
2023-02-18 05:18:12 +0100

    gsuiScrollShadow: handle array of elements

--- gsuiScrollShadow/gsuiScrollShadow.js
+++ gsuiScrollShadow/gsuiScrollShadow.js
@@ -1,36 +1,35 @@
 "use strict";
 
 class gsuiScrollShadow {
+	#scrolledElem = null;
 	#topShadow = null;
 	#leftShadow = null;
 	#rightShadow = null;
 	#bottomShadow = null;
-	#scrolledElem = null;
+	#onscrollBind = this.#onscroll.bind( this );
 
 	constructor( o ) {
 		Object.seal( this );
 		this.#scrolledElem = o.scrolledElem;
-		this.#topShadow = o.topShadow;
-		this.#leftShadow = o.leftShadow;
-		this.#rightShadow = o.rightShadow;
-		this.#bottomShadow = o.bottomShadow;
+		this.#topShadow = o.topShadow ? [ o.topShadow ].flat() : [];
+		this.#leftShadow = o.leftShadow ? [ o.leftShadow ].flat() : [];
+		this.#rightShadow = o.rightShadow ? [ o.rightShadow ].flat() : [];
+		this.#bottomShadow = o.bottomShadow ? [ o.bottomShadow ].flat() : [];
 		gsuiScrollShadow.#initShadow( this.#topShadow, 'top' );
 		gsuiScrollShadow.#initShadow( this.#leftShadow, 'left' );
 		gsuiScrollShadow.#initShadow( this.#rightShadow, 'right' );
 		gsuiScrollShadow.#initShadow( this.#bottomShadow, 'bottom' );
-		this.#scrolledElem.addEventListener( "scroll", this.#onscroll.bind( this ) );
-		GSUI.$observeSizeOf( this.#scrolledElem, this.#onresize.bind( this ) );
+		this.#scrolledElem.addEventListener( "scroll", this.#onscrollBind );
+		GSUI.$observeSizeOf( this.#scrolledElem, this.#onscrollBind );
+		const arr = [];
 	}
 
 	// .........................................................................
-	static #initShadow( el, dir ) {
-		if ( el ) {
+	static #initShadow( elems, dir ) {
+		elems.forEach( el => {
 			el.classList.add( "gsuiScrollShadow" );
 			el.dataset.dir = dir;
-		}
-	}
-	#onresize() {
-		this.#onscroll();
+		} );
 	}
 	#onscroll() {
 		const el = this.#scrolledElem;
@@ -40,10 +39,10 @@ class gsuiScrollShadow {
 		gsuiScrollShadow.#onscroll2( this.#rightShadow, el.scrollWidth - el.clientWidth - el.scrollLeft );
 		gsuiScrollShadow.#onscroll2( this.#bottomShadow, el.scrollHeight - el.clientHeight - el.scrollTop );
 	}
-	static #onscroll2( el, scroll ) {
-		if ( el ) {
+	static #onscroll2( elems, scroll ) {
+		elems.forEach( el => {
 			el.style.setProperty( "--gsuiScrollShadow-dist", `${ Math.min( scroll / 5, 5 ) }px` );
 			el.classList.toggle( "gsuiScrollShadow-shadowed", scroll > 0 );
-		}
+		} );
 	}
 }
