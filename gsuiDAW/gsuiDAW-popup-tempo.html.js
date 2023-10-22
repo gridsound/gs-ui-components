5a0b5ee95d68dcc370c8acb225f1b2eb270b1f7a
mr21 <thomastortorini@gmail.com>
2023-10-23 01:59:57 +0200

    gsuiDAW: fix, BPM tap button (link -> button)

--- gsuiDAW/gsuiDAW-popup-tempo.html.js
+++ gsuiDAW/gsuiDAW-popup-tempo.html.js
@@ -20,7 +20,7 @@ GSUsetTemplate( "gsui-daw-popup-tempo", () =>
 				GSUcreateDiv( { class: "gsuiPopup-row-title" }, "BPM (Beats Per Minute)" ),
 				GSUcreateDiv( { class: "gsuiPopup-row-values" },
 					GSUcreateInput( { class: "gsuiPopup-inputText", name: "bpm", type: "number", min: 1, max: 999.99, step: .01 } ),
-					GSUcreateA( { class: "gsuiDAW-bpmTap gsuiIcon", "data-icon": "tint" } ),
+					GSUcreateButton( { class: "gsuiDAW-bpmTap gsuiIcon", "data-icon": "tint" } ),
 				),
 			),
 		),
