6d0e52741aa3a64a37e84d5c33a034376c5b5413
mr21 <thomastortorini@gmail.com>
2024-02-11 00:12:27 +0100

    gsuiBeatlines: use of gsui0ne

--- gsuiBeatlines/gsuiBeatlines.js
+++ gsuiBeatlines/gsuiBeatlines.js
@@ -1,31 +1,30 @@
 "use strict";
 
-class gsuiBeatlines extends HTMLElement {
-	connectedCallback() {
-		if ( !this.firstChild ) {
-			this.append( ...GSUgetTemplate( "gsui-beatlines" ) );
-			GSUrecallAttributes( this, {
+class gsuiBeatlines extends gsui0ne {
+	constructor() {
+		super( {
+			$cmpName: "gsuiBeatlines",
+			$tagName: "gsui-beatlines",
+			$attributes: {
 				timedivision: "4/4",
 				coloredbeats: true,
 				pxperbeat: 10,
-			} );
-		}
+			},
+		} );
 	}
 	static get observedAttributes() {
 		return [ "timedivision", "pxperbeat" ];
 	}
-	attributeChangedCallback( prop, prev, val ) {
-		if ( prev !== val ) {
-			switch ( prop ) {
-				case "timedivision":
-					this.style.setProperty( "--gsui-bPM", `${ val.split( "/" )[ 0 ] }em` );
-					this.style.setProperty( "--gsui-sPB", `${ 1 / val.split( "/" )[ 1 ] }em` );
-					break;
-				case "pxperbeat":
-					this.style.fontSize = `${ val }px`;
-					this.style.opacity = Math.min( val / 48, 1 );
-					break;
-			}
+	$attributeChanged( prop, val ) {
+		switch ( prop ) {
+			case "timedivision":
+				this.style.setProperty( "--gsui-bPM", `${ val.split( "/" )[ 0 ] }em` );
+				this.style.setProperty( "--gsui-sPB", `${ 1 / val.split( "/" )[ 1 ] }em` );
+				break;
+			case "pxperbeat":
+				this.style.fontSize = `${ val }px`;
+				this.style.opacity = Math.min( val / 48, 1 );
+				break;
 		}
 	}
 }
