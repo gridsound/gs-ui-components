32edba176c7535e9c3970bd1b9e44c598121c282
mr21 <thomastortorini@gmail.com>
2024-02-17 06:50:29 +0100

    *: no return in .html.js files

--- gsuiEnvelope/gsuiEnvelope.html.js
+++ gsuiEnvelope/gsuiEnvelope.html.js
@@ -1,27 +1,25 @@
 "use strict";
 
-GSUsetTemplate( "gsui-envelope", () => {
-	return [
-		[
-			[ "attack", "attack", "att", 0, 1, .01 ],
-			[ "hold", "hold", "hold", 0, 1, .01 ],
-			[ "decay", "decay", "dec", 0, 1, .01 ],
-			[ "sustain", "sustain", "sus", 0, 1, .01 ],
-			[ "release", "release", "rel", 0, 4, .01 ],
-		].map( ( [ prop, title, text, min, max, step ] ) =>
-			GSUcreateDiv( { class: `gsuiEnvelope-prop gsuiEnvelope-${ prop }`, title },
-				GSUcreateDiv( { class: "gsuiEnvelope-propLabel" },
-					GSUcreateSpan( null, text ),
-					GSUcreateDiv( { class: "gsuiEnvelope-propValue" } ),
-				),
-				GSUcreateDiv( { class: "gsuiEnvelope-propContent" },
-					GSUcreateElement( "gsui-slider", { type: "linear-x", disabled: true, min, max, step, "mousemove-size": "800", "data-prop": prop } ),
-				),
-			)
-		),
-		GSUcreateDiv( { class: "gsuiEnvelope-graph" },
-			GSUcreateElement( "gsui-beatlines", { coloredbeats: "" } ),
-			GSUcreateElement( "gsui-envelope-graph" ),
-		),
-	].flat( 1 );
-} );
+GSUsetTemplate( "gsui-envelope", () => [
+	[
+		[ "attack", "attack", "att", 0, 1, .01 ],
+		[ "hold", "hold", "hold", 0, 1, .01 ],
+		[ "decay", "decay", "dec", 0, 1, .01 ],
+		[ "sustain", "sustain", "sus", 0, 1, .01 ],
+		[ "release", "release", "rel", 0, 4, .01 ],
+	].map( ( [ prop, title, text, min, max, step ] ) =>
+		GSUcreateDiv( { class: `gsuiEnvelope-prop gsuiEnvelope-${ prop }`, title },
+			GSUcreateDiv( { class: "gsuiEnvelope-propLabel" },
+				GSUcreateSpan( null, text ),
+				GSUcreateDiv( { class: "gsuiEnvelope-propValue" } ),
+			),
+			GSUcreateDiv( { class: "gsuiEnvelope-propContent" },
+				GSUcreateElement( "gsui-slider", { type: "linear-x", disabled: true, min, max, step, "mousemove-size": "800", "data-prop": prop } ),
+			),
+		)
+	),
+	GSUcreateDiv( { class: "gsuiEnvelope-graph" },
+		GSUcreateElement( "gsui-beatlines", { coloredbeats: "" } ),
+		GSUcreateElement( "gsui-envelope-graph" ),
+	),
+].flat( 1 ) );
--- gsuiSliderGroup/gsuiSliderGroup.html.js
+++ gsuiSliderGroup/gsuiSliderGroup.html.js
@@ -1,23 +1,19 @@
 "use strict";
 
-GSUsetTemplate( "gsui-slidergroup", () => {
-	return (
-		GSUcreateDiv( { class: "gsuiSliderGroup-slidersWrap" },
-			GSUcreateDiv( { class: "gsuiSliderGroup-sliders" },
-				GSUcreateElement( "gsui-beatlines", { coloredbeats: "" } ),
-				GSUcreateDiv( { class: "gsuiSliderGroup-currentTime" } ),
-				GSUcreateDiv( { class: "gsuiSliderGroup-defaultValue" } ),
-				GSUcreateDiv( { class: "gsuiSliderGroup-loop gsuiSliderGroup-loopA" } ),
-				GSUcreateDiv( { class: "gsuiSliderGroup-loop gsuiSliderGroup-loopB" } ),
-			),
-		)
-	);
-} );
+GSUsetTemplate( "gsui-slidergroup", () =>
+	GSUcreateDiv( { class: "gsuiSliderGroup-slidersWrap" },
+		GSUcreateDiv( { class: "gsuiSliderGroup-sliders" },
+			GSUcreateElement( "gsui-beatlines", { coloredbeats: "" } ),
+			GSUcreateDiv( { class: "gsuiSliderGroup-currentTime" } ),
+			GSUcreateDiv( { class: "gsuiSliderGroup-defaultValue" } ),
+			GSUcreateDiv( { class: "gsuiSliderGroup-loop gsuiSliderGroup-loopA" } ),
+			GSUcreateDiv( { class: "gsuiSliderGroup-loop gsuiSliderGroup-loopB" } ),
+		),
+	)
+);
 
-GSUsetTemplate( "gsui-slidergroup-slider", () => {
-	return (
-		GSUcreateDiv( { class: "gsuiSliderGroup-slider" },
-			GSUcreateDiv( { class: "gsuiSliderGroup-sliderInner" } ),
-		)
-	);
-} );
+GSUsetTemplate( "gsui-slidergroup-slider", () =>
+	GSUcreateDiv( { class: "gsuiSliderGroup-slider" },
+		GSUcreateDiv( { class: "gsuiSliderGroup-sliderInner" } ),
+	)
+);
