cfccae4eebf25d26816216009fe77353a20fcb8b
mr21 <thomastortorini@gmail.com>
2024-04-15 00:52:43 +0200

    gsuiEnvelope: use of gsui0ne

--- gsuiEnvelope/gsuiEnvelope.js
+++ gsuiEnvelope/gsuiEnvelope.js
@@ -1,25 +1,35 @@
 "use strict";
 
-class gsuiEnvelope extends HTMLElement {
+class gsuiEnvelope extends gsui0ne {
 	#dur = 4;
 	#waveWidth = 300;
-	#dispatch = GSUdispatchEvent.bind( null, this, "gsuiEnvelope" );
 	#onresizeBind = this.#onresize.bind( this );
-	#children = GSUgetTemplate( "gsui-envelope" );
-	#elements = GSUfindElements( this.#children, {
-		beatlines: "gsui-beatlines",
-		graph: "gsui-envelope-graph",
-		sliders: {
-			attack:  [ ".gsuiEnvelope-attack  gsui-slider", ".gsuiEnvelope-attack  .gsuiEnvelope-propValue" ],
-			hold:    [ ".gsuiEnvelope-hold    gsui-slider", ".gsuiEnvelope-hold    .gsuiEnvelope-propValue" ],
-			decay:   [ ".gsuiEnvelope-decay   gsui-slider", ".gsuiEnvelope-decay   .gsuiEnvelope-propValue" ],
-			sustain: [ ".gsuiEnvelope-sustain gsui-slider", ".gsuiEnvelope-sustain .gsuiEnvelope-propValue" ],
-			release: [ ".gsuiEnvelope-release gsui-slider", ".gsuiEnvelope-release .gsuiEnvelope-propValue" ],
-		},
-	} );
 
 	constructor() {
-		super();
+		super( {
+			$cmpName: "gsuiEnvelope",
+			$tagName: "gsui-envelope",
+			$elements: {
+				$beatlines: "gsui-beatlines",
+				$graph: "gsui-envelope-graph",
+				$sliders: {
+					attack:  [ ".gsuiEnvelope-attack  gsui-slider", ".gsuiEnvelope-attack  .gsuiEnvelope-propValue" ],
+					hold:    [ ".gsuiEnvelope-hold    gsui-slider", ".gsuiEnvelope-hold    .gsuiEnvelope-propValue" ],
+					decay:   [ ".gsuiEnvelope-decay   gsui-slider", ".gsuiEnvelope-decay   .gsuiEnvelope-propValue" ],
+					sustain: [ ".gsuiEnvelope-sustain gsui-slider", ".gsuiEnvelope-sustain .gsuiEnvelope-propValue" ],
+					release: [ ".gsuiEnvelope-release gsui-slider", ".gsuiEnvelope-release .gsuiEnvelope-propValue" ],
+				},
+			},
+			$attributes: {
+				toggle: false,
+				timedivision: "4/4",
+				attack: .1,
+				hold: .1,
+				decay: .1,
+				sustain: .8,
+				release: 1,
+			},
+		} );
 		Object.seal( this );
 
 		GSUlistenEvents( this, {
@@ -37,48 +47,35 @@ class gsuiEnvelope extends HTMLElement {
 	}
 
 	// .........................................................................
-	connectedCallback() {
-		if ( this.#children ) {
-			this.append( ...this.#children );
-			this.#children = null;
-			GSUrecallAttributes( this, {
-				toggle: false,
-				timedivision: "4/4",
-				attack: .1,
-				hold: .1,
-				decay: .1,
-				sustain: .8,
-				release: 1,
-			} );
-			this.updateWave();
-		}
+	$firstTimeConnected() {
+		this.updateWave();
+	}
+	$connected() {
 		GSUobserveSizeOf( this, this.#onresizeBind );
 	}
-	disconnectedCallback() {
+	$disconnected() {
 		GSUunobserveSizeOf( this, this.#onresizeBind );
 	}
 	static get observedAttributes() {
 		return [ "toggle", "attack", "hold", "decay", "sustain", "release" ];
 	}
-	attributeChangedCallback( prop, prev, val ) {
-		if ( prev !== val ) {
-			switch ( prop ) {
-				case "toggle": this.#changeToggle( val !== null ); break;
-				case "timedivision": this.#changeTimedivision( val ); break;
-				case "attack":
-				case "hold":
-				case "decay":
-				case "sustain":
-				case "release":
-					this.#changeProp( prop, +val );
-					break;
-			}
+	$attributeChanged( prop, val ) {
+		switch ( prop ) {
+			case "toggle": this.#changeToggle( val !== null ); break;
+			case "timedivision": this.#changeTimedivision( val ); break;
+			case "attack":
+			case "hold":
+			case "decay":
+			case "sustain":
+			case "release":
+				this.#changeProp( prop, +val );
+				break;
 		}
 	}
 
 	// .........................................................................
 	updateWave( prop, val ) {
-		const g = this.#elements.graph;
+		const g = this.$elements.$graph;
 
 		g.$attack = prop === "attack" ? val : GSUgetAttributeNum( this, "attack" );
 		g.$hold = prop === "hold" ? val : GSUgetAttributeNum( this, "hold" );
@@ -93,40 +90,40 @@ class gsuiEnvelope extends HTMLElement {
 
 	// .........................................................................
 	#changeToggle( b ) {
-		GSUsetAttribute( this.#elements.sliders.attack[ 0 ], "disabled", !b );
-		GSUsetAttribute( this.#elements.sliders.hold[ 0 ], "disabled", !b );
-		GSUsetAttribute( this.#elements.sliders.decay[ 0 ], "disabled", !b );
-		GSUsetAttribute( this.#elements.sliders.sustain[ 0 ], "disabled", !b );
-		GSUsetAttribute( this.#elements.sliders.release[ 0 ], "disabled", !b );
+		GSUsetAttribute( this.$elements.$sliders.attack[ 0 ], "disabled", !b );
+		GSUsetAttribute( this.$elements.$sliders.hold[ 0 ], "disabled", !b );
+		GSUsetAttribute( this.$elements.$sliders.decay[ 0 ], "disabled", !b );
+		GSUsetAttribute( this.$elements.$sliders.sustain[ 0 ], "disabled", !b );
+		GSUsetAttribute( this.$elements.$sliders.release[ 0 ], "disabled", !b );
 	}
 	#changeTimedivision( val ) {
-		GSUsetAttribute( this.#elements.beatlines, "timedivision", val );
+		GSUsetAttribute( this.$elements.$beatlines, "timedivision", val );
 		this.updateWave();
 	}
 	#changeProp( prop, val ) {
-		const [ sli, span ] = this.#elements.sliders[ prop ];
+		const [ sli, span ] = this.$elements.$sliders[ prop ];
 
 		sli.$setValue( val );
 		span.textContent = val.toFixed( 2 );
 	}
 	#updatePxPerBeat() {
-		GSUsetAttribute( this.#elements.beatlines, "pxperbeat", this.#waveWidth / this.#dur );
+		GSUsetAttribute( this.$elements.$beatlines, "pxperbeat", this.#waveWidth / this.#dur );
 	}
 
 	// .........................................................................
 	#onresize() {
-		this.#waveWidth = this.#elements.beatlines.getBoundingClientRect().width;
+		this.#waveWidth = this.$elements.$beatlines.getBoundingClientRect().width;
 		this.#updatePxPerBeat();
-		this.#elements.graph.resized();
+		this.$elements.$graph.resized();
 	}
 	#oninputSlider( prop, val ) {
-		this.#elements.sliders[ prop ][ 1 ].textContent = val.toFixed( 2 );
+		this.$elements.$sliders[ prop ][ 1 ].textContent = val.toFixed( 2 );
 		this.updateWave( prop, val );
-		this.#dispatch( "liveChange", prop, val );
+		this.$dispatch( "liveChange", prop, val );
 	}
 	#onchangeSlider( prop, val ) {
 		GSUsetAttribute( this, prop, val );
-		this.#dispatch( "change", prop, val );
+		this.$dispatch( "change", prop, val );
 	}
 }
 
