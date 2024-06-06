32399be6d3a01a7f6571aa8bfbe9a4ea403c557f
mr21 <thomastortorini@gmail.com>
2024-06-06 04:40:45 +0200

    gsuiAnalyserTime: pinch + amp

--- gsuiAnalyserTime/gsuiAnalyserTime.js
+++ gsuiAnalyserTime/gsuiAnalyserTime.js
@@ -2,8 +2,10 @@
 
 class gsuiAnalyserTime extends gsui0ne {
 	#ctx = null;
+	#amp = 1;
 	#pinch = 0;
-	#setResDeb = GSUdebounce( this.$setResolution.bind( this ), 200 );
+	#color = "#fff";
+	#setResDeb = GSUdebounce( this.#setResolution.bind( this ), 200 );
 
 	constructor() {
 		super( {
@@ -11,7 +13,9 @@ class gsuiAnalyserTime extends gsui0ne {
 			$tagName: "gsui-analysertime",
 			$template: GSUcreateElement( "canvas" ),
 			$attributes: {
+				amp: 1,
 				pinch: 1,
+				color: "#fff",
 			},
 		} );
 		Object.seal( this );
@@ -20,33 +24,35 @@ class gsuiAnalyserTime extends gsui0ne {
 
 	// .........................................................................
 	static get observedAttributes() {
-		return [ "pinch" ];
+		return [ "amp", "color", "pinch" ];
 	}
 	$attributeChanged( prop, val ) {
 		switch ( prop ) {
+			case "amp": this.#amp = +val; break;
+			case "color": this.#color = val; break;
 			case "pinch": this.#pinch = +val; break;
 		}
 	}
 	$onresize( w, h ) {
 		this.#setResDeb( w, h );
 	}
+	#setResolution( w, h ) {
+		this.$element.width = w;
+		this.$element.height = h;
+	}
 
 	// .........................................................................
 	$clear() {
 		this.#ctx.clearRect( 0, 0, this.$element.width, this.$element.height );
 	}
-	$setResolution( w, h ) {
-		this.$element.width = w;
-		this.$element.height = h;
-	}
 	$draw( data ) {
 		const ctx = this.#ctx;
 		const xInc = this.$element.width / data.length;
-		const calcY = gsuiAnalyserTime.#calcY.bind( null, data, this.$element.height / 2, this.#pinch / 2 );
+		const calcY = gsuiAnalyserTime.#calcY.bind( null, data, this.#amp, this.$element.height / 2, this.#pinch / 2 );
 
 		this.$clear();
-		ctx.strokeStyle = "#fff";
-		ctx.lineWidth = 2;
+		ctx.strokeStyle = this.#color;
+		ctx.lineWidth = Math.max( 2, this.$element.height / 150 );
 		ctx.beginPath();
 		ctx.moveTo( 0, calcY( 0 ) );
 		for ( let i = 1; i < data.length; ++i ) {
@@ -54,14 +60,15 @@ class gsuiAnalyserTime extends gsui0ne {
 		}
 		ctx.stroke();
 	}
-	static #calcY( data, h2, pin, i ) {
-		const perc = i / data.length;
+	static #calcY( data, amp, h2, pin, i ) {
+		const perc = i / ( data.length - 1 );
 		const pin2 =
 			perc <     pin ?       perc   / pin :
 			perc > 1 - pin ? ( 1 - perc ) / pin : 1;
 		const d = data[ i ] / 256 * 2 - 1;
+		const d2 = Math.max( -1, Math.min( d * amp, 1 ) );
 
-		return h2 + d * pin2 * h2;
+		return h2 + d2 * GSUeaseOutCirc( pin2 ) * h2;
 	}
 }
 
