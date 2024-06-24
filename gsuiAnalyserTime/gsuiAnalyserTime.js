2be335c2b46783e43eac3262e49903da9c991bd3
mr21 <thomastortorini@gmail.com>
2024-06-24 02:31:54 +0200

    gsuiAnalyserTime: smooth

--- gsuiAnalyserTime/gsuiAnalyserTime.js
+++ gsuiAnalyserTime/gsuiAnalyserTime.js
@@ -5,6 +5,8 @@ class gsuiAnalyserTime extends gsui0ne {
 	#amp = 1;
 	#pinch = 0;
 	#color = "#fff";
+	#smooth = 0;
+	#drawMax = 0;
 	#setResDeb = GSUdebounce( this.#setResolution.bind( this ), 200 );
 
 	constructor() {
@@ -16,6 +18,7 @@ class gsuiAnalyserTime extends gsui0ne {
 				amp: 1,
 				pinch: 1,
 				color: "#fff",
+				smooth: 0,
 			},
 		} );
 		Object.seal( this );
@@ -24,13 +27,14 @@ class gsuiAnalyserTime extends gsui0ne {
 
 	// .........................................................................
 	static get observedAttributes() {
-		return [ "amp", "color", "pinch" ];
+		return [ "amp", "color", "pinch", "smooth" ];
 	}
 	$attributeChanged( prop, val ) {
 		switch ( prop ) {
 			case "amp": this.#amp = +val; break;
 			case "color": this.#color = val; break;
 			case "pinch": this.#pinch = +val; break;
+			case "smooth": this.#smooth = GSUeaseOutCirc( +val ); break;
 		}
 	}
 	$onresize( w, h ) {
@@ -47,17 +51,29 @@ class gsuiAnalyserTime extends gsui0ne {
 	}
 	$draw( data ) {
 		const ctx = this.#ctx;
-		const xInc = this.$element.width / data.length;
-		const calcY = gsuiAnalyserTime.#calcY.bind( null, data, this.#amp, this.$element.height / 2, this.#pinch / 2 );
+		const w = this.$element.width;
+		const h = this.$element.height;
+		const xInc = w / data.length;
+		const calcY = gsuiAnalyserTime.#calcY.bind( null, data, this.#amp, h / 2, this.#pinch / 2 );
+		const r = Math.round( 255 - this.#drawMax * 255 );
+		const g = Math.round( this.#drawMax *  64 );
+		const b = Math.round( this.#drawMax * 255 );
 
-		this.$clear();
-		ctx.strokeStyle = this.#color;
-		ctx.lineWidth = Math.max( 2, this.$element.height / 150 );
+		ctx.globalCompositeOperation = "source-in";
+		ctx.fillStyle = `rgba(${ r },${ g },${ b },${ this.#smooth })`;
+		ctx.globalAlpha = 1;
+		ctx.fillRect( 0, 0, w, h );
 		ctx.beginPath();
 		ctx.moveTo( 0, calcY( 0 ) );
+		this.#drawMax = 0;
 		for ( let i = 1; i < data.length; ++i ) {
+			this.#drawMax = Math.max( Math.abs( data[ i ] ), this.#drawMax );
 			ctx.lineTo( i * xInc, calcY( i ) );
 		}
+		ctx.globalCompositeOperation = "source-over";
+		ctx.lineWidth = Math.max( 2, h / 150 ) + Math.round( 1 * this.#drawMax );
+		ctx.strokeStyle = this.#color;
+		ctx.globalAlpha = GSUeaseOutCirc( this.#drawMax, 8 );
 		ctx.stroke();
 	}
 	static #calcY( data, amp, h2, pin, i ) {
