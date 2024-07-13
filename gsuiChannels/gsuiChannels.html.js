cfcc859d654197284489e4a1a6a6f57290bac3be
mr21 <thomastortorini@gmail.com>
2024-07-13 06:04:18 +0200

    gsuiChannels: VU meter max 120%

--- gsuiChannels/gsuiChannels.html.js
+++ gsuiChannels/gsuiChannels.html.js
@@ -2,7 +2,7 @@
 
 GSUsetTemplate( "gsui-channels", () => [
 	GSUcreateDiv( { class: "gsuiChannels-panMain" },
-		GSUcreateElement( "gsui-analyser-vu", { title: "VU of selected channel" } ),
+		GSUcreateElement( "gsui-analyser-vu", { max: 120, title: "current channel's VU (max 120%)" } ),
 	),
 	GSUcreateDiv( { class: "gsuiChannels-panChannels" },
 		GSUcreateButton( { class: "gsuiChannels-addChan", title: "Add a channel" },
