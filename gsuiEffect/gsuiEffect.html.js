892ba27f7ca6bb56f2dfed388efb2f9a33ec3894
mr21 <thomastortorini@gmail.com>
2024-08-15 08:43:57 +0200

    gsuiEffect: toggle off by default

--- gsuiEffect/gsuiEffect.html.js
+++ gsuiEffect/gsuiEffect.html.js
@@ -4,7 +4,7 @@ GSUsetTemplate( "gsui-effect", () => [
 	GSUcreateDiv( { class: "gsuiEffect-head" },
 		GSUcreateDiv( { class: "gsuiEffect-grip gsuiIcon", "data-icon": "grip-v" } ),
 		GSUcreateButton( { class: "gsuiEffect-expand gsuiIcon", "data-icon": "caret-right" } ),
-		GSUcreateElement( "gsui-toggle", { title: "Toggle this effect" } ),
+		GSUcreateElement( "gsui-toggle", { off: true, title: "Toggle this effect" } ),
 		GSUcreateSpan( { class: "gsuiEffect-name" } ),
 		GSUcreateButton( { class: "gsuiEffect-remove gsuiIcon", "data-icon": "close", title: "Delete this effect" } ),
 	),
