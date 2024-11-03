8dba6bd26c19936a66b7ff99f3b0f6bc3d284985
mr21 <thomastortorini@gmail.com>
2024-11-03 03:44:49 +0100

    gsuiComPlaylist: handle deletion/restoring/forking

--- gsuiComPlaylist/gsuiComPlaylist.js
+++ gsuiComPlaylist/gsuiComPlaylist.js
@@ -4,6 +4,9 @@ class gsuiComPlaylist extends gsui0ne {
 	#dawURL = "";
 	#cmps = new Map();
 	#itsMe = false;
+	#forkPromise = null;
+	#deletePromise = null;
+	#restorePromise = null;
 
 	constructor() {
 		super( {
@@ -17,10 +20,16 @@ class gsuiComPlaylist extends gsui0ne {
 			},
 		} );
 		Object.seal( this );
-		this.#setNbCmps( 0 );
-		this.#setNbBin( 0 );
+		this.#updateNbCmps();
 		this.$elements.$headLinkCmps.onclick = () => GSUsetAttribute( this, "bin", false );
 		this.$elements.$headLinkBin.onclick = () => GSUsetAttribute( this, "bin", true );
+		GSUlistenEvents( this, {
+			gsuiComPlayer: {
+				fork: ( d, t ) => this.#onforkComposition( t ),
+				delete: ( d, t ) => this.#ondeleteRestoreCmp( t, "delete" ),
+				restore: ( d, t ) => this.#ondeleteRestoreCmp( t, "restore" ),
+			},
+		} );
 	}
 
 	// .........................................................................
@@ -37,14 +46,14 @@ class gsuiComPlaylist extends gsui0ne {
 	}
 
 	// .........................................................................
-	$setDAWURL( url ) {
-		this.#dawURL = url;
-	}
+	$setDAWURL( url ) { this.#dawURL = url; }
+	$setForkCallbackPromise( fn ) { this.#forkPromise = fn; }
+	$setDeleteCallbackPromise( fn ) { this.#deletePromise = fn; }
+	$setRestoreCallbackPromise( fn ) { this.#restorePromise = fn; }
 	$clearCompositions() {
 		GSUemptyElement( this.$elements.$listCmps );
 		GSUemptyElement( this.$elements.$listBin );
-		this.#setNbCmps( 0 );
-		this.#setNbBin( 0 );
+		this.#updateNbCmps();
 		this.#cmps.clear();
 	}
 	$changeCompositionProp( id, prop, val ) {
@@ -60,29 +69,82 @@ class gsuiComPlaylist extends gsui0ne {
 				: this.$elements.$listCmps
 			).append( elCmp );
 		} );
-		this.#setNbCmps( this.$elements.$listCmps.childElementCount );
-		this.#setNbBin( this.$elements.$listBin.childElementCount );
+		this.#updateNbCmps();
 	}
 
 	// .........................................................................
-	#setNbCmps( n ) { this.$elements.$headLinkCmps.firstChild.textContent = n; }
-	#setNbBin( n ) { this.$elements.$headLinkBin.firstChild.textContent = n; }
+	#updateNbCmps() {
+		this.$elements.$headLinkCmps.firstChild.textContent = this.$elements.$listCmps.childElementCount;
+		this.$elements.$headLinkBin.firstChild.textContent = this.$elements.$listBin.childElementCount;
+	}
 	#createCmp( cmp ) {
 		const id = cmp.id;
-		const del = cmp.deleted;
-
-		return GSUcreateElement( "gsui-com-player", {
+		const elCmp = GSUcreateElement( "gsui-com-player", {
 			"data-id": id,
-			link: del ? false : `#/cmp/${ id }`,
-			dawlink: del ? false : `${ this.#dawURL }#${ id }`,
 			itsmine: this.#itsMe,
 			private: !cmp.public,
 			opensource: cmp.opensource,
 			name: cmp.name,
 			bpm: cmp.bpm,
-			duration: cmp.duration * 60 / cmp.bpm,
-			// actions: !this.#itsMe ? false : del ? "restore" : "fork delete",
+			duration: cmp.durationSec,
 		} );
+
+		this.#updateCmpLinks( elCmp, cmp.deleted );
+		return elCmp;
+	}
+	#updateCmpLinks( elCmp, del ) {
+		GSUsetAttribute( elCmp, {
+			link: del ? false : `#/cmp/${ elCmp.dataset.id }`,
+			dawlink: del ? false : `${ this.#dawURL }#${ elCmp.dataset.id }`,
+			actions: !this.#itsMe ? "fork" : del ? "restore" : "fork delete",
+		} );
+	}
+
+	// .........................................................................
+	#onforkComposition( elCmp ) {
+		const id = elCmp.dataset.id;
+
+		GSUsetAttribute( elCmp, "actionloading", true );
+		this.#forkPromise?.( id )
+			.then( obj => {
+				const elNewCmp = this.#itsMe && this.#createCmp( obj );
+
+				if ( elNewCmp ) {
+					GSUsetAttribute( elNewCmp, "forking", true );
+					this.$elements.$listCmps.prepend( elNewCmp );
+					this.#updateNbCmps();
+					GSUsetAttribute( elCmp, "actionloading", false );
+					setTimeout( () => {
+						GSUsetAttribute( elNewCmp, "forking", false );
+					}, 350 );
+				}
+			} )
+			.finally( () => GSUsetAttribute( elCmp, "actionloading", false ) );
+	}
+	#ondeleteRestoreCmp( elCmp, act ) {
+		const id = elCmp.dataset.id;
+		const del = act === "delete";
+		const prom = del ? this.#deletePromise : this.#restorePromise;
+
+		GSUsetAttribute( elCmp, "actionloading", true );
+		prom?.( id )
+			.then( () => {
+				const attr = del ? "deleting" : "restoring";
+
+				GSUsetAttribute( elCmp, attr, true );
+				setTimeout( () => {
+					del
+						? this.$elements.$listBin.prepend( elCmp )
+						: this.$elements.$listCmps.prepend( elCmp );
+					GSUsetAttribute( elCmp, attr, false );
+					GSUsetAttribute( elCmp, "actionloading", false );
+					this.#updateNbCmps();
+					this.#updateCmpLinks( elCmp, del );
+				}, 350 );
+			} )
+			.catch( () => {
+				GSUsetAttribute( elCmp, "actionloading", false );
+			} );
 	}
 }
 
