64fc407ac2d9ecfa1965583011a0951bd3f28bdc
mr21 <thomastortorini@gmail.com>
2024-11-10 07:44:39 +0100

    gsuiDropdown

---
+++ gsuiDropdown/gsuiDropdown.js
@@ -0,0 +1,99 @@
+"use strict";
+
+class gsuiDropdown {
+	#elBtn = null;
+	#elem = null;
+	#isOpen = false;
+	#direction = "bottom";
+	#elParent = document.body;
+	#timeoutId = null;
+	#onptrdownBodyBind = this.#onptrdownBody.bind( this );
+	#onbeforeOpening = GSUnoop;
+	#onopenCreateElement = null;
+
+	$bindTargetElement( btn ) {
+		this.#elBtn = btn;
+		btn.addEventListener( "click", this.#onclickTarget.bind( this ) );
+	}
+	$setParent( el ) {
+		this.#elParent = el;
+	}
+	$setDirection( dir ) {
+		this.#direction = dir;
+	}
+	$onbeforeOpening( fn ) {
+		this.#onbeforeOpening = fn;
+	}
+	$onopenCreateElement( fn ) {
+		this.#onopenCreateElement = fn;
+	}
+	$close() {
+		this.#close();
+	}
+
+	// .........................................................................
+	#onclickTarget() {
+		this.#isOpen
+			? this.#close()
+			: this.#open();
+	}
+	#onptrdownBody( e ) {
+		if ( !this.#elBtn.contains( e.target ) && !this.#elem.contains( e.target ) ) {
+			this.#close();
+		}
+	}
+	#open() {
+		if ( !this.#isOpen && this.#onbeforeOpening() !== false ) {
+			this.#isOpen = true;
+			clearTimeout( this.#timeoutId );
+			if ( this.#elem ) {
+				GSUsetAttribute( this.#elem, "data-open", true );
+			} else {
+				this.#elem = this.#createElement();
+				this.#elParent.prepend( this.#elem );
+				document.body.addEventListener( "pointerdown", this.#onptrdownBodyBind );
+				this.#timeoutId = setTimeout( () => {
+					this.#positionElement();
+					GSUsetAttribute( this.#elem, "data-open", true );
+				}, 10 );
+			}
+		}
+	}
+	#close() {
+		if ( this.#isOpen ) {
+			this.#isOpen = false;
+			clearTimeout( this.#timeoutId );
+			GSUsetAttribute( this.#elem, "data-open", false );
+			document.body.removeEventListener( "pointerdown", this.#onptrdownBodyBind );
+			this.#timeoutId = setTimeout( () => {
+				this.#elem.remove();
+				this.#elem = null;
+			}, 250 );
+		}
+	}
+	#createElement() {
+		return GSUcreateDiv( { class: "gsuiDropdown", "data-dir": this.#direction },
+			GSUcreateDiv( { class: "gsuiDropdown-arrow", inert: true } ),
+			GSUcreateDiv( { class: "gsuiDropdown-content" }, this.#onopenCreateElement?.() ),
+		);
+	}
+	#positionElement() {
+		const tarBCR = this.#elBtn.getBoundingClientRect();
+		const menuBCR = this.#elem.getBoundingClientRect();
+		const elArrow = this.#elem.querySelector( ".gsuiDropdown-arrow" );
+		const posObj = getAbsPos( this.#direction, tarBCR, this.#elem.clientWidth, this.#elem.clientHeight, {
+			margin: 8,
+			withArrow: true,
+			absolute: this.#elParent === document.body,
+		} );
+
+		GSUsetStyle( this.#elem, {
+			left: posObj.left,
+			top: posObj.top,
+		} );
+		GSUsetStyle( elArrow, {
+			left: posObj.arrowLeft,
+			top: posObj.arrowTop,
+		} );
+	}
+}
