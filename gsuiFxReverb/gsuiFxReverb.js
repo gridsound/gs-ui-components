524036c274dbc9dc6996144655dfd8bdf1046288
mr21 <thomastortorini@gmail.com>
2024-12-07 06:44:09 +0100

    gsuiFxReverb: fadein + decay

--- gsuiFxReverb/gsuiFxReverb.js
+++ gsuiFxReverb/gsuiFxReverb.js
@@ -9,9 +9,13 @@ class gsuiFxReverb extends gsui0ne {
 				$drySli: "[data-prop='dry'] gsui-slider",
 				$wetSli: "[data-prop='wet'] gsui-slider",
 				$delSli: "[data-prop='delay'] gsui-slider",
+				$decSli: "[data-prop='decay'] gsui-slider",
+				$fadeinSli: "[data-prop='fadein'] gsui-slider",
 				$dryValue: "[data-prop='dry'] .gsuiEffect-param-value",
 				$wetValue: "[data-prop='wet'] .gsuiEffect-param-value",
 				$delValue: "[data-prop='delay'] .gsuiEffect-param-value",
+				$decValue: "[data-prop='decay'] .gsuiEffect-param-value",
+				$fadeinValue: "[data-prop='fadein'] .gsuiEffect-param-value",
 				$beatlines: "gsui-beatlines",
 				$graphDry: ".gsuiFxReverb-graph-dry",
 				$graphWet: ".gsuiFxReverb-graph-wet",
@@ -20,6 +24,8 @@ class gsuiFxReverb extends gsui0ne {
 				dry: 0,
 				wet: 0,
 				delay: 0,
+				decay: 1,
+				fadein: 0,
 				timedivision: "4/4",
 			},
 		} );
@@ -45,7 +51,7 @@ class gsuiFxReverb extends gsui0ne {
 		this.#updateWetPos();
 	}
 	static get observedAttributes() {
-		return [ "timedivision", "dry", "wet", "delay" ];
+		return [ "timedivision", "dry", "wet", "delay", "decay", "fadein" ];
 	}
 	$attributeChanged( prop, val ) {
 		switch ( prop ) {
@@ -69,6 +75,16 @@ class gsuiFxReverb extends gsui0ne {
 				GSUsetAttribute( this.$elements.$delSli, "value", val );
 				this.#updateWetPos();
 				break;
+			case "decay":
+				this.$elements.$decValue.textContent = ( +val ).toFixed( 2 );
+				GSUsetAttribute( this.$elements.$decSli, "value", val );
+				this.#updateWetPos();
+				break;
+			case "fadein":
+				this.$elements.$fadeinValue.textContent = ( +val ).toFixed( 2 );
+				GSUsetAttribute( this.$elements.$fadeinSli, "value", val );
+				this.#updateWetPos();
+				break;
 		}
 	}
 
@@ -89,9 +105,14 @@ class gsuiFxReverb extends gsui0ne {
 	}
 	#updateWetPos() {
 		const delay = GSUgetAttributeNum( this, "delay" );
+		const decay = GSUgetAttributeNum( this, "decay" );
+		const fadein = GSUgetAttributeNum( this, "fadein" );
 		const ppb = GSUgetAttributeNum( this.$elements.$beatlines, "pxperbeat" );
+		const graphW = this.$elements.$beatlines.clientWidth;
 
-		this.$elements.$graphWet.style.left = `${ ( delay * ppb ) / this.$elements.$beatlines.clientWidth * 100 }%`;
+		this.$elements.$graphWet.style.left = `${ ( delay * ppb ) / graphW * 100 }%`;
+		this.$elements.$graphWet.style.width = `${ ( ( fadein + decay ) * ppb ) / graphW * 100 }%`;
+		GSUsetStyle( this, "--gsui-fadein-p", `${ fadein / ( fadein + decay ) * 100 | 0 }%` );
 	}
 }
 
