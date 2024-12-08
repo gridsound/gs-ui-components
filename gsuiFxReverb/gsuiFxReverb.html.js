789c1d7e7cf08c5cb9d8e3c399a1f3d991c19c09
mr21 <thomastortorini@gmail.com>
2024-12-08 05:26:11 +0100

    gsuiFxReverb: wet 1000%

--- gsuiFxReverb/gsuiFxReverb.html.js
+++ gsuiFxReverb/gsuiFxReverb.html.js
@@ -8,7 +8,7 @@ GSUsetTemplate( "gsui-fx-reverb", () => [
 	),
 	GSUcreateDiv( { class: "gsuiEffect-param-row", "data-prop": "wet" },
 		GSUcreateSpan( { class: "gsuiEffect-param-label" }, "wet" ),
-		GSUcreateElement( "gsui-slider", { type: "linear-x", min: 0, max: 1, step: .01 } ),
+		GSUcreateElement( "gsui-slider", { type: "linear-x", min: 0, max: 10, step: .01 } ),
 		GSUcreateSpan( { class: "gsuiEffect-param-value" } ),
 	),
 	GSUcreateDiv( { class: "gsuiEffect-param-row", "data-prop": "delay" },
