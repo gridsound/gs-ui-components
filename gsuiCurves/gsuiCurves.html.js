c2f36dd49580ad4191acd742bbd14280f106843f
mr21 <thomastortorini@gmail.com>
2025-02-13 13:47:32 +0100

    gsuiCurves: CSS, set gsui-analyser-hz bgcolor + curve shadow

--- gsuiCurves/gsuiCurves.html.js
+++ gsuiCurves/gsuiCurves.html.js
@@ -3,7 +3,7 @@
 GSUsetTemplate( "gsui-curves", () => [
 	GSUcreateDiv( { class: "gsuiCurves-marks" } ),
 	GSUcreateDiv( { class: "gsuiCurves-graph" },
-		GSUcreateElement( "gsui-analyser-hz" ),
+		GSUcreateElement( "gsui-analyser-hz", { bgcolor: "55 55 66" } ),
 		GSUcreateElementSVG( "svg", { class: "gsuiCurves-svg", preserveAspectRatio: "none" },
 			GSUcreateElementSVG( "line", { class: "gsuiCurves-line", "shape-rendering": "crispEdges" } ),
 			GSUcreateElementSVG( "g", { class: "gsuiCurves-curves" } ),
