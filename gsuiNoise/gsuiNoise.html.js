00d1528b04bc5398e1b7fcbd2ec1ad8650d9b281
mr21 <thomastortorini@gmail.com>
2025-02-16 08:48:47 +0100

    gsuiNoise: sliders mousemove-size

--- gsuiNoise/gsuiNoise.html.js
+++ gsuiNoise/gsuiNoise.html.js
@@ -2,9 +2,9 @@
 
 GSUsetTemplate( "gsui-noise", txt => [
 	GSUcreateSpan( { class: "gsuiNoise-type" }, "white noise" ),
-	GSUcreateElement( "gsui-slider", { "data-prop": "gain", type: "linear-x", min: 0, max: 1, step: .001 } ),
+	GSUcreateElement( "gsui-slider", { "data-prop": "gain", type: "linear-x", min: 0, max: 1, step: .001, "mousemove-size": 400 } ),
 	GSUcreateSpan( { class: "gsuiNoise-value", "data-prop": "gain" } ),
 	GSUcreateSpan( null, "pan" ),
-	GSUcreateElement( "gsui-slider", { "data-prop": "pan", type: "linear-x", min: -1, max: 1, step: .001 } ),
+	GSUcreateElement( "gsui-slider", { "data-prop": "pan", type: "linear-x", min: -1, max: 1, step: .001, "mousemove-size": 400 } ),
 	GSUcreateSpan( { class: "gsuiNoise-value", "data-prop": "pan" } ),
 ] );
