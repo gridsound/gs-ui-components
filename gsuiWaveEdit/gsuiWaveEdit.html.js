979db6507f5b4b823c1600d6f5ec22823b2bfede
mr21 <thomastortorini@gmail.com>
2025-03-01 12:26:01 +0100

    gsuiWaveEdit: change steps

--- gsuiWaveEdit/gsuiWaveEdit.html.js
+++ gsuiWaveEdit/gsuiWaveEdit.html.js
@@ -2,10 +2,10 @@
 
 GSUsetTemplate( "gsui-wave-edit", () => [
 	GSUcreateSpan( { class: "gsuiWaveEdit-head" },
-		GSUcreateButton( { class: "gsuiWaveEdit-back gsuiIcon", "data-action": "back", "data-icon": "arrow-left" } ),
+		GSUcreateButton( { class: "gsuiWaveEdit-back gsuiIcon", "data-action": "back", "data-icon": "arrow-left", title: "Save and close wave edition" } ),
 		GSUcreateSpan( { class: "gsuiWaveEdit-title", inert: true }, "wave-edition" ),
 	),
 	GSUcreateDiv( { class: "gsuiWaveEdit-graph" },
-		GSUcreateElement( "gsui-dotline", { beatlines: true, viewbox: "0 -1 1 1", xstep: 1 / 128, ystep: 1 / 16 } ),
+		GSUcreateElement( "gsui-dotline", { beatlines: true, viewbox: "0 -1 1 1", xstep: 1 / 100, ystep: 1 / 50 } ),
 	),
 ] );
