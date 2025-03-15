bd8cbfddd040246a11c2adb5e51a4f0e898ea73d
mr21 <thomastortorini@gmail.com>
2025-03-15 22:38:21 +0100

    gsuiWaveEdit: add a help link

--- gsuiWaveEdit/gsuiWaveEdit.html.js
+++ gsuiWaveEdit/gsuiWaveEdit.html.js
@@ -4,6 +4,7 @@ GSUsetTemplate( "gsui-wave-edit", () => [
 	GSUcreateSpan( { class: "gsuiWaveEdit-head" },
 		GSUcreateButton( { class: "gsuiWaveEdit-back gsuiIcon", "data-action": "back", "data-icon": "arrow-left", title: "Save and close wave edition" } ),
 		GSUcreateSpan( { class: "gsuiWaveEdit-title", inert: true }, "wave-edition" ),
+		GSUcreateElement( "gsui-help-link", { page: "synth-wave-edition" } ),
 	),
 	GSUcreateDiv( { class: "gsuiWaveEdit-graph" },
 		GSUcreateElement( "gsui-dotline", { beatlines: true, viewbox: "0 -1 1 1", xstep: 1 / 100, ystep: 1 / 50 } ),
