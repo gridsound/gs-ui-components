18700d7ca1ea231358496ae3272a1adc42142382
mr21 <thomastortorini@gmail.com>
2025-04-06 22:48:20 +0200

    gsuiWaveEdit: CSS, wavetable

--- gsuiWaveEdit/gsuiWaveEdit.html.js
+++ gsuiWaveEdit/gsuiWaveEdit.html.js
@@ -9,4 +9,20 @@ GSUsetTemplate( "gsui-wave-edit", () => [
 	GSUcreateDiv( { class: "gsuiWaveEdit-graph" },
 		GSUcreateElement( "gsui-dotline", { beatlines: true, viewbox: "0 -1 1 1", xstep: 1 / 100, ystep: 1 / 50 } ),
 	),
+	GSUcreateDiv( { class: "gsuiWaveEdit-waves" },
+		GSUgetTemplate( "gsui-wave-edit-wavestep", true ),
+		GSUgetTemplate( "gsui-wave-edit-wavestep", null ),
+		GSUgetTemplate( "gsui-wave-edit-wavestep", null ),
+	),
 ] );
+
+GSUsetTemplate( "gsui-wave-edit-wavestep", ( sel ) =>
+	GSUcreateDiv( { class: "gsuiWaveEdit-wavestep", "data-selected": sel },
+		GSUcreateElement( "gsui-dotlinesvg", {} ),
+		GSUcreateDiv( { class: "gsuiWaveEdit-wavestep-head" },
+			GSUcreateSpan( { class: "gsuiWaveEdit-wavestep-num", inert: true } ),
+			GSUcreateDiv( { class: "gsuiWaveEdit-wavestep-btn gsuiIcon", "data-action": "clone", "data-icon": "clone", title: "Clone this wave" } ),
+			GSUcreateDiv( { class: "gsuiWaveEdit-wavestep-btn gsuiIcon", "data-action": "remove", "data-icon": "close", title: "Remove this wave" } ),
+		),
+	),
+);
