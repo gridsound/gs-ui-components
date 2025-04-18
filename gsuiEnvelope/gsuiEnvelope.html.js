cd8363a9b6a5501d72bb85acdd130255ba1529ae
mr21 <thomastortorini@gmail.com>
2025-04-18 20:13:51 +0200

    gsuiEnvelope: CSS, change the design

--- gsuiEnvelope/gsuiEnvelope.html.js
+++ gsuiEnvelope/gsuiEnvelope.html.js
@@ -1,27 +1,31 @@
 "use strict";
 
-GSUsetTemplate( "gsui-envelope", () => [
-	[
-		[ "attack", "attack", "att", 0, 1, .01 ],
-		[ "hold", "hold", "hold", 0, 1, .01 ],
-		[ "decay", "decay", "dec", 0, 1, .01 ],
-		[ "sustain", "sustain", "sus", 0, 1, .01 ],
-		[ "release", "release", "rel", 0, 4, .01 ],
-		[ "amp", "amplification", "pitch", -24, 24, 1 ],
-		[ "q", "Q", "Q", 0, 25, .01 ],
-	].map( ( [ prop, title, text, min, max, step ] ) =>
-		GSUcreateDiv( { class: "gsuiEnvelope-prop", title, "data-prop": prop },
-			GSUcreateDiv( { class: "gsuiEnvelope-propLabel" },
-				GSUcreateSpan( null, text ),
-				GSUcreateSpan( { class: "gsuiEnvelope-propValue" } ),
+GSUsetTemplate( "gsui-envelope", () =>
+	GSUcreateDiv( { class: "gsuiEnvelope-in" },
+		GSUcreateDiv( { class: "gsuiEnvelope-props" },
+			[
+				[ "attack", "attack", "att", 0, 1, .01 ],
+				[ "hold", "hold", "hold", 0, 1, .01 ],
+				[ "decay", "decay", "dec", 0, 1, .01 ],
+				[ "sustain", "sustain", "sus", 0, 1, .01 ],
+				[ "release", "release", "rel", 0, 4, .01 ],
+				[ "amp", "amplification", "pitch", -24, 24, 1 ],
+				[ "q", "Q", "Q", 0, 25, .01 ],
+			].map( ( [ prop, title, text, min, max, step ] ) =>
+				GSUcreateDiv( { class: "gsuiEnvelope-prop", title, "data-prop": prop },
+					GSUcreateDiv( { class: "gsuiEnvelope-propLabel" },
+						GSUcreateSpan( null, text ),
+						GSUcreateSpan( { class: "gsuiEnvelope-propValue" } ),
+					),
+					GSUcreateDiv( { class: "gsuiEnvelope-propContent" },
+						GSUcreateElement( "gsui-slider", { type: "linear-x", disabled: true, min, max, step, "mousemove-size": "800", "data-prop": prop } ),
+					),
+				)
 			),
-			GSUcreateDiv( { class: "gsuiEnvelope-propContent" },
-				GSUcreateElement( "gsui-slider", { type: "linear-x", disabled: true, min, max, step, "mousemove-size": "800", "data-prop": prop } ),
-			),
-		)
-	),
-	GSUcreateDiv( { class: "gsuiEnvelope-graph" },
-		GSUcreateElement( "gsui-beatlines", { coloredbeats: "" } ),
-		GSUcreateElement( "gsui-envelope-graph" ),
-	),
-].flat( 1 ) );
+		),
+		GSUcreateDiv( { class: "gsuiEnvelope-graph" },
+			GSUcreateElement( "gsui-beatlines", { coloredbeats: "" } ),
+			GSUcreateElement( "gsui-envelope-graph" ),
+		),
+	)
+);
