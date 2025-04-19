5efdfe49c1112a23e95804bd87553cf346e2ea35
mr21 <thomastortorini@gmail.com>
2025-04-19 06:59:37 +0200

    gsuiWaveEdit: the graph is also in the scroll area

--- gsuiWaveEdit/gsuiWaveEdit.html.js
+++ gsuiWaveEdit/gsuiWaveEdit.html.js
@@ -6,10 +6,12 @@ GSUsetTemplate( "gsui-wave-edit", () => [
 		GSUcreateSpan( { class: "gsuiWaveEdit-title", inert: true }, "wave-edition" ),
 		GSUcreateElement( "gsui-help-link", { page: "synth-wave-edition" } ),
 	),
-	GSUcreateDiv( { class: "gsuiWaveEdit-graph" },
-		GSUcreateElement( "gsui-dotline", { beatlines: true, viewbox: "0 -1 1 1", xstep: 1 / 100, ystep: 1 / 50 } ),
+	GSUcreateDiv( { class: "gsuiWaveEdit-scroll" },
+		GSUcreateDiv( { class: "gsuiWaveEdit-graph" },
+			GSUcreateElement( "gsui-dotline", { beatlines: true, viewbox: "0 -1 1 1", xstep: 1 / 100, ystep: 1 / 50 } ),
+		),
+		GSUcreateDiv( { class: "gsuiWaveEdit-waves" } ),
 	),
-	GSUcreateDiv( { class: "gsuiWaveEdit-waves" } ),
 ] );
 
 GSUsetTemplate( "gsui-wave-edit-wavestep", ( id, ind ) =>
