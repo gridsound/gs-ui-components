468cadf4d03203cf660e4452aa455f2303d6f10a
mr21 <thomastortorini@gmail.com>
2025-09-19 01:47:34 +0200

    gsuiComButton: attr `icon`

--- gsuiComButton/gsuiComButton.js
+++ gsuiComButton/gsuiComButton.js
@@ -9,7 +9,13 @@ class gsuiComButton extends gsui0ne {
 			$tagName: "gsui-com-button",
 			$template: GSUcreateButton( null,
 				GSUcreateSpan( { inert: true } ),
+				GSUcreateIcon( { inert: true } ),
 			),
+			$elements: {
+				$btn: "button",
+				$text: "span",
+				$icon: ".gsuiIcon",
+			},
 		} );
 		Object.seal( this );
 		gsuiRipple.$init( this.$element );
@@ -22,15 +28,16 @@ class gsuiComButton extends gsui0ne {
 
 	// .........................................................................
 	static get observedAttributes() {
-		return [ "text", "loading", "disabled", "type", "href" ];
+		return [ "text", "icon", "loading", "disabled", "type", "href" ];
 	}
 	$attributeChanged( prop, val ) {
 		switch ( prop ) {
 			case "disabled":
 			case "loading": this.#updateDisabled(); break;
-			case "text": this.$element.firstChild.textContent = val; break;
+			case "text": this.$elements.$text.textContent = val; break;
 			case "type": this.$element.type = val === "submit" ? val : "button"; break;
 			case "href": this.#href = val; break;
+			case "icon": GSUdomSetAttr( this.$elements.$icon, "data-icon", val ); break;
 		}
 	}
 
