324fa88e19e76f0b878cc8d3035e3aaecf7ecc3b
mr21 <thomastortorini@gmail.com>
2025-11-14 23:51:36 +0100

    gsuiComProfile: following/followers

--- gsuiComProfile/gsuiComProfile.js
+++ gsuiComProfile/gsuiComProfile.js
@@ -3,6 +3,7 @@
 class gsuiComProfile extends gsui0ne {
 	#savingPromise = null;
 	#verifyPromise = null;
+	#followPromise = null;
 	static #emailTexts = {
 		$verify: "Email not verify, send a confirmation email again",
 		$sending: "Email sending...",
@@ -14,6 +15,7 @@ class gsuiComProfile extends gsui0ne {
 			$cmpName: "gsuiComProfile",
 			$tagName: "gsui-com-profile",
 			$elements: {
+				$main: ".gsuiComProfile-main",
 				$avatar: "gsui-com-avatar",
 				$email: ".gsuiComProfile-main-email-addr span",
 				$emailpub: ".gsuiComProfile-main-email-addr .gsuiIcon",
@@ -22,6 +24,9 @@ class gsuiComProfile extends gsui0ne {
 				$firstname: ".gsuiComProfile-main-firstname",
 				$emailVerify: ".gsuiComProfile-main-email-not",
 				$emailVerifyText: ".gsuiComProfile-main-email-not span",
+				$followers: ".gsuiComProfile-main-followers",
+				$following: ".gsuiComProfile-main-following",
+				$followBtn: ".gsuiComProfile-main-follow-btn",
 				$edit: ".gsuiComProfile-main-edit",
 				$form: ".gsuiComProfile-form",
 				$cancel: "gsui-com-button:not([type='submit'])",
@@ -31,18 +36,28 @@ class gsuiComProfile extends gsui0ne {
 			},
 			$attributes: {
 				email: "",
+				followers: 0,
+				following: 0,
 			},
 		} );
 		Object.seal( this );
 		this.$elements.$edit.onclick = this.#onclickEdit.bind( this );
+		this.$elements.$followBtn.onclick = this.#onclickFollow.bind( this );
 		this.$elements.$form.onsubmit = this.#onsubmit.bind( this );
 		this.$elements.$emailVerify.onclick = this.#onclickVerify.bind( this );
 		this.$elements.$cancel.onclick = () => GSUdomRmAttr( this, "editing" );
+		this.$elements.$main.onmouseenter =
+		this.$elements.$main.onmouseleave = () => GSUdomRmAttr( this, "followedjustnow" );
 	}
 
 	// .........................................................................
 	static get observedAttributes() {
-		return [ "itsme", "email", "emailpublic", "emailtoverify", "username", "lastname", "firstname", "avatar" ];
+		return [
+			"itsme", "avatar",
+			"email", "emailpublic", "emailtoverify",
+			"username", "lastname", "firstname",
+			"followed", "followers", "following",
+		];
 	}
 	$attributeChanged( prop, val ) {
 		switch ( prop ) {
@@ -54,18 +69,29 @@ class gsuiComProfile extends gsui0ne {
 			case "firstname": this.$elements.$firstname.textContent = val; break;
 			case "avatar": GSUdomSetAttr( this.$elements.$avatar, "src", val || false ); break;
 			case "emailtoverify": this.$elements.$emailVerifyText.textContent = val !== "" ? "" : gsuiComProfile.#emailTexts.$verify; break;
+			case "followers": this.$elements.$followers.textContent = val; break;
+			case "following": this.$elements.$following.textContent = val; break;
+			case "followed": this.#updateFollowed( val === "" ); break;
 		}
 	}
 
 	// .........................................................................
-	$setSavingCallbackPromise( fn ) {
-		this.#savingPromise = fn;
-	}
-	$setVerifyEmailCallbackPromise( fn ) {
-		this.#verifyPromise = fn;
-	}
+	$setFollowCallbackPromise( fn ) { this.#followPromise = fn; }
+	$setSavingCallbackPromise( fn ) { this.#savingPromise = fn; }
+	$setVerifyEmailCallbackPromise( fn ) { this.#verifyPromise = fn; }
 
 	// .........................................................................
+	#updateFollowed( b ) {
+		GSUdomSetAttr( this.$elements.$followBtn, b
+			? { "data-icon": "follow", title: "Follow" }
+			: { "data-icon": "followed", title: "Unfollow" } );
+	}
+	#updateFollowLoading( b ) {
+		GSUdomTogClass( this.$elements.$followBtn, "gsuiIcon", b );
+		GSUdomSetAttr( this.$elements.$followBtn, b
+			? { "data-spin": "on", disabled: true }
+			: { "data-spin": false, disabled: false } );
+	}
 	#onclickEdit() {
 		if ( !GSUdomHasAttr( this, "editing" ) ) {
 			this.$elements.$inputs[ 0 ].value = GSUdomGetAttr( this, "firstname" );
@@ -75,6 +101,21 @@ class gsuiComProfile extends gsui0ne {
 		}
 		this.$elements.$error.textContent = "";
 		GSUdomTogAttr( this, "editing" );
+	}
+	#onclickFollow() {
+		const willFollow = !GSUdomHasAttr( this, "followed" );
+
+		this.#updateFollowLoading( true );
+		this.#followPromise?.( willFollow )
+			.then( () => {
+				GSUdomSetAttr( this, {
+					followed: willFollow,
+					followers: GSUdomGetAttrNum( this, "followers" ) + willFollow * 2 - 1,
+					followedjustnow: willFollow,
+				} );
+			} )
+			.catch( err => GSUpopup.$alert( `Error ${ err.code }`, err.msg ) )
+			.finally( () => this.#updateFollowLoading( false ) );
 	}
 	#onclickVerify() {
 		if ( !GSUdomHasAttr( this, "emailsending" ) ) {
