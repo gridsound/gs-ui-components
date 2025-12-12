2ce3af88dd2c2866059222f3d5b479a24cf6b8be
mr21 <thomastortorini@gmail.com>
2025-12-12 18:25:30 +0100

    gsuiEffect: HTML, gsuiEffect-name, inert

--- gsuiEffect/gsuiEffect.html.js
+++ gsuiEffect/gsuiEffect.html.js
@@ -5,7 +5,7 @@ GSUsetTemplate( "gsui-effect", () => [
 		GSUcreateDiv( { class: "gsuiEffect-grip gsuiIcon", "data-icon": "grip-v" } ),
 		GSUcreateButton( { class: "gsuiEffect-expand", icon: "caret-right" } ),
 		GSUcreateElement( "gsui-toggle", { off: true, title: "Toggle this effect" } ),
-		GSUcreateSpan( { class: "gsuiEffect-name" } ),
+		GSUcreateSpan( { class: "gsuiEffect-name", inert: true } ),
 		GSUcreateElement( "gsui-help-link" ),
 		GSUcreateButton( { class: "gsuiEffect-remove", icon: "close", title: "Delete this effect" } ),
 	),
