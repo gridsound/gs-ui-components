0204ee72c0251e9aa98f4bd37ad8676b24007071
mr21 <thomastortorini@gmail.com>
2026-01-16 00:32:02 +0100

    gsuiPianoroll: CSS, gsuiPianoroll-block, nesting

--- gsuiPianoroll/gsuiPianoroll.html.js
+++ gsuiPianoroll/gsuiPianoroll.html.js
@@ -2,7 +2,7 @@
 
 GSUsetTemplate( "gsui-pianoroll-block", () =>
 	GSUcreateDiv( { class: "gsuiBlocksManager-block gsuiPianoroll-block", "data-action": "move" },
-		GSUcreateDiv( { class: "gsuiPianoroll-block-key" } ),
+		GSUcreateSpan(),
 		GSUcreateDiv( { class: "gsuiDragline-drop" } ),
 		GSUcreateDiv( { class: "gsuiBlocksManager-block-crop gsuiBlocksManager-block-cropB", "data-action": "cropB" } ),
 	)
