71bd16518dba3155c968f8c43a3783ea8031539e
mr21 <thomastortorini@gmail.com>
2026-01-19 01:29:34 +0100

    gsuiComPlayer: update GSUjq

--- gsuiComPlayer/gsuiComPlayer.js
+++ gsuiComPlayer/gsuiComPlayer.js
@@ -112,7 +112,7 @@ class gsuiComPlayer extends gsui0ne {
 		const dur = +this.$this.$attr( "duration" );
 		const time = +this.$this.$attr( "currenttime" );
 
-		this.$elements.$timeInpVal.$style( "width", `${ time / dur * 100 }%` );
+		this.$elements.$timeInpVal.$width( time / dur * 100, "%" );
 	}
 
 	// .........................................................................
@@ -145,8 +145,8 @@ class gsuiComPlayer extends gsui0ne {
 			} );
 	}
 	#onclickPlay() {
-		if ( this.$elements.$audio[ 0 ].src ) {
-			this.$elements.$audio[ 0 ].paused
+		if ( this.$elements.$audio.$at( 0 ).src ) {
+			this.$elements.$audio.$at( 0 ).paused
 				? this.$play()
 				: this.$pause();
 		} else {
@@ -170,7 +170,7 @@ class gsuiComPlayer extends gsui0ne {
 		}
 	}
 	#onframePlaying() {
-		this.$this.$attr( "currenttime", this.$elements.$audio[ 0 ].currentTime );
+		this.$this.$attr( "currenttime", this.$elements.$audio.$at( 0 ).currentTime );
 	}
 	#updateRendered( b ) {
 		this.$elements.$play.$attr( b
@@ -188,7 +188,7 @@ class gsuiComPlayer extends gsui0ne {
 				{ hidden: true, id: "restore", icon: "trash-restore", name: "Restore it", desc: "Get the composition out of the bin." },
 			];
 			this.#actionMenu = new gsuiActionMenu();
-			this.#actionMenu.$bindTargetElement( this.$elements.$actionsBtn[ 0 ] );
+			this.#actionMenu.$bindTargetElement( this.$elements.$actionsBtn.$at( 0 ) );
 			this.#actionMenu.$setActions( this.#actions );
 			this.#actionMenu.$setDirection( this.#actionMenuDir );
 			this.#actionMenu.$setMaxSize( "260px", "180px" );
@@ -240,7 +240,7 @@ class gsuiComPlayer extends gsui0ne {
 		const x = GSUmathClamp( ( e.clientX - bcr.x ) / bcr.width, 0, 1 );
 
 		this.#settingTime = x;
-		this.$elements.$timeInpVal.$style( "width", `${ x * 100 }%` );
+		this.$elements.$timeInpVal.$width( x * 100, "%" );
 	}
 	#ptrUp( e ) {
 		const t = this.#settingTime;
@@ -249,7 +249,7 @@ class gsuiComPlayer extends gsui0ne {
 		e.target.onpointerup =
 		e.target.onpointermove =
 		this.#settingTime = null;
-		this.$elements.$audio[ 0 ].currentTime = t * this.$this.$attr( "duration" );
+		this.$elements.$audio.$at( 0 ).currentTime = t * this.$this.$attr( "duration" );
 	}
 }
 
