3d3649e4717a668434f6688828f48e645886f898
mr21 <thomastortorini@gmail.com>
2026-01-19 08:38:10 +0100

    gsuiSynthesizer: jqueryfy

--- gsuiSynthesizer/gsuiSynthesizer.js
+++ gsuiSynthesizer/gsuiSynthesizer.js
@@ -24,21 +24,12 @@ class gsuiSynthesizer extends gsui0ne {
 		super( {
 			$cmpName: "gsuiSynthesizer",
 			$tagName: "gsui-synthesizer",
+			$jqueryfy: true,
 			$elements: {
-				$heads: "[].gsuiSynthesizer-head",
-				$presetBtns: "[].gsuiSynthesizer-preset button",
+				$heads: ".gsuiSynthesizer-head",
+				$presetBtns: ".gsuiSynthesizer-preset button",
 				$presetName: ".gsuiSynthesizer-preset > span:last-child",
-				$tabs: {
-					env: {
-						gain: "[data-tab='env gain']",
-						detune: "[data-tab='env detune']",
-						lowpass: "[data-tab='env lowpass']",
-					},
-					lfo: {
-						gain: "[data-tab='lfo gain']",
-						detune: "[data-tab='lfo detune']",
-					},
-				},
+				$tabs: ".gsuiSynthesizer-headTab",
 				$noise: "gsui-noise",
 				$noiseToggle: ".gsuiSynthesizer-headNoise gsui-toggle",
 				$env: "gsui-envelope",
@@ -53,15 +44,11 @@ class gsuiSynthesizer extends gsui0ne {
 			},
 		} );
 		Object.seal( this );
-
-		const onclickHeadsBind = this.#onclickHeads.bind( this );
-
-		this.$elements.$presetBtns[ 0 ].onclick =
-		this.$elements.$presetBtns[ 1 ].onclick = this.#onclickPreset.bind( this );
-		this.$elements.$newOsc.onclick = this.#onclickNewOsc.bind( this );
-		this.$elements.$heads.forEach( el => el.onclick = onclickHeadsBind );
+		this.$elements.$presetBtns.$on( "click", this.#onclickPreset.bind( this ) );
+		this.$elements.$newOsc.$on( "click", this.#onclickNewOsc.bind( this ) );
+		this.$elements.$heads.$on( "click", this.#onclickHeads.bind( this ) );
 		new gsuiReorder( {
-			$root: this.$elements.$oscList,
+			$root: this.$elements.$oscList.$at( 0 ),
 			$parentSelector: ".gsuiSynthesizer-oscList",
 			$itemSelector: "gsui-oscillator",
 			$itemGripSelector: ".gsuiOscillator-grip",
@@ -81,8 +68,8 @@ class gsuiSynthesizer extends gsui0ne {
 					const [ lfoEnv, prop ] = d.$target.parentNode.dataset.tab.split( " " );
 					const elCmp = lfoEnv === "env" ? this.$elements.$env : this.$elements.$lfo;
 
-					if ( GSUdomGetAttr( elCmp, lfoEnv ) === prop ) {
-						GSUdomSetAttr( elCmp, "toggle", b );
+					if ( elCmp.$attr( lfoEnv ) === prop ) {
+						elCmp.$attr( "toggle", b );
 					}
 					GSUdomDispatch( this, lfoEnv === "env" ? GSEV_SYNTHESIZER_TOGGLEENV : GSEV_SYNTHESIZER_TOGGLELFO, prop, b );
 				}
@@ -111,11 +98,11 @@ class gsuiSynthesizer extends gsui0ne {
 	$attributeChanged( prop, val ) {
 		switch ( prop ) {
 			case "preset":
-				this.$elements.$presetName.textContent = val;
+				this.$elements.$presetName.$text( val );
 				break;
 			case "timedivision":
-				GSUdomSetAttr( this.$elements.$env, "timedivision", val );
-				GSUdomSetAttr( this.$elements.$lfo, "timedivision", val );
+				this.$elements.$env.$attr( "timedivision", val );
+				this.$elements.$lfo.$attr( "timedivision", val );
 				break;
 		}
 	}
@@ -126,8 +113,8 @@ class gsuiSynthesizer extends gsui0ne {
 			const wtposCurves = key.wtposCurves;
 
 			this.#previews[ keyId ] = null;
-			this.$elements.$env.$startKey( keyId, bpm, dur );
-			this.$elements.$lfo.$startKey( keyId, bpm, dur );
+			this.$elements.$env.$at( 0 ).$startKey( keyId, bpm, dur );
+			this.$elements.$lfo.$at( 0 ).$startKey( keyId, bpm, dur );
 			this.#uiOscs.forEach( ( osc, oscId ) => osc.$startKey( keyId, wtposCurves[ oscId ] || "0", bpm, dur ) );
 		}, when );
 	}
@@ -139,9 +126,9 @@ class gsuiSynthesizer extends gsui0ne {
 		}
 		delete this.#previews[ keyId ];
 		if ( pId === null ) {
-			this.$elements.$env.$stopKey( keyId );
+			this.$elements.$env.$at( 0 ).$stopKey( keyId );
 			GSUsetTimeout( () => {
-				this.$elements.$lfo.$stopKey( keyId );
+				this.$elements.$lfo.$at( 0 ).$stopKey( keyId );
 				this.#uiOscs.forEach( osc => osc.$stopKey( keyId ) );
 			}, rel / ( bpm / 60 ) );
 		}
@@ -166,33 +153,37 @@ class gsuiSynthesizer extends gsui0ne {
 	$updateGraph( envLFO, prop ) {
 		const elCmp = envLFO === "env" ? this.$elements.$env : this.$elements.$lfo;
 
-		if ( prop === GSUdomGetAttr( elCmp, envLFO ) ) {
-			elCmp.$updateWave();
+		if ( prop === elCmp.$attr( envLFO ) ) {
+			elCmp.$at( 0 ).$updateWave();
 		}
 	}
 	$changeNoiseProp( prop, val ) {
 		this.#data.noise[ prop ] = val;
-		GSUdomSetAttr( this.$elements.$noise, prop, val );
+		this.$elements.$noise.$attr( prop, val );
 		if ( prop === "toggle" ) {
-			GSUdomSetAttr( this.$elements.$noiseToggle, "off", !val );
+			this.$elements.$noiseToggle.$attr( "off", !val );
 		}
 	}
 	$changeEnvProp( env, prop, val ) {
 		this.#data.env[ env ][ prop ] = val;
-		if ( env === GSUdomGetAttr( this.$elements.$env, "env" ) ) {
-			GSUdomSetAttr( this.$elements.$env, prop, val );
+		if ( env === this.$elements.$env.$attr( "env" ) ) {
+			this.$elements.$env.$attr( prop, val );
 		}
 		if ( prop === "toggle" ) {
-			GSUdomSetAttr( this.$elements.$tabs.env[ env ].firstChild, "off", !val );
+			this.$elements.$tabs
+				.$filter( el => el.dataset.tab === `env ${ prop }` )
+				.$child( 0 ).$attr( "off", !val );
 		}
 	}
 	$changeLFOProp( lfo, prop, val ) {
 		this.#data.lfo[ lfo ][ prop ] = val;
-		if ( lfo === GSUdomGetAttr( this.$elements.$lfo, "lfo" ) ) {
-			GSUdomSetAttr( this.$elements.$lfo, prop, val );
+		if ( lfo === this.$elements.$lfo.$attr( "lfo" ) ) {
+			this.$elements.$lfo.$attr( prop, val );
 		}
 		if ( prop === "toggle" ) {
-			GSUdomSetAttr( this.$elements.$tabs.lfo[ lfo ].firstChild, "off", !val );
+			this.$elements.$tabs
+				.$filter( el => el.dataset.tab === `lfo ${ prop }` )
+				.$child( 0 ).$attr( "off", !val );
 		}
 	}
 
@@ -203,7 +194,7 @@ class gsuiSynthesizer extends gsui0ne {
 		this.#uiOscs.set( id, uiOsc );
 		uiOsc.$addWaveCustom( GSUformatWavetableName( this.dataset.id, id ) );
 		uiOsc.$addWaves( this.#waveList );
-		this.$elements.$oscList.append( uiOsc );
+		this.$elements.$oscList.$append( uiOsc );
 		return uiOsc;
 	}
 	$removeOscillator( id ) {
@@ -217,32 +208,31 @@ class gsuiSynthesizer extends gsui0ne {
 
 	// .........................................................................
 	#selectTab( lfoEnv, prop ) {
-		const tabs = this.$elements.$tabs[ lfoEnv ];
+		const tabs = this.$elements.$tabs.$filter( el => el.dataset.tab.startsWith( lfoEnv ) );
 
-		if ( !GSUdomHasAttr( tabs[ prop ], "data-selected" ) ) {
-			const elCmp = lfoEnv === "env" ? this.$elements.$env : this.$elements.$lfo;
-
-			GSUforEach( tabs, el => GSUdomSetAttr( el, "data-selected", el.dataset.tab.endsWith( prop ) ) );
-			GSUdomSetAttr( elCmp, this.#data[ lfoEnv ][ prop ] );
-			GSUdomSetAttr( elCmp, lfoEnv, prop );
+		if ( !tabs.$filter( el => el.dataset.tab.endsWith( prop ) ).$hasAttr( "data-selected" ) ) {
+			tabs.$attr( "data-selected", el => el.dataset.tab.endsWith( prop ) );
+			( lfoEnv === "env" ? this.$elements.$env : this.$elements.$lfo )
+				.$attr( this.#data[ lfoEnv ][ prop ] )
+				.$attr( lfoEnv, prop );
 		}
 	}
 
 	// .........................................................................
 	#dispatchPreset( p ) {
-		if ( p !== GSUdomGetAttr( this, "preset" ) ) {
-			GSUdomSetAttr( this, "preset", p );
+		if ( p !== this.$this.$attr( "preset" ) ) {
+			this.$this.$attr( "preset", p );
 			GSUdomDispatch( this, GSEV_SYNTHESIZER_PRESET, p );
 		}
 	}
 	#onclickPreset( e ) {
 		const list = gsuiSynthesizer.#presetList;
 		const dir = +e.target.dataset.action;
-		const ind = list.indexOf( this.$elements.$presetName.textContent );
+		const ind = list.indexOf( this.$elements.$presetName.$text() );
 		const p = list[ GSUmathClamp( ind + dir, 0, list.length - 1 ) ];
 
 		if ( p ) {
-			this.$elements.$presetName.textContent = p;
+			this.$elements.$presetName.$text( p );
 			this.#dispatchPresetDeb( p );
 		}
 	}
