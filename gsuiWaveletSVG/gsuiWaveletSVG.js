23c02b8fba16dc01ee172026ee9846749d30c82c
mr21 <thomastortorini@gmail.com>
2026-01-25 23:16:08 +0100

    gsuiWaveletSVG

---
+++ gsuiWaveletSVG/gsuiWaveletSVG.js
@@ -0,0 +1,50 @@
+"use strict";
+
+class gsuiWaveletSVG extends gsui0ne {
+	#w = 0;
+	#h = 0;
+
+	constructor() {
+		super( {
+			$cmpName: "gsuiWaveletSVG",
+			$tagName: "gsui-wavelet-svg",
+			$template: GSUcreateElement( "svg", { preserveAspectRatio: "none", inert: true },
+				GSUcreateElement( "polyline" ),
+			),
+		} );
+		Object.seal( this );
+	}
+
+	// .........................................................................
+	$resolution() {
+		this.$element.$viewbox(
+			this.#w = this.$element.$width(),
+			this.#h = this.$element.$height(),
+		);
+	}
+	$draw( arr ) {
+		const w = this.#w;
+		const h = this.#h;
+
+		if ( w && h && arr?.length >= 1 ) {
+			// const arr2 = GSUarrayResize( arr, w );
+			const len = arr.length - 1;
+			const pts = GSUnewArray( len + 1, i => [
+				i / len * w,
+				( .5 - arr[ i ] / 2 ) * h,
+			] );
+
+			pts.unshift(
+				[ -10, h / 2 ],
+				[ -10, pts[ 0 ][ 1 ] ],
+			);
+			pts.push(
+				[ w + 10, pts.at( -1 )[ 1 ] ],
+				[ w + 10, h / 2 ],
+			);
+			this.$element.$child( 0 ).$setAttr( "points", pts.join( " " ) );
+		}
+	}
+}
+
+GSUdomDefine( "gsui-wavelet-svg", gsuiWaveletSVG );
