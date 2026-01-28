b0ea23324648f46ea29e5df9978462510a78e664
mr21 <thomastortorini@gmail.com>
2026-01-28 19:21:28 +0100

    gsuiKeys: use attr instead of class for black/white

--- gsuiKeys/gsuiKeys.html.js
+++ gsuiKeys/gsuiKeys.html.js
@@ -1,16 +1,16 @@
 "use strict";
 
 GSUsetTemplate( "gsui-keys-octave", () => [
-	GSUcreateDiv( { class: "gsuiKeys-key",                   "data-key": 11 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row",                   "data-key": 11 }, GSUcreateDiv() ) ),
-	GSUcreateDiv( { class: "gsuiKeys-key gsuiKeys-keyBlack", "data-key": 10 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row  gsuiKeys-rowBlack", "data-key": 10 }, GSUcreateDiv() ) ),
-	GSUcreateDiv( { class: "gsuiKeys-key",                   "data-key":  9 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row",                   "data-key":  9 }, GSUcreateDiv() ) ),
-	GSUcreateDiv( { class: "gsuiKeys-key gsuiKeys-keyBlack", "data-key":  8 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row  gsuiKeys-rowBlack", "data-key":  8 }, GSUcreateDiv() ) ),
-	GSUcreateDiv( { class: "gsuiKeys-key",                   "data-key":  7 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row",                   "data-key":  7 }, GSUcreateDiv() ) ),
-	GSUcreateDiv( { class: "gsuiKeys-key gsuiKeys-keyBlack", "data-key":  6 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row  gsuiKeys-rowBlack", "data-key":  6 }, GSUcreateDiv() ) ),
-	GSUcreateDiv( { class: "gsuiKeys-key",                   "data-key":  5 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row",                   "data-key":  5 }, GSUcreateDiv() ) ),
-	GSUcreateDiv( { class: "gsuiKeys-key",                   "data-key":  4 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row",                   "data-key":  4 }, GSUcreateDiv() ) ),
-	GSUcreateDiv( { class: "gsuiKeys-key gsuiKeys-keyBlack", "data-key":  3 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row  gsuiKeys-rowBlack", "data-key":  3 }, GSUcreateDiv() ) ),
-	GSUcreateDiv( { class: "gsuiKeys-key",                   "data-key":  2 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row",                   "data-key":  2 }, GSUcreateDiv() ) ),
-	GSUcreateDiv( { class: "gsuiKeys-key gsuiKeys-keyBlack", "data-key":  1 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row  gsuiKeys-rowBlack", "data-key":  1 }, GSUcreateDiv() ) ),
-	GSUcreateDiv( { class: "gsuiKeys-key",                   "data-key":  0 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row",                   "data-key":  0 }, GSUcreateDiv() ) ),
+	GSUcreateDiv( { class: "gsuiKeys-key", "data-col": "w", "data-key": 11 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row", "data-col": "w", "data-key": 11 }, GSUcreateDiv() ) ),
+	GSUcreateDiv( { class: "gsuiKeys-key", "data-col": "b", "data-key": 10 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row", "data-col": "b", "data-key": 10 }, GSUcreateDiv() ) ),
+	GSUcreateDiv( { class: "gsuiKeys-key", "data-col": "w", "data-key":  9 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row", "data-col": "w", "data-key":  9 }, GSUcreateDiv() ) ),
+	GSUcreateDiv( { class: "gsuiKeys-key", "data-col": "b", "data-key":  8 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row", "data-col": "b", "data-key":  8 }, GSUcreateDiv() ) ),
+	GSUcreateDiv( { class: "gsuiKeys-key", "data-col": "w", "data-key":  7 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row", "data-col": "w", "data-key":  7 }, GSUcreateDiv() ) ),
+	GSUcreateDiv( { class: "gsuiKeys-key", "data-col": "b", "data-key":  6 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row", "data-col": "b", "data-key":  6 }, GSUcreateDiv() ) ),
+	GSUcreateDiv( { class: "gsuiKeys-key", "data-col": "w", "data-key":  5 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row", "data-col": "w", "data-key":  5 }, GSUcreateDiv() ) ),
+	GSUcreateDiv( { class: "gsuiKeys-key", "data-col": "w", "data-key":  4 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row", "data-col": "w", "data-key":  4 }, GSUcreateDiv() ) ),
+	GSUcreateDiv( { class: "gsuiKeys-key", "data-col": "b", "data-key":  3 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row", "data-col": "b", "data-key":  3 }, GSUcreateDiv() ) ),
+	GSUcreateDiv( { class: "gsuiKeys-key", "data-col": "w", "data-key":  2 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row", "data-col": "w", "data-key":  2 }, GSUcreateDiv() ) ),
+	GSUcreateDiv( { class: "gsuiKeys-key", "data-col": "b", "data-key":  1 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row", "data-col": "b", "data-key":  1 }, GSUcreateDiv() ) ),
+	GSUcreateDiv( { class: "gsuiKeys-key", "data-col": "w", "data-key":  0 }, GSUcreateDiv( { class: "gsui-row gsuiKeys-row", "data-col": "w", "data-key":  0 }, GSUcreateDiv() ) ),
 ] );
