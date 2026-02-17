b149f25712ed1755e89c67b3dbc4ea00f6c6fe01
mr21 <thomastortorini@gmail.com>
2026-02-17 04:43:41 +0100

    gsuiPanels: jqueryfy

--- gsuiPanels/gsuiPanels.js
+++ gsuiPanels/gsuiPanels.js
@@ -1,9 +1,11 @@
 "use strict";
 
 class gsuiPanels extends gsui0ne {
-	#dir = "";
-	#pos = "";
 	#dirX = false;
+	#pos = "";
+	#dir = "";
+	#mindir = "";
+	#maxdir = "";
 	#pageN = 0;
 	#extend = null;
 	#pans = null;
@@ -30,37 +32,38 @@ class gsuiPanels extends gsui0ne {
 		const dirX = this.$this.$getAttr( "dir" ) === "x";
 		const size = dirX ? this.$this.$width() : this.$this.$height();
 
-		this.#pans = Array.from( this.children );
+		this.#pans = Array.from( this.children ).map( p => $( p ) );
 		this.#dirX = dirX;
-		this.#dir = dirX ? "width" : "height";
 		this.#pos = dirX ? "left" : "top";
+		this.#dir = dirX ? "width" : "height";
+		this.#mindir = dirX ? "minWidth" : "minHeight";
+		this.#maxdir = dirX ? "maxWidth" : "maxHeight";
 		this.$this.$query( ".gsuiPanels-extend" ).$remove();
-		this.#pans.map( p => [ p, GSUdomBCR( p )[ this.#dir ] / size * 100 ] )
-			.reduce( ( x, [ p, perc ] ) => {
+		this.#pans.map( p => [ p, p.$bcr()[ this.#dir ] / size * 100 ] )
+			.reduce( ( x, [ pan, perc ] ) => {
 				const perc2 = GSUmathFix( perc, 1 );
 
-				GSUdomAddClass( p, "gsuiPanels-panel" );
-				p.style[ this.#dir ] = `${ perc2 }%`;
-				p.style[ this.#pos ] = `${ x }%`;
+				pan
+					.$addClass( "gsuiPanels-panel" )
+					.$css( this.#dir, perc2, "%" )
+					.$css( this.#pos, x, "%" );
 				if ( x > 0 ) {
-					p.append( GSUcreateDiv( { class: "gsuiPanels-extend" } ) );
+					pan.$append( GSUcreateDiv( { class: "gsuiPanels-extend" } ) );
 				}
 				return x + perc2;
 			}, 0 );
 	}
-	static #incrSizePans( dir, mov, parentsize, pans ) {
+	#incrSizePans( dir, mov, parentsize, pans ) {
 		return pans.reduce( ( mov, pan ) => {
-			const style = GSUdomStyle( pan );
-			const size = Math.round( GSUdomBCR( pan )[ dir ] );
-			const minsize = parseFloat( style[ `min-${ dir }` ] ) || 10;
-			const maxsize = parseFloat( style[ `max-${ dir }` ] ) || Infinity;
-			const newsizeCorrect = Math.round( Math.max( minsize, Math.min( size + mov, maxsize ) ) );
+			const size = Math.round( pan.$bcr()[ dir ] );
+			const minsize = parseFloat( pan.$css( this.#mindir ) ) || 10;
+			const maxsize = parseFloat( pan.$css( this.#maxdir ) ) || Infinity;
+			const newsizeCorrect = Math.round( GSUmathClamp( size + mov, minsize, maxsize ) );
 			let ret = mov;
 
 			if ( Math.abs( newsizeCorrect - size ) >= .1 ) {
-				pan.style[ dir ] = `${ GSUmathFix( newsizeCorrect / parentsize * 100, 2 ) }%`;
+				pan.$css( dir, GSUmathFix( newsizeCorrect / parentsize * 100, 2 ), "%" );
 				ret -= newsizeCorrect - size;
-				pan.onresizing?.( pan );
 			}
 			return ret;
 		}, mov );
@@ -68,19 +71,17 @@ class gsuiPanels extends gsui0ne {
 
 	// .........................................................................
 	$onresize() {
-		const tot = GSUdomBCR( this )[ this.#dir ];
-		const tot2 = this.#pans.reduce( ( sz, p ) => sz + GSUdomBCR( p )[ this.#dir ], 0 );
+		const tot = this.$this.$bcr()[ this.#dir ];
+		const tot2 = this.#pans.reduce( ( sz, p ) => sz + p.$bcr()[ this.#dir ], 0 );
 
 		if ( Math.abs( tot2 - tot ) > 0 ) {
-			const mindir = this.#dirX ? "minWidth" : "minHeight";
 			let pmax;
 			let pmin;
 			let pmaxPerc = 0;
 			let pminPerc = Infinity;
 
 			this.#pans.forEach( p => {
-				const st = GSUdomStyle( p );
-				const perc = parseFloat( st[ this.#dir ] ) / parseFloat( st[ mindir ] );
+				const perc = parseFloat( p.$css( this.#dir ) ) / parseFloat( p.$css( this.#mindir ) );
 
 				if ( perc > pmaxPerc ) {
 					pmax = p;
@@ -99,30 +100,30 @@ class gsuiPanels extends gsui0ne {
 			if ( pan ) {
 				const perc = tot2 / tot * 100 - 100;
 
-				pan.style[ this.#dir ] = `${ parseFloat( pan.style[ this.#dir ] ) - perc }%`;
+				pan.$css( this.#dir, parseFloat( pan.$get( 0 ).style[ this.#dir ] ) - perc, "%" );
 			}
 		}
 		this.#pans.reduce( ( x, p ) => {
-			p.style[ this.#pos ] = `${ GSUmathFix( x / tot * 100, 2 ) }%`;
-			return x + GSUdomBCR( p )[ this.#dir ];
+			p.$css( this.#pos, GSUmathFix( x / tot * 100, 2 ), "%" );
+			return x + p.$bcr()[ this.#dir ];
 		}, 0 );
 	}
 	#onpointerdown( e ) {
-		const tar = e.target;
-		const pan = tar.parentNode;
+		const tar = $( e.target );
+		const pan = tar.$parent();
 
-		if ( e.button === 0 && pan.parentNode === this && GSUdomHasClass( tar, "gsuiPanels-extend" ) ) {
-			const pInd = this.#pans.indexOf( pan );
+		if ( e.button === 0 && pan.$parent().$is( this ) && tar.$hasClass( "gsuiPanels-extend" ) ) {
+			const pInd = pan.$index( 0 );
 
 			this.#extend = tar;
 			this.#pageN = this.#dirX ? e.pageX : e.pageY;
 			this.#panBefore = this.#pans.slice( 0, pInd ).reverse();
 			this.#panAfter = this.#pans.slice( pInd );
 			this.#panAfterMinSize = this.#panAfter.reduce( ( n, p ) => {
-				return n + parseFloat( GSUdomStyle( p )[ `min-${ this.#dir }` ] ) || 10;
+				return n + parseFloat( p.$css( this.#mindir ) || 10 );
 			}, 0 );
 			this.$this.$css( "cursor", this.#dirX ? "col-resize" : "row-resize" );
-			GSUdomAddClass( tar, "gsui-hover" );
+			tar.$addClass( "gsui-hover" );
 			GSUdomUnselect();
 			this.setPointerCapture( e.pointerId );
 			this.onpointerup = this.#onpointerup.bind( this );
@@ -131,25 +132,26 @@ class gsuiPanels extends gsui0ne {
 	}
 	#onpointerup( e ) {
 		this.$this.$css( "cursor", "" );
-		GSUdomRmClass( this.#extend, "gsui-hover" );
+		this.#extend.$rmClass( "gsui-hover" );
 		this.releasePointerCapture( e.pointerId );
+		this.#extend =
 		this.onpointermove =
 		this.onpointerup = null;
 	}
 	#onpointermove( e ) {
 		const px = Math.round( ( this.#dirX ? e.pageX : e.pageY ) - this.#pageN );
 		const parentsize = this.#dirX ? this.clientWidth : this.clientHeight;
-		const currBeforeSize = this.#panBefore.reduce( ( n, p ) => n + parseFloat( GSUdomStyle( p )[ this.#dir ] ), 0 );
+		const currBeforeSize = this.#panBefore.reduce( ( n, p ) => n + parseFloat( p.$css( this.#dir ) ), 0 );
 		const px2 = px <= 0 ? px : Math.min( px, parentsize - currBeforeSize - this.#panAfterMinSize );
-		const mov = px2 - gsuiPanels.#incrSizePans( this.#dir, px2, parentsize, this.#panBefore );
+		const mov = px2 - this.#incrSizePans( this.#dir, px2, parentsize, this.#panBefore );
 
 		this.#pageN += mov;
 		if ( Math.abs( mov ) > 0 ) {
-			gsuiPanels.#incrSizePans( this.#dir, -mov, parentsize, this.#panAfter );
+			this.#incrSizePans( this.#dir, -mov, parentsize, this.#panAfter );
 		}
 		this.#pans.reduce( ( x, p ) => {
-			p.style[ this.#pos ] = `${ x }%`;
-			return x + parseFloat( p.style[ this.#dir ] );
+			p.$css( this.#pos, x, "%" );
+			return x + parseFloat( p.$get( 0 ).style[ this.#dir ] );
 		}, 0 );
 	}
 }
