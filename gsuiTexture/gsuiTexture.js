348b9ee9603a99d111c8696f0d1cae23c5f267d5
mr21 <thomastortorini@gmail.com>
2026-02-19 02:23:50 +0100

    gsuiTexture: beautify

--- gsuiTexture/gsuiTexture.js
+++ gsuiTexture/gsuiTexture.js
@@ -2,12 +2,15 @@
 
 class gsuiTexture {
 	static #bg = {
-		dots:   "%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23fff5' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E",
-		diag:   "%3Csvg xmlns='http://www.w3.org/2000/svg' width='6' height='6' viewBox='0 0 6 6'%3E%3Cg fill='%23000000' fill-opacity='0.2' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E",
-		damier: "%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 8 8'%3E%3Cg fill='%230001'%3E%3Cpath fill-rule='evenodd' d='M0 0h4v4H0V0zm4 4h4v4H4V4z'/%3E%3C/g%3E%3C/svg%3E",
+		dots:   [ 4, 4, "<path fill='#fff5' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'></path>" ],
+		diag:   [ 6, 6, "<g fill='#000000' fill-opacity='0.2' fill-rule='evenodd'><path d='M5 0h1L0 6V5zM6 5v1H5z'/></g>" ],
+		damier: [ 8, 8, "<g fill='#0001'><path fill-rule='evenodd' d='M0 0h4v4H0V0zm4 4h4v4H4V4z'/></g>" ],
 	};
 
 	static $set( el, t ) {
-		el.$css( "backgroundImage", `url("data:image/svg+xml,${ gsuiTexture.#bg[ t ] }")` );
+		const [ w, h, cnt ] = gsuiTexture.#bg[ t ];
+		const svg = `<svg xmlns='http://www.w3.org/2000/svg' width='${ w }' height='${ h }' viewBox='0 0 ${ w } ${ h }'>${ cnt }</svg>`;
+
+		el.$css( "backgroundImage", `url("data:image/svg+xml,${ encodeURIComponent( svg ) }")` );
 	}
 }
