b093b5d6230167ded760bba4a82fe71ab9c5cde4
mr21 <thomastortorini@gmail.com>
2026-02-19 02:41:19 +0100

    gsuiTexture: simplify svgs

--- gsuiTexture/gsuiTexture.js
+++ gsuiTexture/gsuiTexture.js
@@ -7,9 +7,9 @@
 
 class gsuiTexture {
 	static #bg = {
-		dots:   [ 4, 4, "<path fill='#fff5' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'></path>" ],
-		diag:   [ 6, 6, "<g fill='#000000' fill-opacity='0.2' fill-rule='evenodd'><path d='M5 0h1L0 6V5zM6 5v1H5z'/></g>" ],
-		damier: [ 8, 8, "<g fill='#0001'><path fill-rule='evenodd' d='M0 0h4v4H0V0zm4 4h4v4H4V4z'/></g>" ],
+		dots:   [ 4, 4, "<path fill='#fff5' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'/>" ],
+		diag:   [ 6, 6, "<path fill='#0003' d='M5 0h1L0 6V5zM6 5v1H5z'/>" ],
+		damier: [ 8, 8, "<path fill='#0001' d='M0 0h4v4H0V0zm4 4h4v4H4V4z'/>" ],
 	};
 
 	static $set( el, t ) {
