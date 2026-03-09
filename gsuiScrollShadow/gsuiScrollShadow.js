d0ff09fd4259c77fa0733f1695261ef560c23df7
mr21 <thomastortorini@gmail.com>
2026-03-09 06:43:17 +0100

    gsuiScrollShadow: jqueryfy

--- gsuiScrollShadow/gsuiScrollShadow.js
+++ gsuiScrollShadow/gsuiScrollShadow.js
@@ -10,22 +10,22 @@ class gsuiScrollShadow {
 
 	constructor( o ) {
 		Object.seal( this );
-		this.#scrolledElem = o.scrolledElem;
-		this.#topShadow = o.topShadow ? [ o.topShadow ].flat() : [];
-		this.#leftShadow = o.leftShadow ? [ o.leftShadow ].flat() : [];
-		this.#rightShadow = o.rightShadow ? [ o.rightShadow ].flat() : [];
-		this.#bottomShadow = o.bottomShadow ? [ o.bottomShadow ].flat() : [];
+		this.#scrolledElem = $( o.scrolledElem );
+		this.#topShadow = $( o.topShadow );
+		this.#leftShadow = $( o.leftShadow );
+		this.#rightShadow = $( o.rightShadow );
+		this.#bottomShadow = $( o.bottomShadow );
 		gsuiScrollShadow.#initShadow( this.#topShadow, "top" );
 		gsuiScrollShadow.#initShadow( this.#leftShadow, "left" );
 		gsuiScrollShadow.#initShadow( this.#rightShadow, "right" );
 		gsuiScrollShadow.#initShadow( this.#bottomShadow, "bottom" );
-		this.#scrolledElem.addEventListener( "scroll", this.#onscrollBind );
-		GSUdomObserveSize( this.#scrolledElem, this.#onscrollBind );
+		o.scrolledElem.addEventListener( "scroll", this.#onscrollBind );
+		GSUdomObserveSize( o.scrolledElem, this.#onscrollBind );
 	}
 
 	// .........................................................................
 	$disconnected() {
-		GSUdomUnobserveSize( this.#scrolledElem, this.#onscrollBind );
+		GSUdomUnobserveSize( this.#scrolledElem.$get( 0 ), this.#onscrollBind );
 	}
 
 	// .........................................................................
@@ -33,23 +33,18 @@ class gsuiScrollShadow {
 		this.#onscroll();
 	}
 	static #initShadow( elems, dir ) {
-		elems.forEach( el => {
-			GSUdomAddClass( el, "gsuiScrollShadow" );
-			el.dataset.dir = dir;
-		} );
+		elems.$addClass( "gsuiScrollShadow" ).$setAttr( "data-dir", dir );
 	}
 	#onscroll() {
 		const el = this.#scrolledElem;
 
-		gsuiScrollShadow.#onscroll2( this.#topShadow, el.scrollTop );
-		gsuiScrollShadow.#onscroll2( this.#leftShadow, el.scrollLeft );
-		gsuiScrollShadow.#onscroll2( this.#rightShadow, el.scrollWidth - el.clientWidth - el.scrollLeft );
-		gsuiScrollShadow.#onscroll2( this.#bottomShadow, el.scrollHeight - el.clientHeight - el.scrollTop );
+		gsuiScrollShadow.#onscroll2( this.#topShadow, el.$scrollY() );
+		gsuiScrollShadow.#onscroll2( this.#leftShadow, el.$scrollX() );
+		gsuiScrollShadow.#onscroll2( this.#rightShadow, el.$scrollW() - el.$width() - el.$scrollX() );
+		gsuiScrollShadow.#onscroll2( this.#bottomShadow, el.$scrollH() - el.$height() - el.$scrollY() );
 	}
 	static #onscroll2( elems, scroll ) {
-		elems.forEach( el => {
-			GSUdomStyle( el, "--gsuiScrollShadow-dist", `${ Math.min( scroll / 5, 5 ) }px` );
-			GSUdomTogClass( el, "gsuiScrollShadow-shadowed", scroll > 0 );
-		} );
+		elems.$togClass( "gsuiScrollShadow-shadowed", scroll > 0 )
+			.$css( "--gsuiScrollShadow-dist", `${ Math.min( scroll / 5, 5 ) }px` );
 	}
 }
