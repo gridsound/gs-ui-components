a7401de33a3ade51fc719c3591ac4ab1a84cbd1f
mr21 <thomastortorini@gmail.com>
2026-03-22 21:42:19 +0100

    gsuiAutomation: CSS, target button ellipsis

--- gsuiAutomation/gsuiAutomation.html.js
+++ gsuiAutomation/gsuiAutomation.html.js
@@ -4,7 +4,7 @@ GSUsetTemplate( "gsui-automation", () => [
 	GSUcreateDiv( { class: "gsuiAutomation-in" },
 		GSUcreateDiv( { class: "gsuiAutomation-head" },
 			GSUcreateIcon( { icon: "target" } ),
-			GSUcreateButton( { class: "gsuiAutomation-btnTarget" } ),
+			GSUcreateButton( { class: "gsuiAutomation-btnTarget gsui-ellipsis" } ),
 			GSUcreateElement( "gsui-duration" ),
 		),
 		GSUcreateDiv( { class: "gsuiAutomation-body" },
