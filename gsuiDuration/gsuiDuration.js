39088d91320c00ff077bb08a7d441f8d595ab87d
mr21 <thomastortorini@gmail.com>
2026-03-22 19:16:39 +0100

    gsuiDuration

---
+++ gsuiDuration/gsuiDuration.js
@@ -0,0 +1,52 @@
+"use strict";
+
+class gsuiDuration extends gsui0ne {
+	constructor() {
+		super( {
+			$tagName: "gsui-duration",
+			$template: [
+				GSUcreateFlex( { y: true },
+					GSUcreateSpan( { inert: true }, "duration" ),
+					GSUcreateElement( "gsui-slider", { type: "linear-x", min: 1, max: 8, step: 1, "mousemove-size": 300 } ),
+				),
+				GSUcreateElement( "b", { inert: true }, 1 ),
+			],
+			$elements: {
+				$slider: "gsui-slider",
+				$output: "b",
+			},
+			$attributes: {
+				value: 1,
+			},
+		} );
+		GSUdomListen( this, {
+			[ GSEV_SLIDER_INPUTSTART ]: GSUnoop,
+			[ GSEV_SLIDER_INPUTEND ]: GSUnoop,
+			[ GSEV_SLIDER_CHANGE ]: ( _, dur ) => this.$this.$dispatch( GSEV_DURATION_CHANGE, dur ),
+			[ GSEV_SLIDER_INPUT ]: ( _, dur ) => {
+				this.#updateOutput( dur );
+				this.$this.$dispatch( GSEV_DURATION_INPUT, dur );
+			},
+		} );
+	}
+
+	// .........................................................................
+	static get observedAttributes() {
+		return [ "value" ];
+	}
+	$attributeChanged( prop, val ) {
+		switch ( prop ) {
+			case "value":
+				this.#updateOutput( val );
+				this.$elements.$slider.$setAttr( "value", val );
+				break;
+		}
+	}
+
+	// .........................................................................
+	#updateOutput( dur ) {
+		this.$elements.$output.$text( `${ dur }`.padStart( 2, "0" ) );
+	}
+}
+
+GSUdomDefine( "gsui-duration", gsuiDuration );
