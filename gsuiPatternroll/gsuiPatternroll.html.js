bec50fdf8215e0b2853a100b0bb74599eec11a63
mr21 <thomastortorini@gmail.com>
2026-03-31 20:23:22 +0200

    gsuiBlocksManager: CSS, makes crop div invisible but cursors

--- gsuiPatternroll/gsuiPatternroll.html.js
+++ gsuiPatternroll/gsuiPatternroll.html.js
@@ -2,15 +2,15 @@
 
 GSUsetTemplate( "gsui-patternroll-block", () =>
 	GSUcreateDiv( { class: "gsuiBlocksManager-block gsuiPatternroll-block", "data-action": "move" },
-		GSUcreateDiv( { class: "gsuiBlocksManager-block-crop gsuiBlocksManager-block-cropA", "data-action": "cropA" } ),
-		GSUcreateDiv( { class: "gsuiBlocksManager-block-crop gsuiBlocksManager-block-cropB", "data-action": "cropB" } ),
+		GSUcreateDiv( { "data-action": "cropA" } ),
+		GSUcreateDiv( { "data-action": "cropB" } ),
 		GSUcreateDiv( { class: "gsuiPatternroll-block-header" },
 			GSUcreateSpan( { class: "gsuiPatternroll-block-name" } ),
 		),
 		GSUcreateDiv( { class: "gsuiPatternroll-block-content" } ),
 		GSUcreateDiv( { class: "gsuiPatternroll-block-placeholder" },
-			GSUcreateIcon( { class: "gsuiPatternroll-block-placeholderIcon", icon: "file-corrupt" } ),
-			GSUcreateSpan( { class: "gsuiPatternroll-block-placeholderText" }, "missing data" ),
+			GSUcreateIcon( { icon: "file-corrupt" } ),
+			GSUcreateSpan( null, "missing data" ),
 		),
 	)
 );
--- gsuiPianoroll/gsuiPianoroll.html.js
+++ gsuiPianoroll/gsuiPianoroll.html.js
@@ -4,7 +4,7 @@ GSUsetTemplate( "gsui-pianoroll-block", () =>
 	GSUcreateDiv( { class: "gsuiBlocksManager-block gsuiPianoroll-block", "data-action": "move" },
 		GSUcreateSpan(),
 		GSUcreateDiv( { class: "gsuiDragline-drop" } ),
-		GSUcreateDiv( { class: "gsuiBlocksManager-block-crop gsuiBlocksManager-block-cropB", "data-action": "cropB" } ),
+		GSUcreateDiv( { "data-action": "cropB" } ),
 		GSUcreateElement( "gsui-dragline" ),
 	)
 );
