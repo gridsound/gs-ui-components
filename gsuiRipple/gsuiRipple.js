0111ec70ebbd2fcf346c32e979a8eda37b86c00c
mr21 <thomastortorini@gmail.com>
2026-05-09 05:24:09 +0200

    gsuiRipple: simplify

--- gsuiRipple/gsuiRipple.js
+++ gsuiRipple/gsuiRipple.js
@@ -46,22 +46,21 @@ class gsuiRipple {
 	}
 	static #exec( e, x, y ) {
 		const obj = gsuiRipple.#map.get( e.currentTarget );
-		const el = obj.$elCirclePar;
 		const circ = $( "<span>" )
 			.$addClass( "gsuiRipple-circle" )
 			.$addAttr( "inert" )
 			.$left( x * 100, "%" )
 			.$top( y * 100, "%" )
-			.$prependTo( el );
+			.$prependTo( obj.$elCirclePar );
 
 		GSUclearTimeout( obj.$timeoutId );
 		gsuiRipple.#rmCirc( obj );
 		obj.$elCirc = circ;
-		el.$rmAttr( "data-ripple-active" );
+		circ.$rmAttr( "data-ripple-active" );
 		obj.$timeoutId = GSUsetTimeout( () => {
-			el.$addAttr( "data-ripple-active" );
+			circ.$addAttr( "data-ripple-active" );
 			obj.$timeoutId = GSUsetTimeout( () => {
-				el.$rmAttr( "data-ripple-active" );
+				circ.$rmAttr( "data-ripple-active" );
 				gsuiRipple.#rmCirc( obj );
 			}, .7 );
 		}, .01 );
