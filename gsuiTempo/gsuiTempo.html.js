57bcb6c377c2a6b6b9f048d3dd3a0b731617fe37
mr21 <thomastortorini@gmail.com>
2026-05-12 18:35:51 +0200

    gsuiTempo: use the Popover API

--- gsuiTempo/gsuiTempo.html.js
+++ gsuiTempo/gsuiTempo.html.js
@@ -1,34 +1,35 @@
 "use strict";
 
-$.$setTemplate( "gsui-tempo", () =>
-	$.$button( { title: "Edit the time signature and BPM" },
-		$.$div( { class: "gsuiTempo-timeDivision" },
-			$.$span( { class: "gsuiTempo-beatsPerMeasure" } ),
-			$.$span( { class: "gsuiTempo-stepsPerBeat" } ),
-		),
-		$.$span( { class: "gsuiTempo-bpm" } ),
-	),
-);
+let gsuiTempo_count = 0;
 
-$.$setTemplate( "gsui-tempo-dropdown", () =>
-	$.$elem( "form", { class: "gsuiTempo-popup", tabindex: -1 },
-		$.$div( null,
-			$.$label( null, "Beats per measure" ),
-			$.$input( { type: "number", min: 1, max: 16, required: true } ),
-		),
-		$.$div( null,
-			$.$label( null, "Steps per beat" ),
-			$.$input( { type: "number", min: 1, max: 16, required: true } ),
-		),
-		$.$div( { class: "gsuiTempo-popup-bpm" },
-			$.$label( null, "BPM " ),
-			$.$div( null,
-				$.$input( { type: "number", step: .01, min: 1, max: 999.99, required: true } ),
-				$.$button( { icon: "tint" } ),
+$.$setTemplate( "gsui-tempo", () => {
+	const popId = `gsuiTempo_count_${ ++gsuiTempo_count }`;
+
+	return [
+		$.$button( { popovertarget: popId, title: "Edit the time signature and BPM" },
+			$.$div( { class: "gsuiTempo-timeDivision" },
+				$.$span( { class: "gsuiTempo-beatsPerMeasure" } ),
+				$.$span( { class: "gsuiTempo-stepsPerBeat" } ),
 			),
+			$.$span( { class: "gsuiTempo-bpm" } ),
 		),
-		$.$div( null,
+		$.$elem( "form", { class: "gsuiTempo-form", id: popId, popover: true },
+			$.$div( null,
+				$.$label( null, "Beats per measure" ),
+				$.$input( { type: "number", min: 1, max: 16, required: true } ),
+			),
+			$.$div( null,
+				$.$label( null, "Steps per beat" ),
+				$.$input( { type: "number", min: 1, max: 16, required: true } ),
+			),
+			$.$div( { class: "gsuiTempo-form-bpm" },
+				$.$label( null, "BPM " ),
+				$.$div( null,
+					$.$input( { type: "number", step: .01, min: 1, max: 999.99, required: true } ),
+					$.$button( { icon: "tint" } ),
+				),
+			),
 			$.$button( { type: "submit" }, "Ok" ),
 		),
-	),
-);
+	];
+} );
--- gsuiTempo/gsuiTempo.js
+++ gsuiTempo/gsuiTempo.js
@@ -1,33 +1,26 @@
 "use strict";
 
 class gsuiTempo extends gsui0ne {
-	#dropdown = new gsuiDropdown();
-	#popup = $( $.$getTemplate( "gsui-tempo-dropdown" ) ).$queryMap( {
-		$form: ".gsuiTempo-popup",
-		$inputs: "input",
-		$bpmTap: ".gsuiTempo-popup-bpm button",
-	} );
-
 	constructor() {
 		super( {
 			$tagName: "gsui-tempo",
 			$elements: {
 				$bpm: ".gsuiTempo-bpm",
 				$timediv: ".gsuiTempo-timeDivision",
+				$form: "form",
+				$inputs: "input",
+				$bpmTap: ".gsuiTempo-form-bpm button",
 			},
 			$attributes: {
 				timedivision: "4/4",
 				bpm: 60,
 			},
 		} );
-		this.#dropdown.$setDirection( "B" );
-		this.#dropdown.$bindTargetElement( this.$element );
-		this.#dropdown.$onopenCreateElement( this.#createPopup.bind( this ) );
-		this.#popup.$form.$on( {
+		this.$elements.$form.$on( {
 			submit: this.$onsubmitPopup.bind( this ),
 			keydown: e => e.stopPropagation(),
 		} );
-		this.#popup.$bpmTap.$onclick( () => this.#popup.$inputs.$at( 2 ).$value( gswaBPMTap.$tap() ) );
+		this.$elements.$bpmTap.$onclick( () => this.$elements.$inputs.$at( 2 ).$value( gswaBPMTap.$tap() ) );
 	}
 
 	// .........................................................................
@@ -37,14 +30,14 @@ class gsuiTempo extends gsui0ne {
 	$attributeChanged( prop, val ) {
 		switch ( prop ) {
 			case "bpm":
-				this.#popup.$inputs.$at( 2 ).$value( val );
+				this.$elements.$inputs.$at( 2 ).$value( val );
 				this.$elements.$bpm.$text( val );
 				break;
 			case "timedivision": {
 				const div = GSUsplitNums( val, "/" );
 
-				this.#popup.$inputs.$at( 0 ).$value( div[ 0 ] );
-				this.#popup.$inputs.$at( 1 ).$value( div[ 1 ] );
+				this.$elements.$inputs.$at( 0 ).$value( div[ 0 ] );
+				this.$elements.$inputs.$at( 1 ).$value( div[ 1 ] );
 				this.$elements.$timediv.$child( 0 ).$text( div[ 0 ] );
 				this.$elements.$timediv.$child( -1 ).$text( div[ 1 ] );
 			} break;
@@ -56,13 +49,13 @@ class gsuiTempo extends gsui0ne {
 		const [ bpm, time ] = this.$this.$getAttr( "bpm", "timedivision" );
 		const time2 = GSUsplitNums( time, "/" );
 
-		this.#popup.$inputs.$at( 0 ).$value( time2[ 0 ] );
-		this.#popup.$inputs.$at( 1 ).$value( time2[ 1 ] );
-		this.#popup.$inputs.$at( 2 ).$value( +bpm );
-		return this.#popup.$form;
+		this.$elements.$inputs.$at( 0 ).$value( time2[ 0 ] );
+		this.$elements.$inputs.$at( 1 ).$value( time2[ 1 ] );
+		this.$elements.$inputs.$at( 2 ).$value( +bpm );
+		return this.$elements.$form;
 	}
 	$onsubmitPopup() {
-		const inp = this.#popup.$inputs;
+		const inp = this.$elements.$inputs;
 		const time = `${ inp.$at( 0 ).$value() }/${ inp.$at( 1 ).$value() }`;
 		const bpm = inp.$at( 2 ).$value();
 
@@ -72,7 +65,7 @@ class gsuiTempo extends gsui0ne {
 				timedivision: time,
 			} );
 		}
-		this.#dropdown.$close();
+		this.$elements.$form.$togglePopover( false );
 		return false;
 	}
 }
