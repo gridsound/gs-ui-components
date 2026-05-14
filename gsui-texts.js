8fe86a58b91237d2e72f573298d6f2f190918fc2
mr21 <thomastortorini@gmail.com>
2026-05-14 18:56:07 +0200

    gsuiDAW: use GSTX

--- gsui-texts.js
+++ gsui-texts.js
@@ -2,6 +2,29 @@
 
 const GSTXall = {
 	en: {
+		$daw_gain:          "Main app <b>volume</b> (this will not affect the rendering)",
+		$daw_focus:         "Switch <b>focus</b> between the composition and piano windows",
+		$daw_reboot:        "<b>Reboot</b> the audio engine",
+		$daw_undo:          "<b>Undo</b> the previous action",
+		$daw_redo:          "<b>Redo</b> the last action",
+		$daw_render:        "<b>Render</b> the composition",
+		$daw_settings:      "Settings",
+		$daw_open_compo:    "Toggle the <b>composition</b> window",
+		$daw_open_patterns: "Toggle the <b>patterns</b> panel",
+		$daw_open_mixer:    "Toggle the <b>mixer</b> window",
+		$daw_open_synth:    "Toggle the <b>synthesizer</b> window",
+		$daw_open_drums:    "Toggle the <b>drums</b> window",
+		$daw_open_piano:    "Toggle the <b>piano</b> window",
+		$daw_open_slicer:   "Toggle the <b>slicer</b> window",
+		$daw_toggle_help:   "Toggle <b>help</b> links ❓",
+		$daw_about:         "About <i>GridSound</i> 🎵",
+		$daw_changelog:     "Read the <b>changelog</b>",
+		// .....................................................................
+		$win_rename_keys:   "<b>Rename</b> this keys pattern",
+		$win_rename_synth:  "<b>Rename</b> this synthesizer",
+		$win_rename_drums:  "<b>Rename</b> this drums pattern",
+		$win_rename_slices: "<b>Rename</b> this slices pattern",
+		$win_autoscroll:    "Auto scrolling",
 	},
 };
 
